a238a7ad7e341bb9d9b2b87f327644bd26c4559b
razaakomal <komal.raza@leoatech.com>
2023-12-18 07:41:40 +0500

    Added App Root Page

---
+++ frontend/src/_root/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeroSection from '../../components/_user/HeroSection/HeroSection';
+
+const Home = () => {
+  return (
+    <>
+      <HeroSection />
+    </>
+  )
+}
+
+export default Home
