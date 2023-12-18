46c66969244d981098d17366fc8f1d91e1cd4dec
razaakomal <komal.raza@leoatech.com>
2023-12-18 07:39:58 +0500

    Update main Page Routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routers/router.jsx";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Suspense fallback={<> </>}>
+     <RouterProvider router={router} />
+    </Suspense>
+  </React.StrictMode>
+);
