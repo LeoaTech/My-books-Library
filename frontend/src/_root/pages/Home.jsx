62b82825150deada8ee03e4674fe3fbdb9174ad8
leoatech-Quratulain <quratulain.shoro@leoatech.com>
2024-01-10 13:45:31 +0500

    Update Home.jsx

--- frontend/src/_root/pages/Home.jsx
+++ frontend/src/_root/pages/Home.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
-import HeroSection from '../../components/_user/HeroSection/HeroSection';
+import React from "react";
+import HeroSection from "../../components/_user/HeroSection/HeroSection";
+import RecommendedBooks from "../../components/_user/Recommended/RecommendedBooks";
+import NewArrivalBooks from "../../components/_user/NewArrival/NewArrivalBooks";
+import Genres from "../../components/_user/Categories/Genres";
+import Stats from "../../components/_user/Stats/Stats";
+import Footer from "../../components/_user/Footer/Footer";
 
 const Home = () => {
+  const genres = [
+    "Action",
+    "Adventure",
+    "Comedy",
+    "Drama",
+    "Fantasy",
+    "Horror",
+    "Mystery",
+    "Sci-Fi",
+    "Thriller",
+    "Western",
+    "Romance",
+    "Crime",
+    "Bioraphy",
+    "Business",
+    "Humor",
+    "Gardening",
+    "Health",
+  ];
+
   return (
     <>
       <HeroSection />
+      <RecommendedBooks />
+      <NewArrivalBooks />
+      <Genres genres={genres} />
+      <Stats />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
