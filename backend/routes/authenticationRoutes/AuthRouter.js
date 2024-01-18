7bdba1f380d21ed6208b0b6c27eaede66c61262a
razaakomal <komal.raza@leoatech.com>
2024-01-18 10:41:26 +0500

    LIB-64 :[Backend] Create User Sign up Route

--- backend/routes/authenticationRoutes/AuthRouter.js
+++ backend/routes/authenticationRoutes/AuthRouter.js
@@ -1,35 +1,11 @@
 const express = require("express");
 const {
-  RegisterUser,
-  ForgetPassword,
-  LoginUser,
-  RefreshToken,
-  ResetPassword,
+  RegisterUser
 } = require("../../controllers/AuthController/AuthController.js");
-const requiredAuth = require("../../middleware/authMiddleware.js");
-
 
 const router = express.Router();
 
-// Sign up Route
+// New User Sign up Route
 router.post("/signup", RegisterUser);
-
-// Sign in Route
-router.post("/signin", LoginUser);
-
-// Refresh token after 5 min
-
-router.post("/refresh", RefreshToken);
-// Log out Route
-
-router.post("/logout", LogoutUser);
-
-// Forgot Password Route
-
-router.post("/forgot-password", ForgetPassword);
-
-
-router.post("/reset-password", requiredAuth, ResetPassword);
-
 
 module.exports = router;
