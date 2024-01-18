eaaa835b38d05cadcf2b3e21a848bb449b1ff407
razaakomal <komal.raza@leoatech.com>
2024-01-18 17:36:00 +0500

    LIB-75:[frontend] Updated App Routes with RBAC

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -11,39 +11,77 @@ import AdminLayout from "../_admin/Layout";
 import DashboardPage from "../_admin/pages/Home";
 import AuthLayout from "../_authentication/forms/AuthLayout";
 import {
+  EmailVerification,
   ForgetPassword,
   ResetPassword,
   SignIn,
   SignUp,
 } from "../_authentication/forms";
+import PersistLogin from "../utiliz/PersistLogin";
+import RequiredAuth from "../utiliz/RequiredAuth";
+import InvalidToken from "../_authentication/forms/InvalidToken";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
-      <Route path="/" element={<App />}></Route>
+      {/* Authentication Routes */}
       <Route element={<AuthLayout />}>
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/forgotpassword/:id/:token" element={<ResetPassword />} />
         <Route path="/forgot-password" element={<ForgetPassword />} />
+        <Route
+          path="/email-verification/:token"
+          element={<EmailVerification />}
+        />
       </Route>
-      <Route path="/dashboard" element={<AdminLayout />}>
-        <Route index element={<DashboardPage />} />
-        {routes.map((route, i) => {
-          const { component: Component, path } = route;
-          return (
-            <Route
-              key={i}
-              exact={true}
-              path={`/dashboard${path}`}
-              element={
-                <Suspense fallback={<AdminLoader />}>
-                  <Component />
-                </Suspense>
-              }
-            />
-          );
-        })}
+
+      <Route path="/expired-link" element={<InvalidToken />} />
+
+      <Route element={<PersistLogin />}>
+        <Route path="/" element={<App />}></Route>
+
+        {/* Protect Admin Routes */}
+        <Route element={<RequiredAuth allowedRoles={["admin"]} />}>
+          <Route path="/dashboard" element={<AdminLayout />}>
+            <Route index element={<DashboardPage />} />
+            {routes.map((route, i) => {
+              const { component: Component, path } = route;
+              return (
+                <Route
+                  key={i}
+                  exact={true}
+                  path={`/dashboard${path}`}
+                  element={
+                    <Suspense fallback={<AdminLoader />}>
+                      <Component />
+                    </Suspense>
+                  }
+                />
+              );
+            })}
+          </Route>
+        </Route>
+
+        {/* Librarian Routes */}
+        <Route element={<RequiredAuth allowedRoles={["admin", "librarian"]} />}>
+          <Route
+            path="/librarian/dashboard"
+            element={<>Librarian Route</>}
+          ></Route>
+        </Route>
+
+        {/* Moderator Routes */}
+        <Route
+          element={
+            <RequiredAuth allowedRoles={["admin", "moderator", "librarian"]} />
+          }
+        >
+          <Route
+            path="/moderator/dashboard"
+            element={<>Moderator Route</>}
+          ></Route>
+        </Route>
       </Route>
     </Route>
   )
