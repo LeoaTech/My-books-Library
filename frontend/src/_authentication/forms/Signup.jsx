f0eb94d223347b5b98d5d863fe8c3edaa9775750
razaakomal <komal.raza@leoatech.com>
2024-01-18 17:19:11 +0500

    LIB-71: [frontend] Signup Form with Email and Password Validation

--- frontend/src/_authentication/forms/Signup.jsx
+++ frontend/src/_authentication/forms/Signup.jsx
@@ -3,17 +3,24 @@ import { useLocation, useNavigate } from "react-router-dom";
 import GoogleIcon from "../../assets/google.svg";
 import { useSignup } from "../../hooks/useSignup";
 import { useEffect } from "react";
-import { BASE_URL } from "../../utiliz/baseAPIURL";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
+const schema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(6, { message: "Password length must be at least 6 characters" }),
+});
 const Signup = () => {
   const { search } = useLocation();
   const navigate = useNavigate();
-  const { isLoading, error, signup } = useSignup();
+  const { isLoading, error, signup, message } = useSignup();
   const {
     register, //for tracking form state
     handleSubmit,
     reset,
-    watch,
     formState,
   } = useForm({
     defaultValues: {
@@ -22,10 +29,8 @@ const Signup = () => {
       password: "",
     },
     mode: "all",
+    resolver: zodResolver(schema),
   });
-
-
-  console.log(BASE_URL,"URL")
 
   const { errors, isValid, isDirty, isSubmitting, isSubmitSuccessful } =
     formState;
@@ -39,86 +44,86 @@ const Signup = () => {
 
   useEffect(() => {
     if (isSubmitSuccessful) {
-      reset();
-      navigate("/signin")
+      setTimeout(() => {
+        reset();
+        navigate("/signin"), 2000;
+      });
     }
-  }, [isSubmitSuccessful, reset]);
+  }, [reset, isSubmitSuccessful]);
 
   return (
     <div className="py-40 h-screen flex flex-1 justify-center items-center ">
-      <form className="custom-form" onSubmit={handleSubmit(onSubmit)}>
-        <h1 className="form-title">Create account</h1>
-        <div className="custom-form">
-          {/* <a
+      <div className="rounded-lg border border-stroke bg-white p-20 shadow-lg dark:border-strokedark dark:bg-boxdark">
+        <form className="custom-form" onSubmit={handleSubmit(onSubmit)}>
+          <h1 className="form-title">Create account</h1>
+          <div className="custom-form">
+            {/* <a
             className="google-oauth-button"
             href={`/auth/google/start${search}`}
           >
             <img src={GoogleIcon} width={22} height={22} /> Continue with Google
           </a> */}
-          <input
-            className="custom-input"
-            placeholder="Name"
-            {...register("name", {
-              required: { value: true, message: "Name is required" },
-            })}
-          />
-          {errors?.name?.message && (
-            <p className="format-message error">Name: {errors.name.message}</p>
-          )}
-          <input
-            className="custom-input"
-            placeholder="Email"
-            {...register("email", {
-              pattern: {
-                value:
-                  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-                message: "Invalid email format",
-              },
-            })}
-          />
-          {errors?.email?.message && (
-            <p className="format-message error">
-              Email: {errors.email.message}
-            </p>
-          )}
-          <input
-            className="custom-input"
-            placeholder="Password"
-            type="password"
-            {...register("password", {
-              required: { value: true, message: "Password is required" },
-            })}
-          />
-          {errors?.password?.message && (
-            <p className="format-message error">
-              Password: {errors.password.message}
-            </p>
-          )}
-          {errors?.root?.message && (
-            <p className="format-message error">{errors.root.message}</p>
-          )}
-          {isSubmitSuccessful && (
-            <p className="format-message success">Please check your inbox</p>
-          )}
-          <button
-            disabled={isSubmitting || !isDirty || !isValid}
-            type="submit"
-            className="border border-blue-500 bg-graydark text-white rounded-lg py-2 w-1/2 "
-          >
-            {isSubmitting || isLoading ? "Signing up...." : "Signup"}
-          </button>
-          {error && <span className="text-meta-1 text-sm">{error}</span>}
-          <p className="p-4 mt-2">
-            Already have an Account?{" "}
-            <span
-              className="text-primary cursor-pointer"
-              onClick={() => navigate("/signin")}
+            <input
+              className="custom-input"
+              placeholder="Name"
+              {...register("name", {
+                required: { value: true, message: "Name is required" },
+              })}
+            />
+            {errors?.name?.message && (
+              <p className="format-message error">
+                Name: {errors.name.message}
+              </p>
+            )}
+            <input
+              className="custom-input"
+              placeholder="Email"
+              {...register("email")}
+            />
+            {errors?.email?.message && (
+              <p className="format-message error">
+                Email: {errors.email.message}
+              </p>
+            )}
+            <input
+              className="custom-input"
+              placeholder="Password"
+              type="password"
+              {...register("password")}
+            />
+            {errors?.password?.message && (
+              <p className="format-message error">
+                Password: {errors.password.message}
+              </p>
+            )}
+            {errors?.root?.message && (
+              <p className="format-message error">{errors.root.message}</p>
+            )}
+
+            <button
+              disabled={!isDirty || !isValid || isSubmitting}
+              type="submit"
+              className={` cursor-pointer border border-blue-500 bg-graydark text-white rounded-lg py-2 w-1/2 hover:bg-body `}
             >
-              Sign In
-            </span>
-          </p>
-        </div>
-      </form>
+              {isSubmitting || isLoading ? "Signing up...." : "Signup"}
+            </button>
+
+            {/* Error from server side */}
+            {message && error && (
+              <span className="text-meta-1 text-sm">{error}</span>
+            )}
+            <p className="p-4 mt-2">
+              Already have an Account?{" "}
+              <span
+                className="text-primary cursor-pointer"
+                onClick={() => navigate("/signin")}
+              >
+                Sign In
+              </span>
+            </p>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
