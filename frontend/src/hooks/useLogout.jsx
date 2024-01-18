ef41b965dec31ab8382573def090d391fba3b4cf
razaakomal <komal.raza@leoatech.com>
2024-01-18 18:16:20 +0500

    LIB-81: [frontend] Custom Logout hook for signout

--- frontend/src/hooks/useLogout.jsx
+++ frontend/src/hooks/useLogout.jsx
@@ -1,16 +1,34 @@
 import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "./useAuthContext";
+import { BASE_URL } from "../utiliz/baseAPIURL";
+import axios from "axios";
 
 export const useLogout = () => {
-  const { dispatch } = useAuthContext();
+  const { dispatch, setAuth } = useAuthContext();
   const navigate = useNavigate();
 
-  const logout = () => {
-    // Remove the User from local storage
-    localStorage.removeItem("user");
+  // Email and Password Logout Method
+  const logout = async () => {
+    let res = await axios.get(`${BASE_URL}/auth/user/logout`, {
+      withCredentials: true,
+    });
+    console.log(res, "logged out");
+
+    setAuth({});
+    dispatch({ type: "Logout" });
+    navigate("/");
+  };
+
+  // Google Auth Signout Method
+  const signout = async () => {
+    let res = await fetch(`${BASE_URL}/auth/logout`, {
+      credentials: "include",
+    });
+    console.log(res, "logged out");
+
 
     dispatch({ type: "Logout" });
-    navigate("/signin");
+    navigate("/");
   };
-  return { logout };
+  return { logout,signout };
 };
