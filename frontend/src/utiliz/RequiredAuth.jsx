ced2881e4826475f1f2f1230556ba2908e4db872
razaakomal <komal.raza@leoatech.com>
2024-01-18 17:40:48 +0500

    LIB-76: [frontend] Validate user Role and Access to Route

---
+++ frontend/src/utiliz/RequiredAuth.jsx
@@ -0,0 +1,25 @@
+import { useAuthContext } from "../hooks/useAuthContext";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+
+const RequiredAuth = ({ allowedRoles }) => {
+  const { auth } = useAuthContext();
+  const location = useLocation();
+
+  let rolesList = [...allowedRoles];
+  // Check if user is authenticated
+  if (!auth) {
+    // Redirect to the sign-in page if not authenticated
+    return <Navigate to="/signin" state={{ from: location }} replace />;
+  }
+
+  // Check if the user's role is allowed
+  if (rolesList?.includes(auth.role)) {
+    // User has the required role, allow access
+    return <Outlet />;
+  } else {
+    // User does not have the required role, redirect to Home page
+    return <Navigate to="/" state={{ from: location }} replace />;
+  }
+};
+
+export default RequiredAuth;
