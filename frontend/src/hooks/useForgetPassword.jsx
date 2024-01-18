012f81343a3e8bead1876f6c3c6e017f930f18ed
razaakomal <komal.raza@leoatech.com>
2024-01-18 17:30:49 +0500

    LIB-72: [frontend]- Create Custom forget Passwrod hook

---
+++ frontend/src/hooks/useForgetPassword.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { BASE_URL } from "../utiliz/baseAPIURL";
+import axios from "axios";
+
+export const useForgetPassword = () => {
+  // const [email, setEmail] = useState("");
+  const [error,setError] = useState(false)
+  const [message, setMessage] = useState(false);
+
+  const forgetPassword = async (email) => {
+    console.log(email);
+    const res = await fetch(
+      `${BASE_URL}/auth/forgot-password`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({email})
+      }
+    );
+
+    console.log(res, "response");
+
+    if (res.ok) {
+      setMessage(true);
+      setError(false)
+    } else {
+      setError(true)
+      console.log("Invalid email");
+    }
+  };
+  return { forgetPassword, message ,error};
+};
