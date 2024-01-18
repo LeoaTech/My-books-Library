e1a7fc41a596979d2724f7200477aafd80b257cf
razaakomal <komal.raza@leoatech.com>
2024-01-18 17:17:12 +0500

    LIB-7: [frontend]- Custom Hook for Signin Form

--- frontend/src/hooks/useSignin.jsx
+++ frontend/src/hooks/useSignin.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
 import { useAuthContext } from "./useAuthContext";
 import { BASE_URL } from "../utiliz/baseAPIURL";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export const useSignin = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.path || "/";
 
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(null);
+  const [message, setMessage] = useState(null);
 
-  const { dispatch } = useAuthContext();
+  const { dispatch, setAuth } = useAuthContext();
 
   const signin = async (email, password) => {
     setIsLoading(true);
@@ -19,35 +23,50 @@ export const useSignin = () => {
     const response = await fetch(`${BASE_URL}/auth/signin`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
+      credentials: "include",
       body: JSON.stringify({ email, password }),
     });
+
+    // const response = await axios.post(
+    //   `${BASE_URL}/auth/signin`,
+    //   {
+    //     email,
+    //     password,
+    //   },
+    //   { headers: { "Content-Type": "application/json" }, withCredentials: true }
+    // );
     console.log(response, "Form Response");
-    const result = await response.json();
+    const result = await response.json(); //response?.data;
     console.log(result, "Result");
+
+    setIsLoading(false);
 
     if (!response.ok) {
       setIsLoading(false);
-      setError(result.error);
+      setError(true);
+      setMessage(result?.message);
     }
 
     if (response.ok) {
       setIsLoading(false);
       setError(null);
 
-      //   save the json token to local storage;
+      const accessToken = result?.AccessToken;
+      const user = result?.user;
+      // axios.defaults.headers.common[
+      //   "Authorization"
+      // ] = `Bearer ${result?.AccessToken}`;
 
-      localStorage.setItem("user", JSON.stringify(result));
-
+      //   save the json token to local memory;
+      setAuth({ ...user, accessToken: accessToken });
       dispatch({ type: "Login", payload: result });
-
-      setIsLoading(false);
-      if (result?.role === "admin") {
+      if (result?.user === "admin") {
         navigate("/dashboard");
       } else {
-        navigate("/");
+        navigate(from, { replace: true });
       }
     }
   };
 
-  return { signin, isLoading, error };
+  return { signin, isLoading, error, message };
 };
