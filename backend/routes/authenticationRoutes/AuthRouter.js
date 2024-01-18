e91d4c1de5ffe0ab6c477c0a37b9da5aa433dc40
razaakomal <komal.raza@leoatech.com>
2024-01-18 10:43:56 +0500

    LIB-65:[backend] Create Login User Route

--- backend/routes/authenticationRoutes/AuthRouter.js
+++ backend/routes/authenticationRoutes/AuthRouter.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const {
-  RegisterUser
+  RegisterUser,
+  LoginUser
 } = require("../../controllers/AuthController/AuthController.js");
 
 const router = express.Router();
 
 // New User Sign up Route
 router.post("/signup", RegisterUser);
+
+// Log-in Route
+router.post("/signin", LoginUser);
 
 module.exports = router;
