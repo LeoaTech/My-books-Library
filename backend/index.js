9b01fb60fbbfcf6f4a9af4c1b2c5bec9f84a1b3a
razaakomal <komal.raza@leoatech.com>
2024-02-09 08:55:10 +0500

    LIB-110 BACKEND: Add Permissions Routes

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
-dotenv.config();
-const connectDb = require("./config/dbConfig.js");
+require("dotenv").config();
+const store = require("./config/dbConfig.js");
 const { notfound, errorHanlder } = require("./middleware/errorMiddleware.js");
 const session = require("express-session");
 const passport = require("passport");
@@ -10,20 +9,14 @@ const cookieParser = require("cookie-parser");
 const authRouter = require("./routes/authenticationRoutes/AuthRouter.js");
 const booksRouter = require("./routes/booksRoutes/BooksRoutes.js");
 const googleOAuthRouter = require("./routes/authenticationRoutes/GoogleAuthRoute.js");
+const userRouter = require("./routes/UserRoutes.js");
+const rolesRouter = require("./routes/RolesRoute.js");
+const permissionsRouter = require("./routes/PermissionsRoutes.js")
+
 const port = process.env.PORT || 8100;
 
 const app = express();
-
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
-  })
-);
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cookieParser());  //cookies middleware
+app.use(passport.initialize());
 
 app.use(
   session({
@@ -32,11 +25,26 @@ app.use(
     saveUninitialized: true,
   })
 );
-app.use(passport.initialize());
+
 app.use(passport.session());
-app.use("/",googleOAuthRouter);
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:8000"],
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser()); //cookies middleware
+
+app.use("/", googleOAuthRouter);
 app.use("/auth", authRouter);
 app.use("/books", booksRouter);
+app.use("/users", userRouter);
+app.use("/roles",rolesRouter);
+app.use("/permissions",permissionsRouter);
 
 app.use(notfound);
 app.use(errorHanlder);
@@ -48,4 +56,4 @@ app.listen(port, () => {
   console.log("Server is listening on port", port);
 });
 
-connectDb();
+
