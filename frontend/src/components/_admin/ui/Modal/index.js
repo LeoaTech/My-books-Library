a9e1453d83f26d0a7a4b8f2188a83ca9e6e934b5
razaakomal <komal.raza@leoatech.com>
2024-02-09 09:03:41 +0500

    LIB-110 exports permissions components

--- frontend/src/components/_admin/ui/Modal/index.js
+++ frontend/src/components/_admin/ui/Modal/index.js
@@ -1,5 +1,6 @@
-
 import NewRole from "./Roles/NewRole";
 import RoleModal from "./Roles/RoleActions";
+import AddPermission from "./Permissions/AddPermission";
+import PermissionModal from "./Permissions/PermissionsActions";
 
-export {NewRole, RoleModal}
+export { NewRole, RoleModal, AddPermission, PermissionModal };
