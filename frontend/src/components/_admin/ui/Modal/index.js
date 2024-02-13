c7aa287bd57727b152925c86617a5a57a84b2305
razaakomal <komal.raza@leoatech.com>
2024-02-13 10:18:55 +0500

    LIB-115 Exported Modals components for role-permissions

--- frontend/src/components/_admin/ui/Modal/index.js
+++ frontend/src/components/_admin/ui/Modal/index.js
@@ -2,5 +2,14 @@ import NewRole from "./Roles/NewRole";
 import RoleModal from "./Roles/RoleActions";
 import AddPermission from "./Permissions/AddPermission";
 import PermissionModal from "./Permissions/PermissionsActions";
+import AddRolePermission from "./Roles-Permissions/NewRolePermission";
+import RolePermissionAction from "./Roles-Permissions/RolePermissionsActions";
 
-export { NewRole, RoleModal, AddPermission, PermissionModal };
+export {
+  NewRole,
+  RoleModal,
+  AddPermission,
+  PermissionModal,
+  AddRolePermission,
+  RolePermissionAction,
+};
