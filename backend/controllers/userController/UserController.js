1b7fd7e2330aebba215a9e7bd7cb986d1c383c38
razaakomal <komal.raza@leoatech.com>
2024-02-13 11:33:47 +0500

    LIB-125 Create API to Change Role for User

--- backend/controllers/userController/UserController.js
+++ backend/controllers/userController/UserController.js
@@ -1,7 +1,58 @@
 const asyncHanlder = require("express-async-handler");
+const { Client } = require("pg");
+require("dotenv").config();
+
+const connectionUrl = process.env.CONNECTION_URL;
+
+const client = new Client(connectionUrl);
+
+client.connect((err, res) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Authentication API connected");
+  }
+});
 
 const getAllUsers = asyncHanlder(async (req, res) => {
-  res.json({ message: "All users routes are available" });
+  const getUsersQuery = `SELECT
+  u.id AS user_id,
+  u.email,
+  u.role_id,
+  ARRAY_AGG(p.name) AS permissions
+FROM
+  public.users u
+JOIN
+  public.role_permissions rp ON u.role_id = rp.role_id
+JOIN
+  public.permissions p ON rp.permission_id = p.permission_id
+GROUP BY
+  u.id, u.email, u.role_id;
+`;
+
+  const userQuery = `SELECT
+u.id AS user_id,
+u.email,
+u.role_id,
+u.name AS name,
+ARRAY_AGG(p.name) AS permissions
+FROM
+public.users u
+JOIN
+public.role_permissions rp ON u.role_id = rp.role_id
+JOIN
+public.roles r ON r.role_id = u.role_id
+JOIN
+public.permissions p ON rp.permission_id = p.permission_id
+GROUP BY
+u.id, u.email, u.role_id, r.name ; `;
+  const userExists = await client.query(userQuery);
+
+  // console.log(userExists?.rows, "User Found");
+  res.status(200).json({
+    data: userExists?.rows,
+    message: "All users routes are available",
+  });
 });
 
 // Get user Profile Info
@@ -15,4 +66,44 @@ const updateUserPrfile = asyncHanlder(async (req, res) => {
   res.status(200).json({ message: "Update user Successfully" });
 });
 
-module.exports = { updateUserPrfile, getUserProfile, getAllUsers };
+// Update Role for Active User
+const UpdateRoles = asyncHanlder(async (req, res) => {
+  const userId = req.params.user_id;
+  const newRoleID = req.body.newRoleId;
+
+  console.log(req.params, req.body);
+
+  const foundUserID = await client.query(`SELECT * from users where id= $1`, [
+    userId,
+  ]);
+  let user = foundUserID?.rows[0];
+
+  console.log("found user", user);
+  if (!user) {
+    return res.status(401).json({ message: "Invalid UserId" });
+    // throw new Error("Invalid Email Address");
+  }
+
+  try {
+    // create user
+    const updateQuery = await client.query(
+      `UPDATE users SET role_id = $1 WHERE id = $2 RETURNING *`,
+      [newRoleID, userId]
+    );
+
+    console.log(updateQuery?.rows[0]);
+    return res.status(201).json({ message: "Updated Role for ", userId });
+  } catch (error) {
+    console.log(error, "User Role update Error in db");
+  }
+});
+
+
+
+module.exports = {
+  UpdatePermissions,
+  UpdateRoles,
+  updateUserPrfile,
+  getUserProfile,
+  getAllUsers,
+};
