eaf94de25c81a41d3d764fce4dea3b2cddd9f507
razaakomal <komal.raza@leoatech.com>
2024-02-13 19:05:05 +0500

    LIb-21[frontend] Update Users Table data and action fields

--- frontend/src/components/_admin/ui/Tables/UserTable.jsx
+++ frontend/src/components/_admin/ui/Tables/UserTable.jsx
@@ -1,34 +1,43 @@
 import React, { useState } from "react";
-import Tables from "./Tables";
-
+import { MdEdit } from "react-icons/md";
 import UpdatRolesModal from "../Modal/User/UpdatRolesModal";
+import ViewUserDetails from "../Modal/User/ViewUserDetails";
 
 const UserTable = ({ users }) => {
   const [openModal, setOpenModal] = useState(false);
+  const [details, setDetails] = useState(false);
   const [values, setValues] = useState(null);
 
   // To View User Details
-  const viewUserDetails = (userId) => {
-    const currentUser = users?.find((user) => user.id == userId);
-    setValues(currentUser);
+  const editUserDetails = (userId) => {
+    console.log(userId);
+    const activeUser = users?.find((user) => user?.user_id == userId);
+    console.log(activeUser);
+    setValues(activeUser);
     setOpenModal(!openModal);
   };
 
+  const showUserDetails = (userId) => {
+    setDetails(true);
+    const activeUser = users?.find((user) => user?.user_id == userId);
+    console.log(activeUser);
+    setValues(activeUser);  };
+
   return (
-    <div className="rounded-lg border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+    <div className="rounded-lg border border-[#E2E8F0] bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-[#2E3A47] dark:bg-[#AEB7C0] sm:px-7.5 xl:pb-1">
       <div className="max-w-full overflow-x-auto">
         <table className="w-full table-auto">
           <thead>
-            <tr className="bg-gray-2 text-left dark:bg-meta-4">
+            <tr className="bg-[#F7F9FC] text-left dark:bg-[#313D4A]">
               <th className="min-w-[220px] py-4 px-4 font-medium text-black dark:text-white xl:pl-11">
                 Email
               </th>
 
               <th className="min-w-[120px] py-4 px-4 font-medium text-black dark:text-white">
-                Role
+                Name
               </th>
               <th className="min-w-[150px] py-4 px-4 font-medium text-black dark:text-white">
-                Permissions
+                Role Id
               </th>
               <th className="py-4 px-4 font-medium text-black dark:text-white">
                 Actions
@@ -37,37 +46,36 @@ const UserTable = ({ users }) => {
           </thead>
           <tbody>
             {users?.map((user) => (
-              <tr key={user?.id}>
-                <td className="border-b border-[#eee] py-5 px-4 pl-9 dark:border-strokedark xl:pl-11">
+              <tr key={user?.user_id}>
+                <td className="border-b border-[#eee] py-5 px-4 pl-9 dark:border-[#2E3A47] xl:pl-11">
                   <h5 className="font-medium text-black dark:text-white">
                     {user.email}
                   </h5>
                 </td>
 
-                <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
-                  <p className="inline-flex rounded-full bg-success bg-opacity-10 py-1 px-3 text-sm font-medium text-meta-4">
-                    {user.role_name}
+                <td className="border-b border-[#eee] py-5 px-4 dark:border-[#2E3A47]">
+                  <p className="inline-flex rounded-full bg-[#219653] bg-opacity-10 py-1 px-3 text-sm font-medium text-[#313D4A]">
+                    {user.name}
                   </p>
                 </td>
-                <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
-                  <span className="flex items-center gap-1">
+                <td className="border-b border-[#eee] py-5 px-4 dark:border-[#2E3A47]">
+                  {/*  <span className="flex items-center gap-1">
                     {user?.permissions?.map((per, index) => (
-                      <p
-                        key={index}
-                        className="inline-flex rounded-full bg-meta-7 bg-opacity-10 py-1 px-3 text-sm font-medium text-meta-8"
-                      >
-                        {per}
-                      </p>
-                    ))}
-                  </span>
+                      
+                   ))}
+                  </span> */}
+
+                  <p className="inline-flex rounded-full bg-[#FF6766] bg-opacity-10 py-1 px-3 text-sm font-medium text-[#F0950C]">
+                    {user?.role_id}
+                  </p>
                 </td>
-                <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                <td className="border-b border-[#eee] py-5 px-4 dark:border-[#2E3A47]">
                   <div className="flex items-center space-x-3.5">
                     <button
-                      className="hover:text-meta-8"
-                      onClick={() => viewUserDetails(user?.id)}
+                      className="hover:text-[#F0950C]"
+                      onClick={() => editUserDetails(user?.user_id)}
                     >
-                      <svg
+                      {/* <svg
                         className="fill-current"
                         width="18"
                         height="18"
@@ -83,10 +91,15 @@ const UserTable = ({ users }) => {
                           d="M9 11.3906C7.67812 11.3906 6.60938 10.3219 6.60938 9C6.60938 7.67813 7.67812 6.60938 9 6.60938C10.3219 6.60938 11.3906 7.67813 11.3906 9C11.3906 10.3219 10.3219 11.3906 9 11.3906ZM9 7.875C8.38125 7.875 7.875 8.38125 7.875 9C7.875 9.61875 8.38125 10.125 9 10.125C9.61875 10.125 10.125 9.61875 10.125 9C10.125 8.38125 9.61875 7.875 9 7.875Z"
                           fill=""
                         />
-                      </svg>
+                      </svg> */}
+
+                      <MdEdit />
                     </button>
-                    <button className="hover:text-danger">
-                      <svg
+                    <button
+                      className="hover:text-[#D34053]"
+                      onClick={() => showUserDetails(user?.user_id)}
+                    >
+                      {/* <svg
                         className="fill-current"
                         width="18"
                         height="18"
@@ -110,6 +123,24 @@ const UserTable = ({ users }) => {
                           d="M6.72245 9.67504C6.38495 9.70317 6.1037 10.0125 6.13182 10.35L6.3287 12.825C6.35683 13.1625 6.63808 13.4157 6.94745 13.4157C6.97558 13.4157 6.97558 13.4157 7.0037 13.4157C7.3412 13.3875 7.62245 13.0782 7.59433 12.7407L7.39745 10.2657C7.39745 9.90004 7.08808 9.64692 6.72245 9.67504Z"
                           fill=""
                         />
+                      </svg> */}
+
+                      <svg
+                        className="fill-current"
+                        width="18"
+                        height="18"
+                        viewBox="0 0 18 18"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M8.99981 14.8219C3.43106 14.8219 0.674805 9.50624 0.562305 9.28124C0.47793 9.11249 0.47793 8.88749 0.562305 8.71874C0.674805 8.49374 3.43106 3.20624 8.99981 3.20624C14.5686 3.20624 17.3248 8.49374 17.4373 8.71874C17.5217 8.88749 17.5217 9.11249 17.4373 9.28124C17.3248 9.50624 14.5686 14.8219 8.99981 14.8219ZM1.85605 8.99999C2.4748 10.0406 4.89356 13.5562 8.99981 13.5562C13.1061 13.5562 15.5248 10.0406 16.1436 8.99999C15.5248 7.95936 13.1061 4.44374 8.99981 4.44374C4.89356 4.44374 2.4748 7.95936 1.85605 8.99999Z"
+                          fill=""
+                        />
+                        <path
+                          d="M9 11.3906C7.67812 11.3906 6.60938 10.3219 6.60938 9C6.60938 7.67813 7.67812 6.60938 9 6.60938C10.3219 6.60938 11.3906 7.67813 11.3906 9C11.3906 10.3219 10.3219 11.3906 9 11.3906ZM9 7.875C8.38125 7.875 7.875 8.38125 7.875 9C7.875 9.61875 8.38125 10.125 9 10.125C9.61875 10.125 10.125 9.61875 10.125 9C10.125 8.38125 9.61875 7.875 9 7.875Z"
+                          fill=""
+                        />
                       </svg>
                     </button>
                   </div>
@@ -119,12 +150,17 @@ const UserTable = ({ users }) => {
           </tbody>
         </table>
       </div>
+
+      {/* Update Roles Modal */}
       {openModal && (
-        <UpdatRolesModal currentUser={values} setOpenModal={setOpenModal} />
+        <UpdatRolesModal userData={values} setOpenModal={setOpenModal} />
       )}
+
+      {/*  */}
+
+      {details && <ViewUserDetails userData={values} setDetails={setDetails} />}
     </div>
   );
 };
 
 export default UserTable;
-
