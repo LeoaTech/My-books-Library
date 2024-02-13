5911bd46755d0c76c0dac0cad469ebdc12620d34
razaakomal <komal.raza@leoatech.com>
2024-02-13 11:35:58 +0500

    LIB-85 [fronted] Create a Modal Form to Update Role

--- frontend/src/components/_admin/ui/Modal/User/UpdatRolesModal.jsx
+++ frontend/src/components/_admin/ui/Modal/User/UpdatRolesModal.jsx
@@ -1,66 +1,90 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useFetchRoles } from "../../../../../hooks/users/roles/useFetchRole";
+import { useChangeUserRole } from "../../../../../hooks/users/roles/useUpdateRole";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-const UpdatRolesModal = ({ setOpenModal, currentUser }) => {
+const UpdatRolesModal = ({ setOpenModal, userData }) => {
+  const [roles, setRoles] = useState([]);
+
   const { isPending, data } = useFetchRoles();
+  const queryClient = useQueryClient();
+
   const {
     register,
     handleSubmit,
     reset,
-    formState: { errors, isSubmitting, isSubmitSuccessful, isDirty },
+    watch,
+    formState: { errors, isSubmitting, isSubmitSuccessful, isDirty,isValid },
   } = useForm({
     defaultValues: {
-      ...currentUser,
+      ...userData,
     },
   });
 
-  const onSubmit = (data) => {
+  // console.log(userData)
+  const { isLoading, changeRole } = useChangeUserRole();
 
-    console.log(data)
-    // await UpdateRoles(data)
+  const { mutateAsync: changeRoleMutation } = useMutation({
+    mutationFn: changeRole,
+    onSuccess: () => {
+      reset()
+      queryClient.invalidateQueries(["users"]);
+      setOpenModal((prev) => !prev)
+    },
+  });
+
+  const newRoleId = watch("role_id"); // Get the value of 'role_id' from the form
+
+  console.log(newRoleId);
+
+  const onSubmit = async (data) => {
+    console.log(data, "form");
+
+    await changeRoleMutation(data)
   };
 
-  console.log(data);
+  // console.log(data);
   return (
-    <div className="flex justify-center items-center fixed inset-0 bg-body bg-opacity-75 transition-opacity">
-      {/* <div className="relative p-5 bg-whiter rounded-sm"> */}
+    <div className="flex justify-center items-center fixed inset-0 bg-[#64748B] bg-opacity-75 transition-opacity">
+      {/* <div className="relative p-5 bg-[#F5F7FD] rounded-sm"> */}
       <div className="flex justify-center items-center fixed inset-0 z-10 w-screen overflow-y-auto ">
         <div className="">
           {/* <!--Book Details Form --> */}
-          <div className="relative rounded-sm border py-8 px-20 border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-            <h3 className="mb-5 font-bold text-meta-4 dark:text-white">
+          <div className="relative rounded-sm border py-8 px-20 border-[#E2E8F0] bg-white shadow-default dark:border-[#E2E8F0] dark:bg-[#AEB7C0]">
+            <h3 className="mb-5 font-bold text-[#313D4A] dark:text-white">
               Update Roles
             </h3>
-            <div className="flex justify-between rounded-sm border-b border-stroke py-4 px-6.5 dark:border-strokedark">
+            <div className="flex justify-between rounded-md border-b border-[#E2E8F0] py-4 px-6.5 dark:border-[#E2E8F0]">
               <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="p-6.5 m-5.5 sm:overflow-auto">
                   {/* first Row fields */}
                   <div className="mb-4.5 flex flex-col gap-6 md:flex-row md:gap-9 mb-6">
                     <div className="w-full xl:w-1/2" autoFocus>
-                      <label className="mb-2.5 block text-meta-5 dark:text-white">
+                      <label className="mb-2.5 block text-[#259AE6] dark:text-white">
                         Email
                       </label>
                       <input
                         type="text"
+                        readOnly
                         placeholder="Enter your Email Address"
                         {...register("email")}
-                        className="w-full rounded-sm border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                        className="w-full rounded-sm border-[1.5px] border-[#E2E8F0] bg-transparent py-3 px-5 font-medium outline-none transition focus:border-[#3C50E0] active:border-[#3C50E0] disabled:cursor-default disabled:bg-[#F5F7FD] dark:border-[#3d4d60] dark:bg-[#1d2a39] dark:focus:border-[#3C50E0]"
                       />
                     </div>
 
                     <div className="w-full xl:w-1/2">
-                      <label className="mb-2.5 block text-meta-5 dark:text-white">
+                      <label className="mb-2.5 block text-[#259AE6] dark:text-white">
                         Roles
                       </label>
-                      <div className="relative z-20 bg-transparent dark:bg-form-input">
+                      <div className="relative z-20 bg-transparent dark:bg-[#1d2a39]">
                         <select
-                          className="relative z-20 w-full appearance-none rounded-sm border border-stroke bg-transparent py-3 px-5 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-                          {...register("role_name")}
+                          className="relative z-20 w-full appearance-none rounded-sm border border-[#E2E8F0] bg-transparent py-3 px-5 outline-none transition focus:border-[#3C50E0] active:border-[#3C50E0] dark:border-[#3d4d60] dark:bg-[#1d2a39] dark:focus:border-[#3C50E0]"
+                          {...register("role_id")}
                         >
                           {data &&
                             data?.roles?.map((role) => (
-                              <option value={role?.id} key={role?.id}>
+                              <option value={role?.role_id} key={role?.role_id}>
                                 {role?.name}
                               </option>
                             ))}
@@ -86,13 +110,15 @@ const UpdatRolesModal = ({ setOpenModal, currentUser }) => {
                         </span>
                       </div>
                     </div>
+                   
                   </div>
 
                   {/* Submit or Close button */}
                   <div className="mt-20 bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                     <button
                       type="submit"
-                      className="inline-flex w-full justify-center rounded-md bg-meta-6 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                      disabled={!isDirty || isSubmitting || !isValid}
+                      className="inline-flex w-full justify-center rounded-md bg-[#FFBA00] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 sm:ml-3 sm:w-auto"
                     >
                       Update
                     </button>
