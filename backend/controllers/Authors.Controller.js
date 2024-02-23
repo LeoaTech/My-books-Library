0238c09fd3498b69257f9622fe6735dd4727c055
razaakomal <komal.raza@leoatech.com>
2024-02-23 11:42:12 +0500

    LIB-134-[backend] save new author details in db

--- backend/controllers/Authors.Controller.js
+++ backend/controllers/Authors.Controller.js
@@ -13,7 +13,7 @@ client.connect((err, res) => {
     console.log("Authors API connected");
   }
 });
-/* Get ALL Authorses */
+/* Get ALL Authors */
 const FetchAllAuthors = asyncHanlder(async (req, res) => {
   try {
     const AuthorsQuery = `SELECT * FROM authors`;
@@ -28,10 +28,29 @@ const FetchAllAuthors = asyncHanlder(async (req, res) => {
   }
 });
 
+/* Create New Author */
+
+const AddNewAuthor = asyncHanlder(async (req, res) => {
+  console.log(req.body);
+  try {
+    const createAuthorQuery = await client.query(
+      `INSERT INTO authors (name) VALUES ($1)`,
+      [req.body.name]
+    );
+
+    console.log(createAuthorQuery?.rows[0], "Authors /founded");
+
+    res.status(200).json({
+      authors: createAuthorQuery?.rows[0],
+      message: "Authors Found ",
+    });
+  } catch (error) {
+    console.log(error, "Error creating new author");
+  }
+});
 
 /* Fetch Authors By ID */
 
-
 /* Fetch All Authors by Book ID */
 
-module.exports = {FetchAllAuthors}
+module.exports = { FetchAllAuthors, AddNewAuthor };
