0ca413589514928cd14b958994bcf68bf242f993
razaakomal <komal.raza@leoatech.com>
2024-02-23 16:10:26 +0500

    LIB-20 Display books data in table

--- frontend/src/components/_admin/ui/Tables/Tables.jsx
+++ frontend/src/components/_admin/ui/Tables/Tables.jsx
@@ -1,112 +1,193 @@
-import React, { useState } from "react";
-import BookOne from "../../../../assets/book1.jpg";
-const Tables = ({ setBookDetailModal }) => {
-  return (
-    <div className="rounded-sm border border-stroke bg-white shadow-default max-w-full overflow-x-auto overflow-y-auto  dark:border-strokedark dark:bg-boxdark">
-      <div className="w-full overflow-x-auto">
-        <div className="grid grid-cols-11 border-t border-stroke py-4.5 px-2 dark:border-strokedark sm:grid-cols-11 md:px-6 2xl:px-7.5">
-          <div className="col-span-2 flex items-center">
-            <p className="font-medium">Book Name</p>
-          </div>
-          {/* hidden */}
-          <div className="col-span-2  items-center sm:flex">
-            <p className="font-medium">Genre</p>
-          </div>
-          <div className="col-span-1 flex items-center">
-            <p className="font-medium">Price</p>
-          </div>
-          <div className="col-span-2 flex items-center">
-            <p className="font-medium">Author</p>
-          </div>{" "}
-          {/* <div className="col-span-2 flex items-center">
-            <p className="font-medium">Cover</p>
-          </div>
-          <div className="col-span-2 flex items-center">
-            <p className="font-medium">Condition</p>
-          </div>
-          <div className="col-span-2 flex items-center">
-            <p className="font-medium">Sold</p>
-          </div>
-          <div className="col-span-2 flex items-center">
-            <p className="font-medium">Profit</p>
-          </div> */}
-          <div className="col-span-2 flex items-center">
-            <p className="font-medium">ISBN</p>
-          </div>{" "}
-          <div className="col-span-2 flex items-center">
-            <p className="font-medium">Actions</p>
-          </div>{" "}
-        </div>
+import { useEffect, useMemo, useState } from "react";
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
 
-        {[1, 2, 3, 4, 5, 6, 7].map((index) => {
+import DeleteBookModal from "../Modal/DeleteBookModal";
+import { useFetchBooks } from "../../../../hooks/books/useFetchBooks";
+import { ShowBookDetails } from "../..";
+import { MdEdit, MdOutlineDeleteOutline } from "react-icons/md";
+
+const Tables = () => {
+  const { isPending, error, data: booksData } = useFetchBooks();
+
+  const [data, setData] = useState([]);
+  const columnHelper = createColumnHelper();
+
+  useEffect(() => {
+    if (booksData) {
+      setData(booksData);
+    }
+  }, [data]);
+
+  const columns = useMemo(
+    () => [
+      columnHelper.accessor("title_cover", {
+        header: "Book Title",
+        cell: (info) => {
+          const title = info.row.original.title;
+          const images = info.row.original.cover_img_url;
+
           return (
-            <>
-              <div
-                key={index}
-                className="grid grid-cols-11 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-11 md:px-6 2xl:px-7.5"
-              >
-                <div className="col-span-2 flex items-center">
-                  <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
-                    <div className="h-12.5 w-15 rounded-md">
-                      <img
-                        className="h-12 w-12 rounded-lg"
-                        src={BookOne}
-                        alt="Book"
-                      />
-                    </div>
-                    <p className="text-sm text-black dark:text-white">
-                      Think and Grow Rich
-                    </p>
-                  </div>
-                </div>
-                <div className="col-span-2 hidden items-center sm:flex">
-                  {/* <p className="text-sm text-black dark:text-white"> */}
-                  <p className="inline-flex rounded-full bg-meta-4 bg-opacity-10 py-1 px-3 text-sm font-medium text-primary">
-                    Psychology
-                  </p>
-                </div>
-                <div className="col-span-1 flex items-center">
-                  <p className="text-sm text-black dark:text-white">$12</p>
-                </div>
-                <div className="col-span-2 flex items-center">
-                  {/* <p className="text-sm text-black dark:text-white"> */}
-                  <p className="inline-flex rounded-full bg-success bg-opacity-10 py-1 px-2 text-sm font-medium text-success">
-                    Napolean Hill
-                  </p>
-                </div>
-
-                {/* <div className="col-span-2 flex items-center">
-                  <p className="text-sm text-black dark:text-white">Hardback</p>
-                </div>
-                <div className="col-span-2 flex items-center">
-                  <p className="text-sm text-black dark:text-white">Used</p>
-                </div>
-                <div className="col-span-2 flex items-center">
-                  <p className="text-sm text-black dark:text-white">2</p>
-                </div>
-                <div className="col-span-2 flex items-center">
-                  <p className="text-sm text-black dark:text-white">$25.4</p>
-                </div> */}
-                <div className="col-span-2 flex items-center">
-                  <p className="text-sm text-black dark:text-white">
-                    793DR3992DDEe
-                  </p>
-                </div>
-                <div className="col-span-2 flex items-center">
-                  <p
-                    className="cursor-pointer text-sm text-primary underline dark:text-white"
-                    onClick={() => setBookDetailModal(true)}
-                  >
-                    view details
-                  </p>
-                </div>
+            <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
+              <div className="h-12.5 w-15 rounded-md">
+                {images && (
+                  <img
+                    src={images[0]?.secureURL}
+                    alt={`Cover Image `}
+                    className="h-12 w-12 rounded-lg"
+                  />
+                )}
               </div>
-            </>
+              <p className="text-sm text-slate-600 dark:text-white">{title}</p>
+            </div>
           );
-        })}
+        },
+        footer: (info) => info.column.id,
+      }),
+      columnHelper.accessor("category_name", {
+        header: () => "Genre",
+        cell: (info) => info.renderValue(),
+        footer: (info) => info.column.id,
+      }),
+      columnHelper.accessor("rental_price", {
+        header: () => <span>Rental Price</span>,
+        footer: (info) => info.column.id,
+      }),
+      columnHelper.accessor("purchase_price", {
+        header: "Purchase Price",
+        footer: (info) => info.column.id,
+      }),
+      columnHelper.accessor("author_name", {
+        header: "Author",
+        footer: (info) => info.column.id,
+      }),
+      columnHelper.accessor("isbn", {
+        header: "ISBN",
+        footer: (info) => info.column.id,
+      }),
+      columnHelper.accessor("actions", {
+        header: "Actions",
+        cell: (info) => {
+          const bookId = info.row.original?.id;
+
+          return (
+            <div className="flex gap-2">
+              <button
+                onClick={() => editBookDetails(bookId)}
+                className="text-orange-400"
+              >
+                <MdEdit />
+              </button>
+
+              <button
+                onClick={() => viewBookDetails(bookId)}
+                className="text-orange-400"
+              >
+                <MdOutlineDeleteOutline />
+              </button>
+            </div>
+          );
+        },
+        footer: (info) => info.column.id,
+      }),
+    ],
+    []
+  );
+
+  const table = useReactTable({
+    data: booksData?.books,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  const [values, setValues] = useState(null);
+  const [bookDetailModal, setBookDetailModal] = useState(false);
+  const [editDetailModal, setEditDetailModal] = useState(false);
+
+  const viewBookDetails = (id) => {
+    setValues(id);
+    setBookDetailModal(true);
+  };
+
+  const editBookDetails = (id) => {
+    setValues(id);
+
+    setEditDetailModal(true);
+  };
+
+  const closeModal = () => {
+    setBookDetailModal(false);
+    setEditDetailModal(false);
+  };
+
+  if (isPending) return "Loading..."; //Add Loading Component
+
+  if (error) return "An error has occurred: " + error.message; // Add error Component
+
+  return (
+    <div className="rounded-sm border border-[#E2E8F0] bg-white shadow-default max-w-full overflow-x-auto overflow-y-auto dark:border-[#2E3A47] dark:bg-[#24303F]">
+      {/* React Table */}
+      <div className="max-w-full overflow-x-auto dark:border-[#2E3A47]">
+        {booksData?.books && (
+          <table className="w-full table-auto border">
+            <thead>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <tr
+                  key={headerGroup.id}
+                  className="border-b text-slate-700 uppercase bg-[#f7fcfc] text-center text-md dark:bg-[#313D4A]"
+                >
+                  {headerGroup.headers.map((header) => (
+                    <th
+                      key={header.id}
+                      className="px-4 pr-2 font-medium py-4 text-slate-700 dark:text-white"
+                    >
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </th>
+                  ))}
+                </tr>
+              ))}
+            </thead>
+            <tbody>
+              {table?.getRowModel()?.rows?.map((row) => (
+                <tr key={row.id} className="border-b">
+                  {row?.getVisibleCells()?.map((cell) => (
+                    <td
+                      key={cell.id}
+                      className="text-center pt-[14px] pb-[18px] border-b border-[#eee] py-5 px-4 pl-9 dark:border-[#2E3A47] xl:pl-7"
+                    >
+                      <p className="font-medium text-slate-600 dark:text-white">
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </p>
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+        <div />
       </div>
+
+      {/* Edit / Delete  Book Details Modal */}
+
+      {editDetailModal && (
+        <ShowBookDetails bookValue={values} close={closeModal} />
+      )}
+
+      {bookDetailModal && <DeleteBookModal data={values} close={closeModal} />}
     </div>
   );
 };
-
 export default Tables;
