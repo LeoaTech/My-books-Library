c5067f1f677d3762ef840fb4ac131c4decb4a160
razaakomal <komal.raza@leoatech.com>
2024-02-26 09:32:00 +0500

    LIB-84: [backend] API route to get all users

---
+++ backend/routes/UserRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const {
+  getAllUsers,
+  UpdateRoles,
+  DeleteUser,
+  getUserProfile,
+} = require("../controllers/userController/UserController");
+
+const router = express.Router();
+
+// Get All Users
+router.get("/", getAllUsers);
+
+// Get User Profile by User ID
+router.get("/user", getUserProfile);
+
+/* Protect these Route For Admins Only */
+//Update Role of a User
+router.put("/:user_id/role", UpdateRoles);
+
+// Delete a User Profile
+router.delete("/:user_id", DeleteUser);
+
+module.exports = router;
