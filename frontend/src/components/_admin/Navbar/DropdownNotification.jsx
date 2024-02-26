a6b2988cc319ad120ddf35e3f794ea05692c310e
razaakomal <komal.raza@leoatech.com>
2024-02-26 16:28:27 +0500

    LIB-24 Updated the Header DropDown Notification Menu

--- frontend/src/components/_admin/Navbar/DropdownNotification.jsx
+++ frontend/src/components/_admin/Navbar/DropdownNotification.jsx
@@ -8,7 +8,7 @@ const DropdownNotification = () => {
   const dropdown = useRef(null);
 
   useEffect(() => {
-    const clickHandler = ({ target }) => {
+    const clickHandler = (target) => {
       if (!dropdown.current) return;
       if (
         !dropdownOpen ||
@@ -24,7 +24,7 @@ const DropdownNotification = () => {
 
   // close if the esc key is pressed
   useEffect(() => {
-    const keyHandler = ({ keyCode }) => {
+    const keyHandler = (keyCode) => {
       if (!dropdownOpen || keyCode !== 27) return;
       setDropdownOpen(false);
     };
@@ -38,13 +38,8 @@ const DropdownNotification = () => {
         ref={trigger}
         onClick={() => setDropdownOpen(!dropdownOpen)}
         to="#"
-        className="relative flex h-8.5 w-8.5 items-center justify-center rounded-full border-[0.5px] border-stroke bg-gray hover:text-primary dark:border-strokedark dark:bg-meta-4 dark:text-white"
+        className="relative flex h-8.5 w-8.5 items-center justify-center rounded-full border-[0.5px] border-[#E2E8F0] bg-[#EFF4FB] hover:text-[#3C50E0] dark:border-[#2E3A47] dark:bg-[#313D4A] dark:text-white"
       >
-        {/* Alert on notifications */}
-        {/* <span className="absolute -top-0.5 right-0 z-1 h-2 w-2 rounded-full bg-meta-1">
-          <span className="absolute -z-1 inline-flex h-full w-full animate-ping rounded-full bg-meta-1 opacity-75"></span>
-        </span> */}
-
         <svg
           className="fill-current duration-300 ease-in-out"
           width="18"
@@ -64,84 +59,142 @@ const DropdownNotification = () => {
         ref={dropdown}
         onFocus={() => setDropdownOpen(true)}
         onBlur={() => setDropdownOpen(false)}
-        className={`absolute -right-27 mt-2.5 flex h-90 w-75 flex-col rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark sm:right-0 sm:w-80 ${
+        className={`absolute flex flex-col mt-8 right-0  w-72 h-80 rounded-sm  border border-[#E2E8F0] bg-white shadow-default dark:border-[#2E3A47] dark:bg-[#24303F] ${
           dropdownOpen === true ? "block" : "hidden"
         }`}
       >
-        <div className="px-4.5 py-3">
-          <h5 className="text-sm font-medium text-bodydark2">Notification</h5>
+        <div className="py-3 px-4 ">
+          <h5 className="text-sm font-medium text-[#8A99AF]">Notification</h5>
         </div>
 
-        <ul className="flex h-auto flex-col overflow-y-auto">
+        <ul className="p-3 flex flex-col overflow-y-auto ">
           <li>
             <Link
-              className="flex flex-col gap-2.5 border-t border-stroke px-4.5 py-3 hover:bg-gray-2 dark:border-strokedark dark:hover:bg-meta-4"
+              className="flex flex-col gap-2.5 border-t border-[#E2E8F0] px-4.5 py-3 hover:bg-[#F7F9FC] dark:border-[#2E3A47] dark:hover:bg-[#313D4A]"
               to="#"
             >
-              <p className="text-sm">
-                <span className="text-black dark:text-white">
+              <p className="text-sm text-[#1C2434] dark:text-white">
+                <span className="text-[#1C2434] dark:text-white">
                   Edit your information in a swipe
                 </span>{" "}
                 Sint occaecat cupidatat non proident, sunt in culpa qui officia
                 deserunt mollit anim.
               </p>
 
-              <p className="text-xs">12 May, 2025</p>
+              <p className="text-xs text-gray-400">12 May, 2025</p>
             </Link>
           </li>
           <li>
             <Link
-              className="flex flex-col gap-2.5 border-t border-stroke px-4.5 py-3 hover:bg-gray-2 dark:border-strokedark dark:hover:bg-meta-4"
+              className="flex flex-col gap-2.5 border-t border-[#E2E8F0] px-4.5 py-3 hover:bg-[#F7F9FC] dark:border-[#2E3A47] dark:hover:bg-[#313D4A]"
               to="#"
             >
-              <p className="text-sm">
-                <span className="text-black dark:text-white">
+              <p className="text-sm text-[#1C2434] dark:text-white">
+                <span className="text-[#1C2434] dark:text-white">
                   It is a long established fact
                 </span>{" "}
                 that a reader will be distracted by the readable.
               </p>
 
-              <p className="text-xs">24 Feb, 2025</p>
+              <p className="text-xs text-gray-400">24 Feb, 2025</p>
             </Link>
           </li>
           <li>
             <Link
-              className="flex flex-col gap-2.5 border-t border-stroke px-4.5 py-3 hover:bg-gray-2 dark:border-strokedark dark:hover:bg-meta-4"
+              className="flex flex-col gap-2.5 border-t border-[#E2E8F0] px-4.5 py-3 hover:bg-[#F7F9FC] dark:border-[#2E3A47] dark:hover:bg-[#313D4A]"
               to="#"
             >
-              <p className="text-sm">
-                <span className="text-black dark:text-white">
+              <p className="text-sm text-[#1C2434] dark:text-white">
+                <span className="text-[#1C2434] dark:text-white">
                   There are many variations
                 </span>{" "}
                 of passages of Lorem Ipsum available, but the majority have
                 suffered
               </p>
 
-              <p className="text-xs">04 Jan, 2025</p>
+              <p className="text-xs text-gray-400">04 Jan, 2025</p>
             </Link>
           </li>
           <li>
             <Link
-              className="flex flex-col gap-2.5 border-t border-stroke px-4.5 py-3 hover:bg-gray-2 dark:border-strokedark dark:hover:bg-meta-4"
+              className="flex flex-col gap-2.5 border-t border-[#E2E8F0] px-4.5 py-3 hover:bg-[#F7F9FC] dark:border-[#2E3A47] dark:hover:bg-[#313D4A]"
               to="#"
             >
-              <p className="text-sm">
-                <span className="text-black dark:text-white">
+              <p className="text-sm text-[#1C2434] dark:text-white">
+                <span className="text-[#1C2434] dark:text-white">
                   There are many variations
                 </span>{" "}
                 of passages of Lorem Ipsum available, but the majority have
                 suffered
               </p>
 
-              <p className="text-xs">01 Dec, 2024</p>
+              <p className="text-xs text-gray-400">01 Dec, 2024</p>
             </Link>
           </li>
-          <Link
-            to="/dashboard/notifications"
-            className="flex justify-center items-center rounded-lg text-bodydark m-2 p-2 underline hover:text-body"
-          >
-            See More
-          </Link>
+
+          <li>
+            <Link
+              className="flex flex-col gap-2.5 border-t border-[#E2E8F0] px-4.5 py-3 hover:bg-[#F7F9FC] dark:border-[#2E3A47] dark:hover:bg-[#313D4A]"
+              to="#"
+            >
+              <p className="text-sm text-[#1C2434] dark:text-white">
+                <span className="text-[#1C2434] dark:text-white">
+                  Edit your information in a swipe
+                </span>{" "}
+                Sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim.
+              </p>
+
+              <p className="text-xs text-gray-400">12 May, 2025</p>
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="flex flex-col gap-2.5 border-t border-[#E2E8F0] px-4.5 py-3 hover:bg-[#F7F9FC] dark:border-[#2E3A47] dark:hover:bg-[#313D4A]"
+              to="#"
+            >
+              <p className="text-sm text-[#1C2434] dark:text-white">
+                <span className="text-[#1C2434] dark:text-white">
+                  It is a long established fact
+                </span>{" "}
+                that a reader will be distracted by the readable.
+              </p>
+
+              <p className="text-xs text-gray-400">24 Feb, 2025</p>
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="flex flex-col gap-2.5 border-t border-[#E2E8F0] px-4.5 py-3 hover:bg-[#F7F9FC] dark:border-[#2E3A47] dark:hover:bg-[#313D4A]"
+              to="#"
+            >
+              <p className="text-sm text-[#1C2434] dark:text-white">
+                <span className="text-[#1C2434] dark:text-white">
+                  There are many variations
+                </span>{" "}
+                of passages of Lorem Ipsum available, but the majority have
+                suffered
+              </p>
+
+              <p className="text-xs text-gray-400">04 Jan, 2025</p>
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="flex flex-col gap-2.5 border-t border-[#E2E8F0] px-4.5 py-3 hover:bg-[#F7F9FC] dark:border-[#2E3A47] dark:hover:bg-[#313D4A]"
+              to="#"
+            >
+              <p className="text-sm text-[#1C2434] dark:text-white">
+                <span className="text-[#1C2434] dark:text-white">
+                  There are many variations
+                </span>{" "}
+                of passages of Lorem Ipsum available, but the majority have
+                suffered
+              </p>
+
+              <p className="text-xs text-gray-400">01 Dec, 2024</p>
+            </Link>
+          </li>
         </ul>
       </div>
     </li>
@@ -149,3 +202,4 @@ const DropdownNotification = () => {
 };
 
 export default DropdownNotification;
+
