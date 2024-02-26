7c28c9dbb0041377b84ed4cbab1e08be789ee058
razaakomal <komal.raza@leoatech.com>
2024-02-26 08:57:54 +0500

    LIB-147-[backend] API to Create New Publisher

--- backend/controllers/Publishers.Controller.js
+++ backend/controllers/Publishers.Controller.js
@@ -28,10 +28,29 @@ const FetchPublishers = asyncHanlder(async (req, res) => {
   }
 });
 
+/* Create New Publisher */
 
+const AddNewPublisher = asyncHanlder(async (req, res) => {
+  console.log(req.body);
+  try {
+    const createPublisherQuery = await client.query(
+      `INSERT INTO publishers (name) VALUES ($1)`,
+      [req.body.name]
+    );
+
+    console.log(createPublisherQuery?.rows[0], "Publishers /founded");
+
+    res.status(200).json({
+      publishers: createPublisherQuery?.rows[0],
+      message: "Publishers Found ",
+    });
+  } catch (error) {
+    console.log(error, "Error creating new Publisher");
+  }
+});
 
 /* Fetch Publishers By ID */
 
 /* Fetch All Publishers by Book ID */
 
-module.exports = { FetchPublishers};
+module.exports = { FetchPublishers,AddNewPublisher };
