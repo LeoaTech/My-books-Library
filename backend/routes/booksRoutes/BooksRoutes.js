326214b29a3261f027ffd99792ba353efb5f9200
razaakomal <komal.raza@leoatech.com>
2024-02-27 11:37:36 +0500

    LIB-157 API route to fetch Book Details by ID

--- backend/routes/booksRoutes/BooksRoutes.js
+++ backend/routes/booksRoutes/BooksRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const requiredAuth = require("../../middleware/authMiddleware.js");
 const {
   GetAllBooks,
+  GetBookById,
 } = require("../../controllers/BooksController/Books.Controllers.js");
 // require("../../controllers/AuthController/GoogleAuth.js");
 
@@ -9,5 +10,8 @@ const router = express.Router();
 
 // Get a list of books
 router.get("/", GetAllBooks);
+
+// Get a Book by BookId
+router.get("/book", GetBookById);
 
 module.exports = router;
