24014692718978eb0a776f7bfb1b66cbd610b45d
razaakomal <komal.raza@leoatech.com>
2024-02-27 11:39:22 +0500

    LIB-143 API route to delete Book Details by bookID

--- backend/routes/booksRoutes/BooksRoutes.js
+++ backend/routes/booksRoutes/BooksRoutes.js
@@ -3,6 +3,7 @@ const requiredAuth = require("../../middleware/authMiddleware.js");
 const {
   GetAllBooks,
   GetBookById,
+  DeleteBook,
 } = require("../../controllers/BooksController/Books.Controllers.js");
 // require("../../controllers/AuthController/GoogleAuth.js");
 
@@ -11,7 +12,8 @@ const router = express.Router();
 // Get a list of books
 router.get("/", GetAllBooks);
 
-// Get a Book by BookId
 router.get("/book", GetBookById);
+
+router.delete("/delete/:book_id", DeleteBook);
 
 module.exports = router;
