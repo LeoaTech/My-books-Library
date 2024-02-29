37c9a45328ebb2171ff39d1c32b3e7375b0f6194
razaakomal <komal.raza@leoatech.com>
2024-02-29 16:03:31 +0500

    LIB-176 Fixed image size issue

--- frontend/src/components/_admin/ui/cards/CardBooks.jsx
+++ frontend/src/components/_admin/ui/cards/CardBooks.jsx
@@ -1,28 +1,12 @@
 import Book from "../../../../assets/book01.svg";
 import Book1 from "../../../../assets/book0.svg";
-import { useEffect, useState } from "react";
 
 const CardBooks = () => {
-  const [theme, setTheme] = useState();
-  const val = localStorage.getItem("color-theme");
-
-  useEffect(() => {
-    setTheme(localStorage.getItem("color-theme"));
-  }, [theme]);
-
-  console.log(theme);
-
   return (
     <div className="rounded-lg border border-[#E2E8F0] bg-white py-6 px-7.5 shadow-default dark:border-[#2E3A47] dark:bg-[#24303F]">
       <div className="h-11.5 w-8.5 flex items-center justify-center">
         <span className="flex h-11 w-11 items-center justify-center rounded-full bg-[#EFF2F7] dark:bg-[#313D4A]">
-          {/* {theme === "dark" ? ( */}
           <img src={Book1} alt="book" className="h-7 w-8 " />
-          {/* ) : (
-            <img src={Book} alt="book" className="h-7 w-8 " />
-          )} */}
-
-         
         </span>
       </div>
 
