08115945c640f5033149067c233f4de20239d29c
razaakomal <komal.raza@leoatech.com>
2024-03-11 12:12:41 +0500

    LIB-38: [backend] Update Middleware  to authenticate user

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const { Client } = require("pg");
+
 const connectionUrl = process.env.CONNECTION_URL;
 
 const client = new Client(connectionUrl);
@@ -25,11 +26,14 @@ const requireAuth = async (req, res, next) => {
 
   const token = authorization.split(" ")[1];
 
+  console.log(req.user, "Auth");
   try {
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-      if (err) return res.status(401).json({ error: "Invalid Token" });
+      if (err) return res.status(403).json({ error: "Invalid Token" });
       console.log(decoded);
-      req.user = decoded?.UserInfo?.id;
+      req.user = decoded?.UserInfo;
+      req.userId = decoded.UserInfo.id;
+      req.role = decoded.UserInfo.role_id;
       next();
     });
   } catch (error) {
@@ -38,4 +42,50 @@ const requireAuth = async (req, res, next) => {
   }
 };
 
-module.exports = requireAuth;
+// Verify Authorization of a User using JWT or User Authentication
+const checkAuth = async (req, res, next) => {
+
+  const authorization = req.headers?.cookie;
+
+  // Parsing the cookie string to extract individual cookie values
+  const cookiesArray = authorization.split(";").map((cookie) => cookie.trim());
+
+  if (!cookiesArray?.length)
+    return res.status(401).json({ error: "Un-authorized" });
+
+  if (req.user) {
+    //extract user data from request
+    req.user = req.user;
+    next();
+  } else {
+    // Find the "refreshToken" cookie and extract its value
+    let refreshTokenValue;
+    for (const cookie of cookiesArray) {
+      const [cookieName, cookieValue] = cookie.split("=");
+      if (cookieName === "refreshToken") {
+        refreshTokenValue = cookieValue;
+        break;
+      }
+    }
+
+    try {
+      jwt.verify(
+        refreshTokenValue,
+        process.env.JWT_REFRESH_SECRET,
+        (err, decoded) => {
+          if (err) return res.status(403).json({ error: "Invalid Token" });
+          // console.log(decoded, "refersh token cookeie");
+          req.user = decoded?.data;
+          req.userId = decoded?.data.id;
+          req.role = decoded.data.role_id;
+          next();
+        }
+      );
+    } catch (error) {
+      console.log(error);
+      res.status(401).json({ error: "Request is not Authorized" });
+    }
+  }
+};
+
+module.exports = { requireAuth, checkAuth };
