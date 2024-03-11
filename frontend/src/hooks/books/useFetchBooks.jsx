aee8a01db70466e79736b6f1a6b6714ae858e9d2
razaakomal <komal.raza@leoatech.com>
2024-03-11 16:15:38 +0500

    LIB-160 Updated custom hook to fetch all books

--- frontend/src/hooks/books/useFetchBooks.jsx
+++ frontend/src/hooks/books/useFetchBooks.jsx
@@ -1,38 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
-import { BASE_URL } from "../../utiliz/baseAPIURL";
-
-// API CALL to FETCH ROLES OF USERS
-
-const fetchBooks = async ({ signal }) =>
-  await fetch(`${BASE_URL}/books`, { signal, credentials: "include" })
-    .then((res) => {
-      // console.log(res, "Books fetched");
-      if (!res.ok) {
-        throw new Error("Couldn't fetch books");
-      } else {
-        return res.json();
-      }
-    })
-    .catch((err) => console.log(err));
+import { fetchBooks } from "../../api/books";
 
 export const useFetchBooks = () => {
   return useQuery({
     queryKey: ["books"],
     queryFn: fetchBooks,
   });
-};
-
-// Fetch Book Details by ID
-
-export const fetchBookById = async (bookId) => {
-  try {
-    const response = await fetch(`${BASE_URL}/books/book?bookId=${bookId}`, {
-      credentials: "include",
-    });
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching books", error);
-    throw error; // Rethrow the error to let React Query handle it
-  }
 };
