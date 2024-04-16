52859d875550959d92f16e3288ad119d3931e361
razaakomal <komal.raza@leoatech.com>
2024-04-16 10:08:05 +0500

    LIB-149 Schema for Edit Book Details Form Validation

--- frontend/src/schemas/books.js
+++ frontend/src/schemas/books.js
@@ -19,3 +19,24 @@ export const bookSchema = z.object({
   credit: z.string(),
   author: z.unknown(),
 });
+
+
+
+export const EditBookSchema = z.object({
+  title: z.string().min(3, { message: "Title must be provided" }),
+  rental_price: z.string().min(1, { message: "Please Enter rent price" }),
+  purchase_price: z.string().min(1, { message: "Please Enter purchase price" }),
+  condition: z.string(),
+  cover: z.string(),
+  category: z.string(),
+  isbn: z.string().min(8, { message: "Please Enter ISBN number" }),
+  available: z.boolean(),
+  vendor_id: z.unknown(),
+  branch_id: null || z.unknown(),
+  discount_percentage: z.string(),
+  summary: z.string(),
+  publish_year: z.string(),
+  publisher: z.unknown(),
+  credit: z.unknown(),
+  author: z.unknown(),
+});
