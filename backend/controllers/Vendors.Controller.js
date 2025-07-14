a23a02c91bc81082587b73dbf311e878244e260e
razaakomal <komal.raza@leoatech.com>
2025-07-14 10:08:03 +0500

    LIB-226 fixed db config: removed code for db connection in vendors controllers file

--- backend/controllers/Vendors.Controller.js
+++ backend/controllers/Vendors.Controller.js
@@ -1,25 +1,13 @@
 const asyncHanlder = require("express-async-handler");
-const { Client } = require("pg");
-require("dotenv").config();
+const pool = require("../config/dbConfig");
 
-const connectionUrl = process.env.CONNECTION_URL;
-
-const client = new Client(connectionUrl);
-
-client.connect((err, res) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Vendors API connected");
-  }
-});
 /* Get ALL vendors */
 const GetVendors = asyncHanlder(async (req, res) => {
   try {
     const vendorsQuery = `SELECT * FROM vendors`;
-    const getAllVendors = await client.query(vendorsQuery);
+    const getAllVendors = await pool.query(vendorsQuery);
 
-    console.log(getAllVendors?.rows)
+    // console.log(getAllVendors?.rows);
     res.status(200).json({
       vendors: getAllVendors?.rows,
       message: "Vendors Found ",
@@ -29,10 +17,90 @@ const GetVendors = asyncHanlder(async (req, res) => {
   }
 });
 
+// Create New Vendor Details
 
+const NewVendor = asyncHanlder(async (req, res) => {
+  try {
+    const VendorData = req.body;
+
+    const { name, role_id } = VendorData;
+
+    const roleId = parseInt(role_id);
+
+    console.log(roleId);
+
+    /* Check if role_id exists in table */
+
+    const existVendorQuery = `SELECT * FROM vendors WHERE role_id = $1 and name = $2`;
+
+    const findVendor = await pool.query(existVendorQuery, [roleId, name]);
+
+    console.log(findVendor?.rows[0], "Vendor found");
+    if (findVendor?.rowCount > 0) {
+      // Update Details for Vendor
+
+      const editVendorQuery = `Update vendors SET last_login = $1 WHERE name =$2 AND role_id =$3 Returning *`;
+      const updateNewVendor = await pool.query(editVendorQuery, [
+        new Date(),
+        name,
+        roleId,
+      ]);
+
+      console.log(updateNewVendor?.rows[0], "Update Vendor");
+
+      res
+        .status(200)
+        .json({ message: "Update Vendor", result: updateNewVendor?.rows[0] });
+    } else {
+      const addVendorQuery = `INSERT INTO vendors (name, role_id,last_login) values ($1,$2,$3) Returning *`;
+      const saveNewVendor = await pool.query(addVendorQuery, [
+        name,
+        role_id,
+        new Date(),
+      ]);
+
+      console.log(saveNewVendor?.rows[0]);
+
+      res
+        .status(200)
+        .json({ message: "New Vendor", result: saveNewVendor?.rows[0] });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/* Delete Vendor  */
+
+const DeleteVendor = asyncHanlder(async (req, res) => {
+  try {
+    const VendorData = req.body;
+    const { role_id } = req.params;
+    const { name } = VendorData;
+
+    const roleId = parseInt(role_id);
+
+    console.log(roleId);
+
+    /* Check if role_id exists in table */
+
+    const existVendorQuery = `SELECT * FROM vendors WHERE role_id = $1 and name = $2`;
+
+    const findVendor = await pool.query(existVendorQuery, [roleId, name]);
+
+    console.log(findVendor?.rows[0], "Vendor found");
+    if (findVendor?.rowCount > 0) {
+      // Update Details for Vendor
+
+      res.status(200).json({ message: "Deleted  Vendor" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 /* Fetch vendors By ID */
 
 /* Fetch All vendors by Book ID */
 
-module.exports = { GetVendors };
+module.exports = { GetVendors, NewVendor, DeleteVendor };
