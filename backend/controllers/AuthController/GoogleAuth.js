e903ad1bb0d209afa081b36c8e85a253decb8675
razaakomal <komal.raza@leoatech.com>
2025-07-14 10:09:48 +0500

    LIB-226 fixed db config: removed code for db connection in google-auth-controller

--- backend/controllers/AuthController/GoogleAuth.js
+++ backend/controllers/AuthController/GoogleAuth.js
@@ -1,19 +1,6 @@
 const passport = require("passport");
+const pool = require("../../config/dbConfig");
 const GoogleStrategy = require("passport-google-oauth2").Strategy;
-const { Client } = require("pg");
-const { jwtDecode } = require("jwt-decode");
-
-const connectionUrl = process.env.CONNECTION_URL;
-require("dotenv").config();
-const client = new Client(connectionUrl);
-
-client.connect((err, res) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Google Auth controller connected");
-  }
-});
 
 passport.use(
   new GoogleStrategy(
@@ -30,16 +17,16 @@ passport.use(
 
         // Check if the user already exists in the database
         const query = "SELECT * FROM users WHERE email= $1";
-        const result = await client.query(query, [profile?.email]);
+        const result = await pool.query(query, [profile?.email]);
 
         if (result?.rows?.length > 0) {
           // User already exists, return the user
           return done(null, result?.rows[0]);
         }
 
-        const checkRole = await client.query(
+        const checkRole = await pool.query(
           "SELECT role_id FROM roles Where name=  $1",
-          ["user" || "USER" || "User"]
+          ["member" || "Member" || "MEMBER"]
         );
 
         let role;
@@ -50,7 +37,7 @@ passport.use(
         // User doesn't exist, create a new user
         const insertQuery =
           "INSERT INTO users ( name, password,role_id, email,img_url  ) VALUES ($1, $2,$3,$4,$5) RETURNING *";
-        const insertedUser = await client.query(insertQuery, [
+        const insertedUser = await pool.query(insertQuery, [
           profile?.displayName,
           "",
           role,
@@ -77,18 +64,21 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser(async (user, done) => {
-  try {
-    const query = "SELECT * FROM users WHERE email= $1";
-    const result = await client.query(query, [user?.email]);
+  // try {
+  //   const query = "SELECT * FROM users WHERE email= $1";
+  //   const result = await pool.query(query, [user?.email]);
 
-    // console.log(result, "Deserialized user");
-    if (result?.rows?.length > 0) {
-      // User already exists, return the user
-      return done(null, result?.rows[0]);
-    }
-  } catch (error) {
-    return done(error, null);
-  }
+  //   // console.log(result, "Deserialized user");
+  //   if (result?.rows?.length > 0) {
+  //     // User already exists, return the user
+  //     return done(null, result?.rows[0]);
+  //   }
+  // } catch (error) {
+  //   return done(error, null);
+  // }
+
+  console.log(user, "Deserializing user");
+  done(null, user);
 });
 
 module.exports = passport;
