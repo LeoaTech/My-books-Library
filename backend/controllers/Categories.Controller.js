6be521271ea2ce464644814abbe6cda67eecac66
razaakomal <komal.raza@leoatech.com>
2025-07-14 10:05:36 +0500

    LIB-226 fixed db config: removed code for db connection in categories controllers file

--- backend/controllers/Categories.Controller.js
+++ backend/controllers/Categories.Controller.js
@@ -1,22 +1,11 @@
 const asyncHanlder = require("express-async-handler");
-const { Client } = require("pg");
-require("dotenv").config();
+const pool = require("../config/dbConfig");
 
-const connectionUrl = process.env.CONNECTION_URL;
-const client = new Client(connectionUrl);
-
-client.connect((err, res) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Categories API connected");
-  }
-});
 /* Get ALL Categories */
 const GetCategories = asyncHanlder(async (req, res) => {
   try {
     const categoriesQuery = `SELECT * FROM categories`;
-    const getAllCategories = await client.query(categoriesQuery);
+    const getAllCategories = await pool.query(categoriesQuery);
 
     res.status(200).json({
       categories: getAllCategories?.rows,
@@ -26,6 +15,5 @@ const GetCategories = asyncHanlder(async (req, res) => {
     console.log(error);
   }
 });
-
 
 module.exports = { GetCategories };
