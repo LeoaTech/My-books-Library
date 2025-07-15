45b8e97bde63ff1c5c5b82c8a2b14da9e4c76d2f
razaakomal <komal.raza@leoatech.com>
2025-07-15 10:35:41 +0500

    LIb-222 added pagination on Users Table list data

--- frontend/src/components/_admin/ui/Tables/UserTable.jsx
+++ frontend/src/components/_admin/ui/Tables/UserTable.jsx
@@ -2,11 +2,26 @@ import React, { useState } from "react";
 import { MdEdit } from "react-icons/md";
 import UpdatRolesModal from "../Modal/User/UpdatRolesModal";
 import ViewUserDetails from "../Modal/User/ViewUserDetails";
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from "react-icons/hi";
 
 const UserTable = ({ users }) => {
   const [openModal, setOpenModal] = useState(false);
   const [details, setDetails] = useState(false);
   const [values, setValues] = useState(null);
+
+  // Paginations
+  const [currentPage, setCurrentPage] = useState(1);
+  const [rowsPerPage] = useState(7);
+
+  // Calculate indexes for pagination
+  const indexOfLastRow = currentPage * rowsPerPage;
+  const indexOfFirstRow = indexOfLastRow - rowsPerPage;
+
+  const currentRows = users?.slice(indexOfFirstRow, indexOfLastRow);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  console.log(users, "Result");
 
   // To View User Details
   const editUserDetails = (userId) => {
@@ -15,13 +30,19 @@ const UserTable = ({ users }) => {
     setOpenModal(!openModal);
   };
 
+  const deleteUserDetails = (userId) => {
+    const deleteUser = users?.find((user) => user?.user_id == userId);
+    setValues(deleteUser);
+    setOpenModal(!openModal);
+  };
+
   const showUserDetails = (userId) => {
     setDetails(true);
     const activeUser = users?.find((user) => user?.user_id == userId);
-    setValues(activeUser);  };
-
+    setValues(activeUser);
+  };
   return (
-    <div className="rounded-lg border border-[#E2E8F0] bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-[#2E3A47] dark:bg-[#AEB7C0] sm:px-7.5 xl:pb-1">
+    <div className="rounded-lg border border-[#E2E8F0] bg-white px-5 pt-6 pb-2.5 shadow-default  max-w-full overflow-x-auto overflow-y-auto dark:border-[#2E3A47] dark:bg-[#24303F]  sm:px-7.5 xl:pb-1">
       <div className="max-w-full overflow-x-auto">
         <table className="w-full table-auto">
           <thead>
@@ -34,7 +55,7 @@ const UserTable = ({ users }) => {
                 Name
               </th>
               <th className="min-w-[150px] py-4 px-4 font-medium text-black dark:text-white">
-                Role Id
+                Role
               </th>
               <th className="py-4 px-4 font-medium text-black dark:text-white">
                 Actions
@@ -42,16 +63,16 @@ const UserTable = ({ users }) => {
             </tr>
           </thead>
           <tbody>
-            {users?.map((user) => (
+            {currentRows?.map((user) => (
               <tr key={user?.user_id}>
                 <td className="border-b border-[#eee] py-5 px-4 pl-9 dark:border-[#2E3A47] xl:pl-11">
-                  <h5 className="font-medium text-black dark:text-white">
+                  <h5 className="font-medium text-gray-600 dark:text-gray-400">
                     {user.email}
                   </h5>
                 </td>
 
                 <td className="border-b border-[#eee] py-5 px-4 dark:border-[#2E3A47]">
-                  <p className="inline-flex rounded-full bg-[#219653] bg-opacity-10 py-1 px-3 text-sm font-medium text-[#313D4A]">
+                  <p className="inline-flex rounded-full bg-[#219653] bg-opacity-10 py-1 px-3 text-sm font-medium text-[#2980de]">
                     {user.name}
                   </p>
                 </td>
@@ -63,13 +84,13 @@ const UserTable = ({ users }) => {
                   </span> */}
 
                   <p className="inline-flex rounded-full bg-[#FF6766] bg-opacity-10 py-1 px-3 text-sm font-medium text-[#F0950C]">
-                    {user?.role_id}
+                    {user?.role_name}
                   </p>
                 </td>
                 <td className="border-b border-[#eee] py-5 px-4 dark:border-[#2E3A47]">
                   <div className="flex items-center space-x-3.5">
                     <button
-                      className="hover:text-[#F0950C]"
+                      className="text-green-500 hover:text-[#F0950C]"
                       onClick={() => editUserDetails(user?.user_id)}
                     >
                       {/* <svg
@@ -93,10 +114,34 @@ const UserTable = ({ users }) => {
                       <MdEdit />
                     </button>
                     <button
-                      className="hover:text-[#D34053]"
+                      className="text-orange-400 hover:text-orange-600"
                       onClick={() => showUserDetails(user?.user_id)}
                     >
-                      {/* <svg
+                      <svg
+                        className="fill-current"
+                        width="18"
+                        height="18"
+                        viewBox="0 0 18 18"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M8.99981 14.8219C3.43106 14.8219 0.674805 9.50624 0.562305 9.28124C0.47793 9.11249 0.47793 8.88749 0.562305 8.71874C0.674805 8.49374 3.43106 3.20624 8.99981 3.20624C14.5686 3.20624 17.3248 8.49374 17.4373 8.71874C17.5217 8.88749 17.5217 9.11249 17.4373 9.28124C17.3248 9.50624 14.5686 14.8219 8.99981 14.8219ZM1.85605 8.99999C2.4748 10.0406 4.89356 13.5562 8.99981 13.5562C13.1061 13.5562 15.5248 10.0406 16.1436 8.99999C15.5248 7.95936 13.1061 4.44374 8.99981 4.44374C4.89356 4.44374 2.4748 7.95936 1.85605 8.99999Z"
+                          fill=""
+                        />
+                        <path
+                          d="M9 11.3906C7.67812 11.3906 6.60938 10.3219 6.60938 9C6.60938 7.67813 7.67812 6.60938 9 6.60938C10.3219 6.60938 11.3906 7.67813 11.3906 9C11.3906 10.3219 10.3219 11.3906 9 11.3906ZM9 7.875C8.38125 7.875 7.875 8.38125 7.875 9C7.875 9.61875 8.38125 10.125 9 10.125C9.61875 10.125 10.125 9.61875 10.125 9C10.125 8.38125 9.61875 7.875 9 7.875Z"
+                          fill=""
+                        />
+                      </svg>
+                    </button>
+
+                    {/* Hide the Delete user Button */}
+                    {/* <button
+                      onClick={() => deleteUserDetails(user?.user_id)}
+                      className="text-red-500"
+                    >
+                      <svg
                         className="fill-current"
                         width="18"
                         height="18"
@@ -120,26 +165,8 @@ const UserTable = ({ users }) => {
                           d="M6.72245 9.67504C6.38495 9.70317 6.1037 10.0125 6.13182 10.35L6.3287 12.825C6.35683 13.1625 6.63808 13.4157 6.94745 13.4157C6.97558 13.4157 6.97558 13.4157 7.0037 13.4157C7.3412 13.3875 7.62245 13.0782 7.59433 12.7407L7.39745 10.2657C7.39745 9.90004 7.08808 9.64692 6.72245 9.67504Z"
                           fill=""
                         />
-                      </svg> */}
-
-                      <svg
-                        className="fill-current"
-                        width="18"
-                        height="18"
-                        viewBox="0 0 18 18"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M8.99981 14.8219C3.43106 14.8219 0.674805 9.50624 0.562305 9.28124C0.47793 9.11249 0.47793 8.88749 0.562305 8.71874C0.674805 8.49374 3.43106 3.20624 8.99981 3.20624C14.5686 3.20624 17.3248 8.49374 17.4373 8.71874C17.5217 8.88749 17.5217 9.11249 17.4373 9.28124C17.3248 9.50624 14.5686 14.8219 8.99981 14.8219ZM1.85605 8.99999C2.4748 10.0406 4.89356 13.5562 8.99981 13.5562C13.1061 13.5562 15.5248 10.0406 16.1436 8.99999C15.5248 7.95936 13.1061 4.44374 8.99981 4.44374C4.89356 4.44374 2.4748 7.95936 1.85605 8.99999Z"
-                          fill=""
-                        />
-                        <path
-                          d="M9 11.3906C7.67812 11.3906 6.60938 10.3219 6.60938 9C6.60938 7.67813 7.67812 6.60938 9 6.60938C10.3219 6.60938 11.3906 7.67813 11.3906 9C11.3906 10.3219 10.3219 11.3906 9 11.3906ZM9 7.875C8.38125 7.875 7.875 8.38125 7.875 9C7.875 9.61875 8.38125 10.125 9 10.125C9.61875 10.125 10.125 9.61875 10.125 9C10.125 8.38125 9.61875 7.875 9 7.875Z"
-                          fill=""
-                        />
                       </svg>
-                    </button>
+                    </button> */}
                   </div>
                 </td>
               </tr>
@@ -147,15 +174,38 @@ const UserTable = ({ users }) => {
           </tbody>
         </table>
       </div>
+      {/* Pagination controls */}
+      <div className="flex justify-start md:justify-end gap-4 mt-5 mb-5">
+        <button
+          onClick={() => paginate(currentPage - 1)}
+          disabled={currentPage === 1}
+          className="disabled:text-slate-300 dark:disabled:text-gray-500 text-blue-500"
+        >
+          <HiChevronDoubleLeft style={{ height: 17, width: 27 }} />
+        </button>
 
+        <span className="text-orange-600 bg-gray-100 border text-sm rounded-full h-[40px] w-[45px] flex justify-center items-center">
+          {currentPage}
+        </span>
+        {users?.length > rowsPerPage && (
+          <button
+            onClick={() => paginate(currentPage + 1)}
+            disabled={indexOfLastRow >= users?.length}
+            className="disabled:text-slate-300 dark:disabled:text-gray-500 text-blue-500"
+          >
+            <HiChevronDoubleRight style={{ height: 17, width: 27 }} />
+          </button>
+        )}
+      </div>
       {/* Update Roles Modal */}
       {openModal && (
         <UpdatRolesModal userData={values} setOpenModal={setOpenModal} />
       )}
 
-      {/*  */}
+      {/*  View User Details*/}
 
       {details && <ViewUserDetails userData={values} setDetails={setDetails} />}
+      
     </div>
   );
 };
