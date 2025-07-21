78a3b43f2a117e0b73d71a6a44dc5200e37a9d13
razaakomal <komal.raza@leoatech.com>
2025-07-21 21:11:53 +0500

    LIB-241- fixed dark theme styling issue and holding values of items list in the order update Form in Edit Order Modal

--- frontend/src/components/_admin/ui/Modal/Orders/EditOrder.jsx
+++ frontend/src/components/_admin/ui/Modal/Orders/EditOrder.jsx
@@ -24,10 +24,11 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { schema } from "../../../../../schemas/orders";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useOrdersApi } from "../../../../../hooks/orders/useOrdersApi";
+import LoadingSpinner from "../../../Loader/LoadingSpinner";
 const EditOrder = ({ getOrderDetails, onClose }) => {
   console.log(getOrderDetails);
   const queryClient = useQueryClient();
-  const { updateOrder } = useOrdersApi();
+  const { updateOrder, error, isLoading } = useOrdersApi();
 
   const {
     register,
@@ -44,6 +45,7 @@ const EditOrder = ({ getOrderDetails, onClose }) => {
     mode: "all",
   });
 
+
   useEffect(() => {
     reset({
       ...getOrderDetails,
@@ -59,16 +61,34 @@ const EditOrder = ({ getOrderDetails, onClose }) => {
       queryClient.invalidateQueries(["orders"]); // invalidate books query to refetch
       onClose();
     },
+    onError: () => {
+      onClose();
+    }
   });
 
-  console.log(isValid, "Form valid");
-  console.log(errors, "Form errors");
+
+
+
+  // console.log(isValid, "Form valid");
+  // console.log(errors, "Form errors");
   const onSubmit = async (updateData) => {
     console.log(updateData, "Form");
-    const updateFormData = { ...updateData, id: getOrderDetails?.id };
+    const updateFormData = { ...updateData, items: getOrderDetails?.items, id: getOrderDetails?.id };
     await updateOrderMutation(updateFormData);
   };
 
+
+
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      if (error) {
+        console.log(error)
+      } else {
+        console.log("Updated Successfully");
+
+      }
+    }
+  }, [isSubmitSuccessful, error])
   return (
     <div className="fixed left-0 top-0  inset-0 bg-[#64748B] bg-opacity-75 transition-opacity dark:bg-slate-300 dark:bg-opacity-75 lg:left-[18rem]">
       <div className="relative p-5 rounded-md">
@@ -88,13 +108,13 @@ const EditOrder = ({ getOrderDetails, onClose }) => {
         {/* my-5 rounded-md flex justify-center items-center z-70  overflow-hidden xs:h-[400px] overflow-y-auto */}
         <div className=" md:mx-20">
           <div className=" p-10 relative rounded-md border border-[#E2E8F0] bg-white shadow-lg dark:border-[#2E3A47] dark:bg-[#24303F] md:px-8 md:py-8 ">
-            <div className=" flex justify-between items-center rounded-sm p-3 bg-slate-100 border border-[#E2E8F0] py-4 px-6.5 dark:border-[#2E3A47]">
+            <div className=" flex justify-between items-center rounded-sm p-3 bg-slate-100 border border-[#E2E8F0] py-4 px-6.5 dark:border-[#2E3A47] dark:bg-[#2E3A47]">
               <h3 className="font-bold text-[#313D4A] dark:text-white">
                 Update Order Details
               </h3>
               <h5 className="flex justify-start items-center gap-2 text-md font-semibold text-slate-400">
                 Order Id:{" "}
-                <span className="text-slate-700 font-medium text-md">
+                <span className="text-slate-700  dark:text-neutral-100 font-medium text-md">
                   {getOrderDetails?.id}
                 </span>
               </h5>
@@ -106,7 +126,7 @@ const EditOrder = ({ getOrderDetails, onClose }) => {
               <form onSubmit={handleSubmit(onSubmit)}>
                 {/* Order details */}
                 <div className="flex flex-col md:gap-5 my-5 ">
-                  <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-400 mb-3 ">
+                  <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-500 mb-3 ">
                     <MdEmail />
                     Email Address
                   </h5>
@@ -123,32 +143,32 @@ const EditOrder = ({ getOrderDetails, onClose }) => {
                   {/* </span> */}
                 </div>
 
-                  {/* Order Created Details  */}
-              <p className="mt-7 text-lg font-semibold text-slate-600 border-b border-blue-100 mb-4">
-                Ordered Items List
-              </p>
-              <div className="flex flex-col justify-start items-start">
-                <div className="flex flex-col mb-5">
-                  <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-400 mb-3 ">
-                    <MdOutlineBook />
-                    Items
-                  </h5>
-                  <ul className="ml-10 font-medium text-md text-slate-400 grid grid-cols-1 gap-x-8 md:grid-cols-2 md:gap-12">
-                    {getOrderDetails?.items?.map((book, index) => (
-                      <li className="" key={book.book_id}>
-                        <span className="font-semibold flex items-center gap-2 text-emerald-900">
-                          <MdShoppingBag />
-                          Order Item {index + 1}{" "}
-                        </span>
+                {/* Order Created Details  */}
+                <p className="mt-7 text-lg font-semibold text-slate-600 border-b border-blue-100 mb-4">
+                  Ordered Items List
+                </p>
+                <div className="flex flex-col justify-start items-start">
+                  <div className="flex flex-col mb-5">
+                    <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-500 mb-3 ">
+                      <MdOutlineBook />
+                      Items
+                    </h5>
+                    <ul className="ml-10 font-medium text-md text-slate-400 grid grid-cols-1 gap-x-8 md:grid-cols-2 md:gap-12">
+                      {getOrderDetails && getOrderDetails?.items?.map((book, index) => (
+                        <li className="" key={book?.book_id}>
+                          <span className="font-semibold flex items-center gap-2 text-slate-500 dark:text-blue-600">
+                            <MdShoppingBag />
+                            Order Item {index + 1}{" "}
+                          </span>
 
-                        <p className="text-blue-400 ml-8 mt-2 text-lg">
-                          {book?.book_title}
-                        </p>
-                      </li>
-                    ))}
-                  </ul>
+                          <p className="text-blue-500 ml-8 mt-2 text-lg">
+                            {book?.book_title}
+                          </p>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
                 </div>
-              </div>
 
                 {/* Order Created Details  */}
                 <p className="mt-10 text-lg font-semibold text-slate-600 border-b-2 w-2/3 border-blue-100 mb-4">
@@ -156,7 +176,7 @@ const EditOrder = ({ getOrderDetails, onClose }) => {
                 </p>
                 <div className="flex flex-col justify-start items-start">
                   <div className="flex flex-col mb-5">
-                    <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-400 mb-3 ">
+                    <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-500 mb-3 ">
                       <MdOutlinePerson />
                       Order Placed by
                     </h5>
@@ -170,7 +190,7 @@ const EditOrder = ({ getOrderDetails, onClose }) => {
                     {/* </p> */}
                   </div>
                   <div className="flex flex-col mb-5">
-                    <h5 className="flex justify-start items-start gap-2 text-md font-normal text-blue-400 mb-3 ">
+                    <h5 className="flex justify-start items-start gap-2 text-md font-normal text-blue-500 mb-3 ">
                       <MdOutlineCalendarMonth />
                       Created On
                     </h5>
@@ -178,6 +198,7 @@ const EditOrder = ({ getOrderDetails, onClose }) => {
                     <input
                       type="text"
                       name="order_on"
+                      disabled
                       {...register("order_on")}
                       className="w-full md:w-[400px] md:ml-10  rounded-sm border border-[#E2E8F0] bg-transparent py-3 px-5 font-medium text-slate-400 outline-none transition focus:border-[#3C50E0] active:border-[#3C50E0] disabled:cursor-default disabled:bg-[#F5F7FD] dark:border-form-[#3d4d60] dark:bg-[#1d2a39] dark:focus:border-[#3C50E0]"
                     />
@@ -192,7 +213,7 @@ const EditOrder = ({ getOrderDetails, onClose }) => {
 
                 <div className="flex flex-col gap-5 md:flex-row md:gap-26 md:items-center lg:gap-32">
                   <div className="flex flex-col md:mb-5">
-                    <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-400 mb-3 ">
+                    <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-500 mb-3 ">
                       <MdCurrencyExchange /> Payment ID
                     </h5>
                     <input
@@ -207,7 +228,7 @@ const EditOrder = ({ getOrderDetails, onClose }) => {
                   </div>
 
                   <div className="flex-flex-col mb-5">
-                    <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-400 mb-3  ">
+                    <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-500 mb-3  ">
                       <BsCreditCard2BackFill /> Payment Mode
                     </h5>
 
@@ -230,7 +251,7 @@ const EditOrder = ({ getOrderDetails, onClose }) => {
                 </p>
                 <div className="flex flex-col  gap-5 md:flex-row md:gap-22 lg:gap-32">
                   <div className="flex flex-col mb-5">
-                    <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-400 mb-3 ">
+                    <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-500 mb-3 ">
                       <BiSolidDiscount />
                       Discount Code
                     </h5>
@@ -246,7 +267,7 @@ const EditOrder = ({ getOrderDetails, onClose }) => {
                   </div>
 
                   <div className="flex-flex-col mb-5">
-                    <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-400 mb-3  ">
+                    <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-500 mb-3  ">
                       <LiaMoneyCheckAltSolid />
                       Discount Value
                     </h5>
@@ -272,7 +293,7 @@ const EditOrder = ({ getOrderDetails, onClose }) => {
                     <RiUserLocationFill />
                     Customer Info
                   </p>
-                  <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-400 mb-3 ">
+                  <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-500 mb-3 ">
                     <IoLocation /> Address
                   </h5>
                   {/* <p className="ml-10  text-md font-medium text-slate-400">
@@ -284,7 +305,7 @@ const EditOrder = ({ getOrderDetails, onClose }) => {
                     {...register("address")}
                     className="w-full md:w-[400px] md:ml-10  rounded-sm border border-[#E2E8F0] bg-transparent py-3 px-5 font-medium text-slate-400 outline-none transition focus:border-[#3C50E0] active:border-[#3C50E0] disabled:cursor-default disabled:bg-[#F5F7FD] dark:border-form-[#3d4d60] dark:bg-[#1d2a39] dark:focus:border-[#3C50E0]"
                   />
-                  <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-400 mb-3 ">
+                  <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-500 mb-3 ">
                     {" "}
                     <MdLocationCity />
                     City
@@ -298,7 +319,7 @@ const EditOrder = ({ getOrderDetails, onClose }) => {
                     {...register("city")}
                     className="w-full md:w-[400px] md:ml-10  rounded-sm border border-[#E2E8F0] bg-transparent py-3 px-5 font-medium text-slate-400 outline-none transition focus:border-[#3C50E0] active:border-[#3C50E0] disabled:cursor-default disabled:bg-[#F5F7FD] dark:border-form-[#3d4d60] dark:bg-[#1d2a39] dark:focus:border-[#3C50E0]"
                   />
-                  <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-400 mb-3 ">
+                  <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-500 mb-3 ">
                     <RiFlag2Fill />
                     Country
                   </h5>{" "}
@@ -311,7 +332,7 @@ const EditOrder = ({ getOrderDetails, onClose }) => {
                     {...register("phone")}
                     className="w-full md:w-[400px] md:ml-10  rounded-sm border border-[#E2E8F0] bg-transparent py-3 px-5 font-medium text-slate-400 outline-none transition focus:border-[#3C50E0] active:border-[#3C50E0] disabled:cursor-default disabled:bg-[#F5F7FD] dark:border-form-[#3d4d60] dark:bg-[#1d2a39] dark:focus:border-[#3C50E0]"
                   />
-                  <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-400 mb-3">
+                  <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-500 mb-3">
                     <MdLocalPhone />
                     Phone Number
                   </h5>{" "}
@@ -333,7 +354,7 @@ const EditOrder = ({ getOrderDetails, onClose }) => {
                     <MdLocalAirport />
                     Shipping Details
                   </p>
-                  <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-400 mb-3 ">
+                  <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-500 mb-3 ">
                     <GrMapLocation />
                     Adress
                   </h5>
@@ -346,7 +367,7 @@ const EditOrder = ({ getOrderDetails, onClose }) => {
                     {...register("shipping_address")}
                     className="w-full md:w-[400px] md:ml-10  rounded-sm border border-[#E2E8F0] bg-transparent py-3 px-5 font-medium text-slate-400 outline-none transition focus:border-[#3C50E0] active:border-[#3C50E0] disabled:cursor-default disabled:bg-[#F5F7FD] dark:border-form-[#3d4d60] dark:bg-[#1d2a39] dark:focus:border-[#3C50E0]"
                   />
-                  <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-400 mb-3 ">
+                  <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-500 mb-3 ">
                     {" "}
                     <FaArrowRightToCity />
                     City
@@ -360,7 +381,7 @@ const EditOrder = ({ getOrderDetails, onClose }) => {
                     {...register("shipping_city")}
                     className="w-full md:w-[400px] md:ml-10  rounded-sm border border-[#E2E8F0] bg-transparent py-3 px-5 font-medium text-slate-400 outline-none transition focus:border-[#3C50E0] active:border-[#3C50E0] disabled:cursor-default disabled:bg-[#F5F7FD] dark:border-form-[#3d4d60] dark:bg-[#1d2a39] dark:focus:border-[#3C50E0]"
                   />
-                  <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-400 mb-3 ">
+                  <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-500 mb-3 ">
                     <FaFlag />
                     Country
                   </h5>{" "}
@@ -373,7 +394,7 @@ const EditOrder = ({ getOrderDetails, onClose }) => {
                     {...register("shipping_country")}
                     className="w-full md:w-[400px] md:ml-10  rounded-sm border border-[#E2E8F0] bg-transparent py-3 px-5 font-medium text-slate-400 outline-none transition focus:border-[#3C50E0] active:border-[#3C50E0] disabled:cursor-default disabled:bg-[#F5F7FD] dark:border-form-[#3d4d60] dark:bg-[#1d2a39] dark:focus:border-[#3C50E0]"
                   />
-                  <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-400 mb-3">
+                  <h5 className="flex justify-start items-center gap-2 text-md font-normal text-blue-500 mb-3">
                     <MdOutlineLocalPhone />
                     Phone Number
                   </h5>{" "}
@@ -397,11 +418,11 @@ const EditOrder = ({ getOrderDetails, onClose }) => {
                     Cancel
                   </button>
                   <button
-                    disabled={!isValid || isSubmitting}
+                    disabled={!isDirty || isSubmitting || isLoading}
                     type="submit"
                     className="bg-orange-400 text-white font-medium text-md cursor-pointer p-2 px-5 rounded-md "
                   >
-                    Update
+                    {isLoading ? <LoadingSpinner /> : "Update"}
                   </button>
                 </div>
               </form>
