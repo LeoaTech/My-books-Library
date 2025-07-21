93f8fc04652af16bbcad243a18399674ca2e6335
razaakomal <komal.raza@leoatech.com>
2025-07-21 17:45:00 +0500

    LIB-240 db migration: removed not null tracking_id reference from orders table

---
+++ backend/migrations/1753100003912_removed_tracking_id_notnull_reference_in_orders_table.js
@@ -0,0 +1,30 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+
+const up = (pgm) => {
+  pgm.alterColumn("orders", "tracking_id", {
+    notNull: false,
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+const down = (pgm) => {
+    pgm.alterColumn("orders", "tracking_id", {
+    notNull: true,
+  });
+};
+
+
+module.exports={up,down, shorthands}
