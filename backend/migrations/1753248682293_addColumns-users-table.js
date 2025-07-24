5b643b4b8817d29a4b93588440049a33c6dd9fad
razaakomal <komal.raza@leoatech.com>
2025-07-24 10:47:22 +0500

    LIB-250 alter users table migration for adding columns

---
+++ backend/migrations/1753248682293_addColumns-users-table.js
@@ -0,0 +1,29 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+const up = (pgm) => {
+  pgm.addColumns("users", {
+    city: { type: "text" },
+    country: { type: "text" },
+    user_details: { type: "json" },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+const down = (pgm) => {
+  pgm.dropColumns("users", ["city", "country", "user_details"]);
+};
+
+
+module.exports ={up,down, shorthands}
