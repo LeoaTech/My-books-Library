2f4f485d2fbc7cada546fde9eca55c23bf9499ae
razaakomal <komal.raza@leoatech.com>
2025-07-28 13:33:06 +0500

    LIB-249-[frontend]: updated the validation schema for order details Form

--- frontend/src/schemas/orders.js
+++ frontend/src/schemas/orders.js
@@ -1,21 +1,8 @@
 import * as z from "zod";
 
-
 export const schema = z.object({
-    name: z.string().min(3, { message: "Please Enter a name" }),
-    email: z.string().email({message: "Please Enter Email"}),
-    order_by: z.string().min(1, { message: "Please Enter customer name" }),
-    order_on: z.string(),
-    payment_id: z.string(),
-    mode_of_payment: z.string(),
-    discount_code: z.string(),
-    discount_value: z.number(),
-    address: z.string(),
-    city: z.string(),
-    country: z.string(),
-    phone: z.string(),
-    shipping_address: z.string(),
-    shipping_city: z.string(),
-    shipping_country: z.string(),
-    shipping_phone: z.string(),
-  });
+  shipping_address: z.string(),
+  shipping_city: z.string(),
+  shipping_country: z.string(),
+  shipping_phone: z.string(),
+});
