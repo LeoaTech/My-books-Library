8482c9e16ba79a7af7d963034828cee1618f85b5
razaakomal <komal.raza@leoatech.com>
2025-07-29 10:27:24 +0500

    LIB-250[database]: update users table - to alter column datatype for address

---
+++ backend/migrations/1753341441050_alterColumn-datatype-users-table.js
@@ -0,0 +1,30 @@
+
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+const up = (pgm) => {
+  pgm.alterColumn("users", "address", {
+    type: "text",
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+const down = (pgm) => {
+    pgm.alterColumn("users", "address", {
+    type:"json"
+  });
+};
+
+
+module.exports = {up,down,shorthands}
