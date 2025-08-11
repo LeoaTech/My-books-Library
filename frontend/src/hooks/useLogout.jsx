219ca0d7867c48e636b459ee5e122bea5f8bc0b4
razaakomal <komal.raza@leoatech.com>
2025-08-11 16:41:26 +0500

    LIB-257-[frontend]: updated the logout method to remove the auth details and display a message for logout action

--- frontend/src/hooks/useLogout.jsx
+++ frontend/src/hooks/useLogout.jsx
@@ -2,6 +2,25 @@ import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "./useAuthContext";
 import { BASE_URL } from "../utiliz/baseAPIURL";
 import axios from "axios";
+import { toast } from "react-toastify";
+const LogoutFailed = () => toast.error("Failed to Logout Try again", {
+  hideProgressBar: true,
+  closeOnClick: false,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: "dark",
+})
+
+const LogoutSuccess = () => toast.success("Logout Successfully", {
+  hideProgressBar: true,
+  closeOnClick: false,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: "dark",
+})
+const serverUrl = import.meta.env.VITE_SERVER_ENDPOINT;
 
 export const useLogout = () => {
   const { dispatch, setAuth } = useAuthContext();
@@ -13,23 +32,39 @@ export const useLogout = () => {
       withCredentials: true,
     });
 
-    setAuth({});
+    setAuth(null);
+    LogoutSuccess();
+    localStorage.removeItem('user');
+    localStorage.setItem('persist', false)
+    // localStorage.setItem("auth-source", false);
+    localStorage.setItem("color-theme", "light")
+    localStorage.removeItem("auth-source");
+    delete axios.defaults.headers.common['Authorization'];
+
     dispatch({ type: "Logout" });
     navigate("/");
   };
 
   // Google Auth Signout Method
   const signout = async () => {
-    let res = await axios.get(`${BASE_URL}/auth/logout`, {
+    let res = await axios.get(`${serverUrl}/auth/logout`, {
       withCredentials: true,
     });
-    if (res.status === 200) {
-      dispatch({ type: "Logout" });
-      localStorage.removeItem("google-auth");
 
-      setAuth({});
+    
+    if (res.status === 200) {
+      LogoutSuccess();
+      dispatch({ type: "Logout" });
+
+      // localStorage.removeItem("google-auth");
+      localStorage.removeItem("auth-source");
+      localStorage.removeItem('user');
+      localStorage.setItem('persist', false)
+      setAuth(null);
       navigate("/");
+      delete axios.defaults.headers.common['Authorization'];
     } else {
+      LogoutFailed()
       console.log("logout failed");
     }
   };
