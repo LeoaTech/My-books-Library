9609a5e2205d260ab86e507900bdda95bb8d4f19
razaakomal <komal.raza@leoatech.com>
2025-08-11 10:52:13 +0500

    LIB-262: Users can create new library with their google account

--- backend/controllers/AuthController/GoogleAuth.js
+++ backend/controllers/AuthController/GoogleAuth.js
@@ -1,6 +1,13 @@
 const passport = require("passport");
 const db = require("../../config/dbConfig");
 const GoogleStrategy = require("passport-google-oauth2").Strategy;
+const {
+  createEntity,
+  createRole,
+  createBranch,
+  createUser,
+} = require("../../helpers/user_onboarding.js");
+const { pool } = require("../../config/dbConfig.js");
 
 passport.use(
   new GoogleStrategy(
@@ -12,73 +19,137 @@ passport.use(
     },
 
     async (request, accessToken, refreshToken, profile, done) => {
+      const client = await pool.connect();
+
       try {
-        console.log(accessToken, "USER PROFILE", refreshToken, "Tokens");
+        console.log(profile, "USER PROFILE", refreshToken, "Tokens");
+        await client.query("BEGIN");
 
+        if (!profile.email || !profile.displayName) {
+          return done(new Error("Invalid Google profile data"), null);
+        }
         // Check if the user already exists in the database
-        const query = "SELECT * FROM users WHERE email= $1";
-        const result = await db.query(query, [profile?.email]);
+        const query = `
+        SELECT 
+        u.id, u.email,u.name, u.password, u.branch_id, u.role_id,
+        b.entity_id , b.name AS branch_name,
+        e.name AS entity_name,
+        r.name AS role_name
+      FROM users u
+      JOIN branches b ON u.branch_id = b.id
+      JOIN entity e  ON b.entity_id  = e.id
+      JOIN roles r ON u.role_id = r.role_id
+      WHERE u.email = $1
+        `;
+        const result = await db.query(query, [profile.email]);
 
+        let user;
         if (result?.rows?.length > 0) {
-          // User already exists, return the user
-          return done(null, result?.rows[0]);
+          user = result.rows[0].id;
+        } else {
+          /* Create an Entity  */
+
+          const entityData = {
+            businessName: profile?.displayName + "-library",
+            city: "",
+            country: "",
+            address: "",
+            city: "",
+            phone: "",
+            description: "",
+            typeOfBooks: "",
+            hasMultipleBranches: false,
+            deliverIntercity: false,
+          };
+
+          /* Create an Organization */
+          const entityId = await createEntity(client, entityData);
+          console.log(entityId, "entity Created");
+
+          const branch = {
+            businessName: profile.displayName + "-branch",
+            city: "",
+            country: "",
+            address: "",
+            city: "",
+            phone: "",
+            entityId,
+          };
+          const branchId = await createBranch(client, branch, entityId);
+          console.log(branchId, "Branch Created");
+
+          /* Create Role for this Entity ID */
+          const roleId = await createRole(client, entityId);
+          console.log(roleId, "Role Created");
+
+          /* Create new user  */
+          const userData = {
+            fullName: profile.displayName,
+            email: profile.email,
+            hashedPassword: "",
+            city: "",
+            country: "",
+            address: "",
+            phone: "",
+            roleId,
+            img_url: profile?.picture || profile.photos?.[0]?.value,
+          };
+          // User doesn't exist, create a new user
+          const userId = await createUser(client, userData, branchId);
+          console.log(userId, "User Created");
+          await client.query("COMMIT");
+
+          user = userId;
+
+          //  return the new user
+          return done(null, user);
         }
-
-        const checkRole = await db.query(
-          "SELECT role_id FROM roles Where name=  $1",
-          ["member" || "Member" || "MEMBER"]
-        );
-
-        let role;
-        if (checkRole?.rowCount > 0) {
-          role = checkRole?.rows[0]?.role_id;
-        }
-
-        // User doesn't exist, create a new user
-        const insertQuery =
-          "INSERT INTO users ( name, password,role_id, email,img_url  ) VALUES ($1, $2,$3,$4,$5) RETURNING *";
-        const insertedUser = await db.query(insertQuery, [
-          profile?.displayName,
-          "",
-          role,
-          profile?.email,
-          profile?.picture,
-        ]);
-
-        console.log(insertedUser?.rowCount, "Google Login inserted data");
-
-        let signedInUser = { ...insertedUser?.rows[0] };
-        // Return the new user
-        return done(null, signedInUser);
+        // Return the existing user
+        return done(null, user);
       } catch (err) {
         // Handle errors
+        await client.query("ROLLBACK");
         console.log(err, "Google login error");
         return done(err, null);
+      } finally {
+        client.release();
       }
     }
   )
 );
 
 passport.serializeUser((user, done) => {
-  done(null, user);
+  console.log("Serilaze user", user);
+
+  done(null, user); //store only user_id in the session
 });
 
 passport.deserializeUser(async (user, done) => {
-  // try {
-  //   const query = "SELECT * FROM users WHERE email= $1";
-  //   const result = await db.query(query, [user?.email]);
+  // console.log(user, "deserialize User");
 
-  //   // console.log(result, "Deserialized user");
-  //   if (result?.rows?.length > 0) {
-  //     // User already exists, return the user
-  //     return done(null, result?.rows[0]);
-  //   }
-  // } catch (error) {
-  //   return done(error, null);
-  // }
+  try {
+    const query = `SELECT 
+        u.id, u.email,u.name, u.password, u.branch_id, u.role_id,
+        b.entity_id , b.name AS branch_name,
+        e.name AS entity_name,
+        r.name AS role_name
+      FROM users u
+      JOIN branches b ON u.branch_id = b.id
+      JOIN entity e  ON b.entity_id  = e.id
+      JOIN roles r ON u.role_id = r.role_id
+      WHERE u.id= $1`;
+    const result = await db.query(query, [user]);
 
-  console.log(user, "Deserializing user");
-  done(null, user);
+    console.log(result.rows[0], "Deserialized user");
+    if (result?.rows?.length > 0) {
+      // User already exists, return the user
+      return done(null, result?.rows[0]);
+    }
+
+    return done("User not found", null);
+  } catch (error) {
+    return done(error, null);
+  }
 });
 
 module.exports = passport;
