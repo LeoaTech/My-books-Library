e39f779f12cb28a04426b6fd6a47e639b18fba5b
razaakomal <komal.raza@leoatech.com>
2025-08-20 18:50:14 +0500

    feat: implement google authentication handling in App component and enhance navigation flow

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,55 @@
+import Home from "./_root/pages/Home";
 import "./App.css";
+import { ToastContainer } from 'react-toastify';
+
 import Navbar from "./components/_user/Navbar/Navbar";
+import {  useNavigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
+import { useEffect } from "react";
 function App() {
+  const { auth, googleAuth, getUser } = useAuthContext();
+  const navigate = useNavigate();
+  
+
+  // console.log(googleAuth," Source of Auth");
+
+
+  useEffect(() => {
+    const handleAuth = async () => {
+      const redirectUrl = await getUser();
+      navigate(redirectUrl, { replace: true });
+    };
+
+    if (googleAuth != "email") {
+      handleAuth();
+    // } else {
+    //   console.log("No need to fetch google auth result");
+
+    }
+  }, [getUser, navigate, googleAuth]);
+
+
+  console.log("Auth in main app", auth);
   return (
     <>
       <Navbar />
+      {/* <Home /> */}
+      <h2 className="mt-20 flex justify-center items-center">Landing Page</h2>
+
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        theme="dark"
+      />
     </>
-  );
+  )
 }
 
-export const Header = () => {
-  return <nav>Header</nav>;
-};
 
 export default App;
