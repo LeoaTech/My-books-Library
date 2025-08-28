e13516e62cb70e96488905e539d8748f4ca59177
razaakomal <komal.raza@leoatech.com>
2025-08-28 07:57:38 +0500

    LIB-284-[frontend]- added a custom hook to add new cover type from creatable select

--- frontend/src/hooks/books/useFetchCovers.jsx
+++ frontend/src/hooks/books/useFetchCovers.jsx
@@ -1,5 +1,43 @@
 import { useQuery } from "@tanstack/react-query";
 import { BASE_URL } from "../../utiliz/baseAPIURL";
+import { useState } from "react";
+
+export const useCoverActions = () => {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+  const [message, setMessage] = useState(null);
+
+  // Create New Cover
+  const addCover = async (data) => {
+    setIsLoading(true);
+    setError(null);
+
+    // console.log(data, "Cover name");
+    
+    try {
+      const response = await fetch(`${BASE_URL}/covers/new`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+        body: JSON.stringify({name:data}),
+      });
+
+      const result = await response.json(); //response?.data;
+      console.log(result, "Cover Save Result");
+      setError(null);
+      setMessage(result.message);
+      return result;
+    } catch (error) {
+      console.log(error);
+      setError(error.message);
+    } finally {
+      setIsLoading(false)
+    }
+  };
+
+
+  return { isLoading, error, message, addCover };
+};
 
 // API CALL to FETCH TYPES OF BOOK COVERS
 
