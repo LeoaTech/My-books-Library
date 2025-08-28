b8f8320e12b0134d4f9ff2019be150a5db11e663
razaakomal <komal.raza@leoatech.com>
2025-08-28 08:19:55 +0500

    LIB-131-[backend]- refactor fetch covers api to fetch covers for a specific library

--- backend/controllers/Cover.Controller.js
+++ backend/controllers/Cover.Controller.js
@@ -4,9 +4,14 @@ const db = require("../config/dbConfig");
 /* Get ALL Cover types */
 
 const getBookCovers = asyncHanlder(async (req, res) => {
+  const entityId = req?.user?.entityId || req?.user?.entity_id;
+
+  if (!entityId) {
+    return res.status(403).json("Invalid Request, No Library ID provided");
+  }
   try {
-    const bookCoversQuery = `SELECT * FROM covers`;
-    const getAllCovers = await db.query(bookCoversQuery);
+    const bookCoversQuery = `SELECT * FROM covers where entity_id =$1`;
+    const getAllCovers = await db.query(bookCoversQuery, [entityId]);
 
     res.status(200).json({
       covers: getAllCovers?.rows,
@@ -16,5 +21,7 @@ const getBookCovers = asyncHanlder(async (req, res) => {
     console.log(error);
   }
 });
+
+
 
 module.exports = { getBookCovers };
