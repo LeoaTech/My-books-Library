d3f74ac1bcecc87ef8872a6c4e34e71d1816e220
razaakomal <komal.raza@leoatech.com>
2025-08-28 08:03:10 +0500

    LIB-128-[backend]-ehance author api to fetch or create new author for a specific entityId

--- backend/controllers/Authors.Controller.js
+++ backend/controllers/Authors.Controller.js
@@ -1,11 +1,16 @@
 const asyncHanlder = require("express-async-handler");
-const db = require("../config/dbConfig")
+const db = require("../config/dbConfig");
 
 /* Get ALL Authors */
 const FetchAllAuthors = asyncHanlder(async (req, res) => {
+  const entityId = req?.user?.entityId || req?.user?.entity_id;
+
+  if (!entityId) {
+    return res.status(403).json("Invalid Request, No Library ID provided");
+  }
   try {
-    const AuthorsQuery = `SELECT * FROM authors`;
-    const getAllAuthors = await db.query(AuthorsQuery);
+    const AuthorsQuery = `SELECT * FROM authors WHERE entity_id =$1`;
+    const getAllAuthors = await db.query(AuthorsQuery, [entityId]);
 
     res.status(200).json({
       authors: getAllAuthors?.rows,
@@ -20,25 +25,41 @@ const FetchAllAuthors = asyncHanlder(async (req, res) => {
 
 const AddNewAuthor = asyncHanlder(async (req, res) => {
   console.log(req.body);
+
+  const entityId = req?.user?.entityId || req?.user?.entity_id;
+
+  if (!entityId) {
+    return res.status(403).json("Invalid Request, No Library ID provided");
+  }
   try {
+    if (!req.body.authorsForm) {
+      return res.status(400).json("Invalid Authors Details");
+    }
+
+    const { name, links, description } = req.body.authorsForm;
+
     const createAuthorQuery = await db.query(
-      `INSERT INTO authors (name) VALUES ($1)`,
-      [req.body.name]
+      `INSERT INTO authors (name, links, description, entity_id) VALUES ($1,$2,$3, $4)`,
+      [name, links, description, entityId]
     );
 
-    console.log(createAuthorQuery?.rows[0], "Authors /founded");
+    console.log(createAuthorQuery?.rows[0], "Authors Saved");
 
     res.status(200).json({
       authors: createAuthorQuery?.rows[0],
-      message: "Authors Found ",
+      message: "Authors Saved Successfully ",
     });
   } catch (error) {
     console.log(error, "Error creating new author");
+    res.status(500).json({
+      error,
+      message: error.message || "Error Creating Authors",
+    });
   }
 });
 
-/* Fetch Authors By ID */
 
-/* Fetch All Authors by Book ID */
+
+
 
 module.exports = { FetchAllAuthors, AddNewAuthor };
--- backend/routes/AuthorsRoutes.js
+++ backend/routes/AuthorsRoutes.js
@@ -1,21 +1,25 @@
 const express = require("express");
 const {
   FetchAllAuthors,
-  AddNewAuthor,
+  AddNewAuthor
 } = require("../controllers/Authors.Controller");
+const { checkAuth } = require("../middleware/authMiddleware");
 
 const router = express.Router();
+// Verify User Authentication (Logged-in users can see this route)
+router.use(checkAuth);
 
+//Get: fetch Authors List
 router.get("/", FetchAllAuthors);
 
-// Add New Author
+// Post: Add New Author
 router.post("/new", AddNewAuthor);
 
-// Update Author
+// PUT:Update Author details
 
 router.put("/update/:author_id");
 
-// Delete Author
+// DeleteL remove Author
 
 router.delete("/remove/:author_id");
 
