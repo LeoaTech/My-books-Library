29550fbdbb6f96d3d7f475305ecc4dd8a56b55fb
razaakomal <komal.raza@leoatech.com>
2025-08-28 08:31:19 +0500

    LIB-147-[backend]- enhance create new publisher api to save publisher details for a specific library

--- backend/controllers/Publishers.Controller.js
+++ backend/controllers/Publishers.Controller.js
@@ -21,7 +21,44 @@ const FetchPublishers = asyncHanlder(async (req, res) => {
   }
 });
 
+/* Create New Publisher */
+
+const AddNewPublisher = asyncHanlder(async (req, res) => {
+  console.log(req.body);
+
+  const entityId = req?.user?.entityId || req?.user?.entity_id;
+
+  if (!entityId) {
+    return res.status(403).json("Invalid Request, No Library ID provided");
+  }
+  try {
+    if (!req.body.publishersForm) {
+      return res.status(400).json("Invalid Publisher Details");
+    }
+
+    const { name, links, description } = req.body.publishersForm;
+
+    const createPublisherQuery = await db.query(
+      `INSERT INTO publishers (name, links,description, entity_id) VALUES ($1, $2,$3, $4)`,
+      [name, links, description, entityId]
+    );
+
+    console.log(createPublisherQuery?.rows[0], "Publishers Added ");
+
+    res.status(200).json({
+      publishers: createPublisherQuery?.rows[0],
+      message: "Publishers Found ",
+    });
+  } catch (error) {
+    console.log(error, "Error creating new Publisher");
+    res.status(500).json({
+      error,
+      message: error.message || "Error Creating Publishers",
+    });
+  }
+});
 
 
 
-module.exports = { FetchPublishers };
+
+module.exports = { FetchPublishers ,AddNewPublisher};
--- backend/routes/PublishersRoutes.js
+++ backend/routes/PublishersRoutes.js
@@ -3,12 +3,22 @@ const {
   FetchPublishers,
   AddNewPublisher,
 } = require("../controllers/Publishers.Controller");
+const { checkAuth } = require("../middleware/authMiddleware");
 
 const router = express.Router();
+router.use(checkAuth);
 
 router.get("/", FetchPublishers);
 
 // Add New Publisher
 router.post("/new", AddNewPublisher);
+
+// PUT:Update Publisher details
+
+router.put("/update/:publisher_id");
+
+// DeleteL remove Publisher
+
+router.delete("/remove/:publisher_id");
 
 module.exports = router;
