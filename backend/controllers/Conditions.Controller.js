eed892d8646fe50b16cdac55ae9d038445c847c4
razaakomal <komal.raza@leoatech.com>
2025-08-28 08:26:35 +0500

    LIB-130-[backend]- refactor fetch conditions api to fetch conditions for a specific library

--- backend/controllers/Conditions.Controller.js
+++ backend/controllers/Conditions.Controller.js
@@ -4,9 +4,14 @@ const db = require("../config/dbConfig");
 /* Get ALL Conditions */
 
 const GetBookCondition = asyncHanlder(async (req, res) => {
+  const entityId = req?.user?.entityId || req?.user?.entity_id;
+
+  if (!entityId) {
+    return res.status(403).json("Invalid Request, No Library ID provided");
+  }
   try {
-    const bookConditionQuery = `SELECT * FROM conditions`;
-    const getAllCondition = await db.query(bookConditionQuery);
+    const bookConditionQuery = `SELECT * FROM conditions where entity_id=$1`;
+    const getAllCondition = await db.query(bookConditionQuery, [entityId]);
 
     res.status(200).json({
       conditions: getAllCondition?.rows,
@@ -16,5 +21,9 @@ const GetBookCondition = asyncHanlder(async (req, res) => {
     console.log(error);
   }
 });
+
+
+
+
 
 module.exports = { GetBookCondition };
