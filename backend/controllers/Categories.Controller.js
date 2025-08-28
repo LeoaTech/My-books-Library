d1bcb3bec5d82b3a161f699109cc4e0821802497
razaakomal <komal.raza@leoatech.com>
2025-08-28 08:14:24 +0500

    LIB-129-[backend]-fetch categories  for a specific library

--- backend/controllers/Categories.Controller.js
+++ backend/controllers/Categories.Controller.js
@@ -3,9 +3,15 @@ const db = require("../config/dbConfig");
 
 /* Get ALL Categories */
 const GetCategories = asyncHanlder(async (req, res) => {
+  const entityId = req?.user?.entityId || req?.user?.entity_id;
+
+  if (!entityId) {
+    return res.status(403).json("Invalid Request, No Library ID provided");
+  }
+
   try {
-    const categoriesQuery = `SELECT * FROM categories`;
-    const getAllCategories = await db.query(categoriesQuery);
+    const categoriesQuery = `SELECT * FROM categories WHERE entity_id =$1`;
+    const getAllCategories = await db.query(categoriesQuery, [entityId]);
 
     res.status(200).json({
       categories: getAllCategories?.rows,
@@ -15,5 +21,7 @@ const GetCategories = asyncHanlder(async (req, res) => {
     console.log(error);
   }
 });
+
+
 
 module.exports = { GetCategories };
