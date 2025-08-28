1594461a38634988b907fd8e0a452927f6c32ec8
razaakomal <komal.raza@leoatech.com>
2025-08-28 07:56:59 +0500

    LIB-284-[frontend]- added a custom hook to add new conditions from creatable select

--- frontend/src/hooks/books/useFetchConditions.jsx
+++ frontend/src/hooks/books/useFetchConditions.jsx
@@ -1,5 +1,44 @@
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import { BASE_URL } from "../../utiliz/baseAPIURL";
+
+export const useConditionActions = () => {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+  const [message, setMessage] = useState(null);
+
+  // Create New Condition
+  const addConditionType = async (data) => {
+    setIsLoading(true);
+    setError(null);
+
+    console.log(data, "Condition name");
+    
+    try {
+      const response = await fetch(`${BASE_URL}/conditions/new`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+        body: JSON.stringify({name:data}),
+      });
+
+      const result = await response.json(); //response?.data;
+      console.log(result, "Condition Save Result");
+      setError(null);
+      setMessage(result.message);
+      return result;
+    } catch (error) {
+      console.log(error);
+      setError(error.message);
+    } finally {
+      setIsLoading(false)
+    }
+  };
+
+
+  return { isLoading, error, message, addConditionType };
+};
+
 
 // API CALL to FETCH CONDITIONS TYPES FOR BOOKS
 
