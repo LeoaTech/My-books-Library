7d56048911c17eb336be1e67ee821f891e9cf174
razaakomal <komal.raza@leoatech.com>
2025-08-28 08:16:53 +0500

    LIB-287-[backend]- create api route to save new category in database

--- backend/controllers/Categories.Controller.js
+++ backend/controllers/Categories.Controller.js
@@ -22,6 +22,38 @@ const GetCategories = asyncHanlder(async (req, res) => {
   }
 });
 
+/* Create New Category */
 
+const AddNewCategory = asyncHanlder(async (req, res) => {
+  console.log(req.body, "Category Payload");
 
-module.exports = { GetCategories };
+  const entityId = req?.user?.entityId || req?.user?.entity_id;
+
+  if (!entityId) {
+    return res.status(403).json("Invalid Request, No Library ID provided");
+  }
+  try {
+    if (!req.body?.name) {
+      return res.status(400).json("Invalid Category");
+    }
+    const updateCategory = await db.query(
+      `INSERT INTO categories (name, entity_id) VALUES ($1, $2) Returning id, name`,
+      [req.body.name, entityId]
+    );
+
+    console.log(updateCategory?.rows[0], "Category Saved");
+
+    res.status(200).json({
+      categories: updateCategory?.rows[0],
+      message: "Category Saved Successfully ",
+    });
+  } catch (error) {
+    console.log(error, "Error creating new category");
+    res.status(500).json({
+      error,
+      message: error.message || "Error Creating Category",
+    });
+  }
+});
+
+module.exports = { GetCategories, AddNewCategory };
--- backend/routes/CategoriesRoutes.js
+++ backend/routes/CategoriesRoutes.js
@@ -1,9 +1,28 @@
 const express = require("express");
-const {GetCategories } = require("../controllers/Categories.Controller");
+const {GetCategories, AddNewCategory } = require("../controllers/Categories.Controller");
+const { checkAuth } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 
+// Verify User Authentication (Logged-in users can see this route)
+router.use(checkAuth);
+
+//Get: fetch Categories List
+
 router.get("/", GetCategories);
+
+// Post: Add New Category
+router.post("/new", AddNewCategory);
+
+// PUT:Update Category 
+
+router.put("/update/:category_id");
+
+// Delete: remove Category  => Not implemented yet
+
+router.delete("/remove/:category_id");
+
+
 
 module.exports = router;
