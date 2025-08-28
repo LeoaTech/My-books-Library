248dbd91f981b25992dae2cb71d882eeacacc922
razaakomal <komal.raza@leoatech.com>
2025-08-28 08:23:42 +0500

    LIB-288-[backend]- add new api route to save new cover type

--- backend/controllers/Cover.Controller.js
+++ backend/controllers/Cover.Controller.js
@@ -22,6 +22,39 @@ const getBookCovers = asyncHanlder(async (req, res) => {
   }
 });
 
+/* Add New Cover Type for  */
+
+const AddCoverType = asyncHanlder(async (req, res) => {
+  console.log(req.body);
+  const entityId = req?.user?.entityId || req?.user?.entity_id;
+
+  if (!entityId) {
+    return res.status(403).json("Invalid Request, No Library ID provided");
+  }
+  if (!req.body.name) {
+    return res.status(400).json("Invalid cover name");
+  }
+  try {
+    const updateCoverQuery = await db.query(
+      `INSERT INTO covers (name, entity_id) VALUES ($1,$2) Returning id ,name`,
+      [req.body.name, entityId]
+    );
+
+    console.log(updateCoverQuery?.rows[0], "Cover Saved");
+
+    res.status(200).json({
+      covers: updateCoverQuery?.rows[0],
+      message: "New Cover Saved Successfully ",
+    });
+  } catch (error) {
+    console.log(error, "Error creating new cover");
+    res.status(500).json({
+      error,
+      message: error.message || "Error Creating cover",
+    });
+  }
+});
 
 
-module.exports = { getBookCovers };
+
+module.exports = { getBookCovers,AddCoverType };
--- backend/routes/CoversRoutes.js
+++ backend/routes/CoversRoutes.js
@@ -1,8 +1,21 @@
 const express = require("express");
-const { getBookCovers } = require("../controllers/Cover.Controller");
+const {
+  getBookCovers,
+  AddCoverType,
+
+} = require("../controllers/Cover.Controller");
+const { checkAuth } = require("../middleware/authMiddleware");
 
 const router = express.Router();
+router.use(checkAuth);
 
 router.get("/", getBookCovers);
+
+// Post: Add New Cover Type
+router.post("/new", AddCoverType);
+
+// PUT:Update Cover type
+
+router.put("/update/:cover_id");
 
 module.exports = router;
