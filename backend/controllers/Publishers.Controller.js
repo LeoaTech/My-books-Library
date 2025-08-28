900b82f272f20514d5e81df840714ef7222e0aab
razaakomal <komal.raza@leoatech.com>
2025-08-28 08:29:39 +0500

    LIB-146-[backend]- refactor fetch publishers api to fetch publishers for a specific library

--- backend/controllers/Publishers.Controller.js
+++ backend/controllers/Publishers.Controller.js
@@ -3,9 +3,14 @@ const db = require("../config/dbConfig");
 
 /* Get ALL Publishers */
 const FetchPublishers = asyncHanlder(async (req, res) => {
+  const entityId = req?.user?.entityId || req?.user?.entity_id;
+
+  if (!entityId) {
+    return res.status(403).json("Invalid Request, No Library ID provided");
+  }
   try {
-    const PublishersQuery = `SELECT * FROM publishers`;
-    const getAllPublishers = await db.query(PublishersQuery);
+    const PublishersQuery = `SELECT * FROM publishers WHERE entity_id =$1`;
+    const getAllPublishers = await db.query(PublishersQuery, [entityId]);
 
     res.status(200).json({
       publishers: getAllPublishers?.rows,
@@ -16,29 +21,7 @@ const FetchPublishers = asyncHanlder(async (req, res) => {
   }
 });
 
-/* Create New Publisher */
 
-const AddNewPublisher = asyncHanlder(async (req, res) => {
-  console.log(req.body);
-  try {
-    const createPublisherQuery = await db.query(
-      `INSERT INTO publishers (name) VALUES ($1)`,
-      [req.body.name]
-    );
 
-    console.log(createPublisherQuery?.rows[0], "Publishers /founded");
 
-    res.status(200).json({
-      publishers: createPublisherQuery?.rows[0],
-      message: "Publishers Found ",
-    });
-  } catch (error) {
-    console.log(error, "Error creating new Publisher");
-  }
-});
-
-/* Fetch Publishers By ID */
-
-/* Fetch All Publishers by Book ID */
-
-module.exports = { FetchPublishers,AddNewPublisher };
+module.exports = { FetchPublishers };
