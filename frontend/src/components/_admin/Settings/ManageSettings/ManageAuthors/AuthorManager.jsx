8fcac192da6080afe92964de6a9efbdef08f65d3
razaakomal <komal.raza@leoatech.com>
2025-08-29 19:52:50 +0500

    LIB-291-[frontend]-fear:implemented author data management functionality to create, and edit author

---
+++ frontend/src/components/_admin/Settings/ManageSettings/ManageAuthors/AuthorManager.jsx
@@ -0,0 +1,202 @@
+import { lazy, Suspense, useState } from 'react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+const AuthorsTable = lazy(() => import('./AuthorsTable'));
+const AuthorsForm = lazy(() => import('./AuthorsForm'));
+import { useFetchAuthors } from '../../../../../hooks/books/useFetchAuthors';
+import { useAuthor } from "../../../../../hooks/books/useSaveAuthor";
+import SkeletonTable from '../../../../Loader/SkeletonTable';
+import SkeletonModal from '../../../../Loader/SkeletonModal';
+
+
+function AuthorManager() {
+    const [showForm, setShowForm] = useState(false);
+    const [editData, setEditData] = useState(null);
+    const [viewData, setViewData] = useState(null);
+    const [deleteModal, setDeleteModal] = useState(false)
+    const [value, setValue] = useState(null)
+    const queryClient = useQueryClient();
+
+    const { data: authorsData, isLoading } = useFetchAuthors();
+
+    const { addAuthor, updateAuthor, deleteAuthor } = useAuthor();
+    const createMutation = useMutation({
+        mutationFn: addAuthor,
+        onSuccess: () => {
+            queryClient.invalidateQueries(['authors']);
+            setShowForm(false);
+        },
+    });
+
+    const updateMutation = useMutation({
+        mutationFn: updateAuthor,
+        onSuccess: () => {
+            queryClient.invalidateQueries(['authors']);
+            setShowForm(false);
+        },
+    });
+
+    const deleteMutation = useMutation({
+        mutationFn: deleteAuthor,
+        onSuccess: () => {
+            queryClient.invalidateQueries(['authors']);
+            setDeleteModal(false);
+            setValue(null)
+        },
+    });
+
+    const handleCreate = () => {
+        setEditData(null);
+        setShowForm(true);
+    };
+
+    const handleEdit = (author) => {
+        // console.log(author, "Edit ");
+        const editedAuthor = authorsData?.authors?.find((aut) => aut.id == author);
+        // console.log(author, "Edit ", editedAuthor);
+
+        setEditData(editedAuthor);
+        setShowForm(true);
+    };
+
+    const handleView = (author) => {
+        const viewAuthor = authorsData?.authors?.find((aut) => aut.id == author);
+
+        setViewData(viewAuthor);
+    };
+
+    const handleDelete = (id) => {
+        setDeleteModal(true);
+        setValue(id)
+    };
+
+    const handleSubmit = (data) => {
+        if (editData) {
+            updateMutation.mutate({ ...data, id: editData.id });
+        } else {
+            createMutation.mutate(data);
+        }
+    };
+
+    const deleteAuthorData = (data) => {
+
+        // console.log(data, "Delete");
+
+        deleteMutation.mutate(data);
+    }
+
+    if (isLoading) return <div className="text-center">Loading...</div>;
+
+    return (
+        <div>
+            <div className="mb-4 flex justify-end items-center">
+                <button
+                    onClick={handleCreate}
+                    className=" bg-[#758aae] text-white active:bg-[#80CAEE] 
+            font-medium rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 px-2 py-2 md:px-3 "
+                    type="button"
+                >
+                    Create New Author
+                </button>
+            </div>
+
+            {(!authorsData || authorsData?.authors?.length === 0) ? (
+                <div className="text-center text-gray-500">
+                    No data available. Please add new data.
+                </div>
+            ) : (
+                <Suspense fallback={<SkeletonTable columns={4} rows={2} />}>
+                    <AuthorsTable
+                        data={authorsData?.authors}
+                        onView={handleView}
+                        onEdit={handleEdit}
+                        onDelete={handleDelete}
+                    />
+                </Suspense>
+            )}
+
+            {showForm && (
+                <div className="fixed inset-0 lg:left-[18rem] bg-black bg-opacity-50 flex items-center justify-center">
+                    <div className="bg-neutral-50 dark:border-[#2E3A47] dark:bg-[#24303F] p-6 rounded shadow-lg">
+                        <Suspense fallback={<SkeletonModal />}>
+                            <AuthorsForm
+                                initialData={editData}
+                                saveAuthor={handleSubmit}
+                                onCancel={() => setShowForm(false)}
+                            /></Suspense>
+                    </div>
+                </div>
+            )}
+
+            {/* Delete Author */}
+            {deleteModal && (
+                <div className="fixed inset-0 lg:left-[18rem] bg-black bg-opacity-50 flex items-center justify-center">
+                    <div className="bg-neutral-50 dark:border-[#2E3A47] dark:bg-[#24303F] p-6 rounded shadow-lg">
+
+
+
+                        <div className=" md:mx-20">
+                            <div className="rounded-sm p-3 mb-8 bg-slate-100 border-b border-[#E2E8F0] py-4 px-6.5 dark:border-[#2E3A47]  dark:bg-[#2c3745]">
+                                <h3 className="font-bold text-[#313D4A] dark:text-white ">
+                                    Delete Author
+                                </h3>
+                            </div>
+                            <p>
+                                Are you sure you want to delete this author?
+
+                            </p>
+                            <div className='flex justify-between gap-4'>
+                                <button
+                                    onClick={() => deleteAuthorData(value)}
+                                    className="mt-4  bg-red-500 text-white py-2 px-4 rounded hover:bg-gray-600"
+                                >
+                                    Delete
+                                </button>
+                                <button
+                                    onClick={() => setDeleteModal(null)}
+                                    className="mt-4 bg-gray-500 text-white py-2 px-4 rounded hover:bg-gray-600"
+                                >
+                                    Close
+                                </button>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            )}
+            {viewData && (
+                <div className="fixed inset-0 lg:left-[18rem] bg-black bg-opacity-50 flex items-center justify-center">
+                    <div className="bg-neutral-50 dark:border-[#2E3A47] dark:bg-[#24303F] p-6 rounded shadow-lg">
+
+                        <div className=" md:mx-20">
+                            {/* Form Heading */}
+                            <div className="rounded-sm p-3 mb-8 bg-slate-100 border-b border-[#E2E8F0] py-4 px-6.5 dark:border-[#2E3A47]  dark:bg-[#2c3745]">
+                                <h3 className="font-bold text-[#313D4A] dark:text-white ">
+                                    View Author
+                                </h3>
+                            </div>
+                            <div className='p-2 border bg-slate-100 rounded-md shadow-sm'>
+                                <p
+                                    className='py-4 px-2'
+                                ><strong className='text-gray-700 dark:text-neutral-400 px-2'>Name:</strong> {viewData.name}</p>
+                                <p className='py-4 px-2'><strong className='text-gray-700 dark:text-neutral-400 px-2'>Links:</strong> {viewData.links}</p>
+                                <p className='py-4 px-2'><strong className='text-gray-700 dark:text-neutral-400 px-2'>Description:</strong> {viewData.description}</p>
+
+
+                            </div>
+                            <div className='flex justify-between gap-4'>
+                                <button
+                                    onClick={() => setViewData(null)}
+                                    className="mt-4 bg-gray-500 text-white py-2 px-4 rounded hover:bg-gray-600"
+                                >
+                                    Close
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default AuthorManager;
