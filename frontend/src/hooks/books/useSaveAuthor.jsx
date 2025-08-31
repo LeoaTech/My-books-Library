668ff1b0bfa0ccc049a2ce886e64f84966fd7eea
razaakomal <komal.raza@leoatech.com>
2025-08-31 17:24:20 +0500

    LIB-307-[frontend]-feat: enhance useAuthor custom hook to implement delete author function

--- frontend/src/hooks/books/useSaveAuthor.jsx
+++ frontend/src/hooks/books/useSaveAuthor.jsx
@@ -62,5 +62,34 @@ export const useAuthor = () => {
   };
 
 
-  return { isLoading, error, message, addAuthor ,updateAuthor};
+  /* Delete an Author */
+
+  const deleteAuthor = async (data) => {
+    setIsLoading(true);
+    setError(null);
+console.log(data);
+
+    try {
+      const response = await fetch(`${BASE_URL}/authors/remove/${data}`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+        // body: JSON.stringify(authorsID: data }),
+      });
+
+      const result = await response.json(); //response?.data;
+      console.log(result, "Author deleted Result");
+      setError(null);
+      setMessage(result.message);
+      return result;
+    } catch (error) {
+      console.log(error);
+      setError(error.message);
+      setMessage(error.detail)
+    } finally {
+      setIsLoading(false)
+    }
+  };
+
+  return { isLoading, error, message, addAuthor ,updateAuthor, deleteAuthor};
 };
