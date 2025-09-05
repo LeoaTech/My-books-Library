29e87dd11e2c686d68f42318241a531210c48de9
razaakomal <komal.raza@leoatech.com>
2025-09-05 07:43:19 +0500

    LIB-110-[backend]- added user auth validation for permissions routes

--- backend/routes/PermissionsRoutes.js
+++ backend/routes/PermissionsRoutes.js
@@ -5,9 +5,9 @@ const {
   DeletePermission,
   FetchPermissions,
 } = require("../controllers/RolesAndPermissions/PermissionsController");
-
+const {checkAuth} = require("../middleware/authMiddleware")
 const router = express.Router();
-
+router.use(checkAuth);
 router.get("/", FetchPermissions);
 
 // Add New Permission
