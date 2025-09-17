141a4e123e126ce0efac65b2b0c04c18bb3f9f50
razaakomal <komal.raza@leoatech.com>
2025-09-17 05:13:22 +0500

    LIB-242-[frontend]: feat-add issue book button in booking page

--- frontend/src/_admin/pages/Bookings.jsx
+++ frontend/src/_admin/pages/Bookings.jsx
@@ -1,52 +1,27 @@
 import { lazy, Suspense, useCallback, useEffect, useMemo, useState } from "react";
 import { HiPlus } from "react-icons/hi";
-import { useQuery } from "@tanstack/react-query";
-import { fetchPermissionsByRoleID } from "../../hooks/roles_permissions/useFetchRolesPermissions";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import { MdWarning } from "react-icons/md";
 import Loader from "../../components/_admin/Loader/Loader";
 import SkeletonTable from "../../components/Loader/SkeletonTable";
-import { debounce } from "lodash"; 
+import { debounce } from "lodash";
+import useVerifyPermissions from "../../hooks/verifyPermissions";
+import SkeletonModal from "../../components/Loader/SkeletonModal";
+import BookIssue from "../../components/_admin/ui/Modal/Bookings/IssueBooks";
 // Lazy Load Components
 const UnAuthorizedRoles = lazy(() => import("../../components/_admin/UnAuthorized"));
 const BookingTable = lazy(() => import("../../components/_admin/ui/Tables/BookingTable"));
 
-// Custom hook for permissions
-const usePermissions = (role) => {
-  const { data: permissionsList, isPending } = useQuery({
-    queryKey: ["role-permissions", { role }],
-    queryFn: () => fetchPermissionsByRoleID(role),
-    enabled: !!role,
-    staleTime: 1000 * 60 * 5, 
-  });
 
-  const permissions = useMemo(() => {
-    if (!permissionsList) return new Set();
-    return new Set(
-      permissionsList.permissions
-        .filter((p) => p?.permission_name.toUpperCase().includes("BOOKING"))
-        .map((p) => p.permission_name.toUpperCase())
-    );
-  }, [permissionsList]);
-
-  const hasPermission = useCallback(
-    (permission) => {
-      const permUpper = permission.toUpperCase();
-      return Array.from(permissions).some((p) => p.includes(permUpper));
-    },
-    [permissions]
-  );
-
-  return { permissions, hasPermission, isPending };
-};
 
 const Bookings = () => {
   const [showModal, setShowModal] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
   const { auth } = useAuthContext();
-  const { permissions, hasPermission, isPending } = usePermissions(auth?.role);
+  const selectedRole = auth?.role;
 
-  console.log(hasPermission("READ"), permissions);
+  const { hasPermission, isPending, error, hasPageAccess } = useVerifyPermissions(selectedRole || auth?.role, "BOOK")
+
 
   // Debounced search input handle
   const debouncedSearch = useMemo(
@@ -66,7 +41,7 @@ const Bookings = () => {
   const handleSearch = useCallback(
     (e) => {
       const value = e.target.value;
-      if (value.length <= 10) { 
+      if (value.length <= 10) {
         debouncedSearch(value);
       }
     },
@@ -75,20 +50,17 @@ const Bookings = () => {
 
   if (isPending) {
     return (
-      <div className="flex justify-center items-center h-screen">
-        <Loader />
-      </div>
-    );
+      <Loader />
+    )
   }
 
-  if (!permissions.size) {
+  if (error || !hasPageAccess()) {
     return (
       <Suspense fallback={<Loader />}>
         <UnAuthorizedRoles />
       </Suspense>
     );
   }
-
   return (
     <div className="container mx-auto px-4">
       <div className="flex justify-between items-center py-5">
@@ -109,17 +81,17 @@ const Bookings = () => {
           />
         )}
 
-        {hasPermission("CREATE") ? (
-          <button
-            className="flex items-center gap-2 bg-[#758aae] text-white px-4 py-2 rounded-md 
+        {/* {hasPermission("CREATE") ? ( */}
+        <button
+          className="flex items-center gap-2 bg-[#758aae] text-white px-4 py-2 rounded-md 
               hover:bg-[#80CAEE] focus:outline-none focus:ring-2 focus:ring-[#758aae] 
               transition-colors duration-200"
-            onClick={(prev) => setShowModal(!prev)}
-            aria-label="Add new booking"
-          >
-            <HiPlus /> New Booking
-          </button>
-        ) : (
+          onClick={() => setShowModal(true)}
+          aria-label="issue books"
+        >
+          <HiPlus />Issue Books
+        </button>
+        {/* ) : (
           <div className="relative group">
             <button
               className="flex items-center gap-2 bg-[#758aae]/50 text-white px-4 py-2 rounded-md 
@@ -134,7 +106,7 @@ const Bookings = () => {
               <MdWarning /> Access Denied
             </span>
           </div>
-        )}
+        )} */}
       </div>
 
       {hasPermission("READ") ? (
@@ -148,11 +120,12 @@ const Bookings = () => {
         </Suspense>
       )}
 
-      {showModal && hasPermission("CREATE") && (
-        <Suspense fallback={<Loader />}>
-          <h2>Add Booking </h2>
-        </Suspense>
-      )}
+
+      {showModal &&
+        <Suspense fallback={<SkeletonModal title={"Issue Book"} close={close} actionButton={"Issue Books"} />}>
+          <BookIssue onClose={() => setShowModal(false)} />
+        </Suspense>}
+
     </div>
   );
 };
