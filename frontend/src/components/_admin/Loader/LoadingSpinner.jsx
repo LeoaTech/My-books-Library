bb7cfa785ab2fe19b1b7ab48a4fc0e2accf09fc2
razaakomal <komal.raza@leoatech.com>
2025-09-19 11:03:20 +0500

    fixed: loading spinner style issue inside the button

--- frontend/src/components/_admin/Loader/LoadingSpinner.jsx
+++ frontend/src/components/_admin/Loader/LoadingSpinner.jsx
@@ -2,7 +2,7 @@ import "./LoadSpinner.css"
 const LoadingSpinner = () => {
 
     return (
-        <span className="spinner"></span>
+        <span className="spinner flex items-center justify-center shrink-0 "></span>
 
     )
 }
