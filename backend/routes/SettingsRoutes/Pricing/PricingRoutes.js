c06d65c2f5235d98e0e3892117e2994b1e959b6e
razaakomal <komal.raza@leoatech.com>
2025-09-25 11:01:57 +0500

    LIB-346-[backend]- feat: create an api route to add new  membership_plan for a library

--- backend/routes/SettingsRoutes/Pricing/PricingRoutes.js
+++ backend/routes/SettingsRoutes/Pricing/PricingRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { checkAuth } = require("../../../middleware/authMiddleware");
-const { FetchPricingPlans } = require("../../../controllers/SettingsController/Pricing.Controller");
+const { FetchPricingPlans,CreatePlan } = require("../../../controllers/SettingsController/Pricing.Controller");
 
 const router = express.Router();
 // Verify User Authentication (Logged-in users can see this route)
@@ -8,6 +8,10 @@ router.use(checkAuth);
 
 //Get: fetch pricing plans List
 router.get("/", FetchPricingPlans);
+
+// Post: Add New Plan
+router.post("/create", CreatePlan);
+
 
 
 module.exports = router;
