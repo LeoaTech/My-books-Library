9f3674928f56685f3995587e7c5cbb7807329d59
razaakomal <komal.raza@leoatech.com>
2025-09-25 10:33:44 +0500

    LIB-343-[backend]-feat: save or update settings for a library

--- backend/controllers/SettingsController/Settings.Controller.js
+++ backend/controllers/SettingsController/Settings.Controller.js
@@ -21,4 +21,94 @@ const FetchSettings = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { FetchSettings };
+/* Update Settings */
+
+const UpdateSettings = asyncHandler(async (req, res) => {
+  //   console.log(req.body, "Settings Form");
+
+  const entityId = req?.user?.entityId || req?.user?.entity_id;
+
+  if (!entityId) {
+    return res.status(403).json("Invalid Request, No Library ID provided");
+  }
+
+  const checkSettingsExist = await db.query(
+    `Select id from settings where entity_id =$1`,
+    [entityId]
+  );
+  console.log(checkSettingsExist.rowCount, "If settings exists");
+
+  try {
+    if (!req.body) {
+      return res.status(400).json("Invalid Settings Details");
+    }
+
+    const {
+      allow_select_booking_date,
+      default_booking_duration,
+      allow_purchase,
+      allow_borrow,
+      consecutive_renewals,
+      late_returns_fine,
+    } = req.body;
+
+    if (checkSettingsExist.rowCount === 0) {
+      const createNewSettingsQuery = await db.query(
+        `INSERT INTO settings (allow_select_booking_date,
+    default_booking_duration,
+    allow_purchase,
+    allow_borrow,
+    consecutive_renewals,
+    late_returns_fine,
+     entity_id) VALUES ($1,$2,$3, $4, $5,$6,$7)`,
+        [
+          allow_select_booking_date,
+          default_booking_duration,
+          allow_purchase,
+          allow_borrow,
+          consecutive_renewals,
+          late_returns_fine,
+          entityId,
+        ]
+      );
+
+      // console.log(createNewSettingsQuery, "Create Settings");
+    } else {
+      const updateSettingsQuery = await db.query(
+        `UPDATE settings SET 
+    allow_select_booking_date =$1,
+    default_booking_duration =$2,
+    allow_purchase =$3,
+    allow_borrow =$4,
+    consecutive_renewals =$5,
+    late_returns_fine =$6
+    Where 
+       id=$7 
+    AND entity_id=$8 RETURNING id`,
+        [
+          allow_select_booking_date,
+          default_booking_duration,
+          allow_purchase,
+          allow_borrow,
+          consecutive_renewals,
+          late_returns_fine,
+          checkSettingsExist?.rows[0]?.id,
+          entityId,
+        ]
+      );
+      // console.log(updateSettingsQuery, "Updated settings");
+    }
+
+    res.status(200).json({
+      message: "Settings Updated Successfully ",
+    });
+  } catch (error) {
+    console.log(error, "Error saving settings");
+    res.status(500).json({
+      error,
+      message: error.message || "Error saving settings",
+    });
+  }
+});
+
+module.exports = { FetchSettings, UpdateSettings };
