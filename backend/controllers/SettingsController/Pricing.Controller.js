7a96aae7f730a9c8f6f915d08d700d11c42f5064
razaakomal <komal.raza@leoatech.com>
2025-09-25 10:57:11 +0500

    LIB-346-[backend]- feat: added an api route to create new  membership_plan for a library

--- backend/controllers/SettingsController/Pricing.Controller.js
+++ backend/controllers/SettingsController/Pricing.Controller.js
@@ -21,5 +21,48 @@ const FetchPricingPlans = asyncHandler(async (req, res) => {
   }
 });
 
+/* Create New Pricing*/
 
-module.exports = { FetchPricingPlans };
+const CreatePlan = asyncHandler(async (req, res) => {
+  console.log(req.body);
+
+  const entityId = req?.user?.entityId || req?.user?.entity_id;
+
+  if (!entityId) {
+    return res.status(403).json("Invalid Request, No Library ID provided");
+  }
+  try {
+    if (!req.body) {
+      return res.status(400).json("Invalid Pricing Details");
+    }
+
+    const { plan_name, price, duration, credits_allocated, features } =
+      req.body;
+
+    const jsonFeatures = JSON.stringify(features);
+    const createPricinguery = await db.query(
+      `INSERT INTO membership_plan (plan_name, price, duration, credits_allocated, features,entity_id) VALUES ($1,$2,$3, $4,$5, $6) RETURNING plan_id,plan_name`,
+      [plan_name, price, duration, credits_allocated, jsonFeatures, entityId]
+    );
+
+    console.log(createPricinguery?.rows[0], "Pricing plan Saved");
+
+    res.status(200).json({
+      pricing: createPricinguery?.rows[0],
+      message: "Pricing plan Saved Successfully ",
+    });
+  } catch (error) {
+    console.log(error, "Error creating new pricing plan");
+    res.status(500).json({
+      error,
+      message: error.message || "Error Creating Pricing",
+    });
+  }
+});
+
+
+
+
+
+
+module.exports = { FetchPricingPlans,CreatePlan };
