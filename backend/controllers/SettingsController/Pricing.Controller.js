0030fc19de506429643196c95d8ddb811d15e16f
razaakomal <komal.raza@leoatech.com>
2025-09-25 10:59:02 +0500

    LIB-346-[backend]- feat: added an api route to update membership_plan details for a library

--- backend/controllers/SettingsController/Pricing.Controller.js
+++ backend/controllers/SettingsController/Pricing.Controller.js
@@ -21,7 +21,7 @@ const FetchPricingPlans = asyncHandler(async (req, res) => {
   }
 });
 
-/* Create New Pricing*/
+/* Create New Pricing Plan*/
 
 const CreatePlan = asyncHandler(async (req, res) => {
   console.log(req.body);
@@ -45,7 +45,7 @@ const CreatePlan = asyncHandler(async (req, res) => {
       [plan_name, price, duration, credits_allocated, jsonFeatures, entityId]
     );
 
-    console.log(createPricinguery?.rows[0], "Pricing plan Saved");
+    // console.log(createPricinguery?.rows[0], "Pricing plan Saved");
 
     res.status(200).json({
       pricing: createPricinguery?.rows[0],
@@ -60,9 +60,61 @@ const CreatePlan = asyncHandler(async (req, res) => {
   }
 });
 
+const UpdatePlan = asyncHandler(async (req, res) => {
+  console.log(req.body, req.params);
+
+  const entityId = req?.user?.entityId || req?.user?.entity_id;
+
+  if (!entityId) {
+    return res.status(403).json("Invalid Request, No Library ID provided");
+  }
+
+  try {
+    if (!req.body) {
+      return res.status(400).json("Invalid Pricing plan Details");
+    }
+
+    if (!req.params.plan_id) {
+      return res.status(400).json({
+        message: "Invalid plan ID",
+      });
+    }
+    const { plan_id } = req.params;
+
+    const { plan_name, price, duration, credits_allocated, features } =
+      req.body;
+
+    const jsonFeatures = JSON.stringify(features);
+    const updatePricingQuery = await db.query(
+      `UPDATE membership_plan SET plan_name=$1, price=$2, duration =$3, credits_allocated=$4, features=$5 WHERE entity_id=$6 AND plan_id=$7 RETURNING plan_id,plan_name`,
+      [
+        plan_name,
+        price,
+        duration,
+        credits_allocated,
+        jsonFeatures,
+        entityId,
+        plan_id,
+      ]
+    );
+
+    // console.log(updatePricingQuery?.rows[0], "Pricing Plan Updated");
+
+    res.status(200).json({
+      pricing: updatePricingQuery?.rows[0],
+      message: "Pricing Plan Updated Successfully ",
+    });
+  } catch (error) {
+    console.log(error, "Error Updating pricing Plans");
+    res.status(500).json({
+      error,
+      message: error.message || "Error Updating Pricing Plan",
+    });
+  }
+});
 
 
 
 
 
-module.exports = { FetchPricingPlans,CreatePlan };
+module.exports = { FetchPricingPlans,CreatePlan , UpdatePlan};
