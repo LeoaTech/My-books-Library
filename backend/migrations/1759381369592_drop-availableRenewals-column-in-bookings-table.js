a8e8e944159c7ba3c3f3d1c79921a76373c2139c
razaakomal <komal.raza@leoatech.com>
2025-10-02 13:13:44 +0500

    LIB-358-[db-migration]-drop the avaiable_renewals column from bookings table

---
+++ backend/migrations/1759381369592_drop-availableRenewals-column-in-bookings-table.js
@@ -0,0 +1,30 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+const up = (pgm) => {
+  pgm.dropColumn("bookings", "available_renewals");
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+const down = (pgm) => {
+  pgm.addColumn("bookings", {
+    available_renewals: {
+      type: "integer",
+      default: 0,
+      notNull: true,
+    },
+  });
+};
+
+module.exports= {up,down, shorthands}
