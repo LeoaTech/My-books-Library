2177af20faa27da29b9f5edbfc4cc60ce72650b4
razaakomal <komal.raza@leoatech.com>
2025-10-07 16:41:47 +0500

    LIB-362-[db-migration]-added new column for book edition in books table

---
+++ backend/migrations/1759808987908_add-new-column-in-books-table.js
@@ -0,0 +1,29 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+const up = (pgm) => {
+  pgm.addColumn("books", {
+    edition: {
+      type: "integer",
+      default: 0,
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+const down = (pgm) => {
+    pgm.dropColumn("books", "edition")
+};
+
+module.exports = { up, down, shorthands };
