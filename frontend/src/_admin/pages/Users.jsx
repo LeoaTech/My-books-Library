4620c8e27f771c9272f24e896ec5757bde082b64
razaakomal <komal.raza@leoatech.com>
2025-11-24 19:46:38 +0500

    LIb-395-[frontend]-fixed: plan based permissions to create users on a library

--- frontend/src/_admin/pages/Users.jsx
+++ frontend/src/_admin/pages/Users.jsx
@@ -18,12 +18,12 @@ const Users = () => {
   const [showModal, setShowModal] = useState(false)
   const [searchTerm, setSearchTerm] = useState("");
   const { data: currentPlan, isLoading } = useFetchCurrentPlan(auth)
-  const hasPermissionToAddUser = currentPlan?.userCounts?.count <= 5;
+  const hasPermissionToAddUser = !isLoading ? ! currentPlan?.isActive ? true : currentPlan?.userCounts?.count <= 5:false;
   
   /* Fetch:/ Users in Library */
   const { isPending, error, data } = useFetchUserRoles();
 
-  if (isPending) return <Loader />;
+  if (isPending || isLoading) return <Loader />;
   if (error) return "An error has occurred: " + error.message;
 
   return (
@@ -31,7 +31,7 @@ const Users = () => {
       <h1 className="m-5 text-lg md:text-2xl text-[#8A99AF]">Users</h1>
       <div className="flex justify-end items-end mb-2">
         <button
-        disabled={!currentPlan?.isActive && hasPermissionToAddUser}
+        disabled={hasPermissionToAddUser}
           className=" bg-[#758aae] text-white active:bg-[#80CAEE] 
             font-medium rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 px-2 py-2 md:px-3 "
           type="button"
