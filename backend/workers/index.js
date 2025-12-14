d0458c2e900d58adaeeac73c2f41f4a89c661e28
razaakomal <komal.raza@leoatech.com>
2025-12-14 13:17:17 +0500

    LIB-442-[backend]-initialize push notification worker

--- backend/workers/index.js
+++ backend/workers/index.js
@@ -1,17 +1,16 @@
-require('dotenv').config(); 
-const { connection } = require('../config/redisConfig'); 
+require("dotenv").config();
+const { connection } = require("../config/redisConfig");
 
-
-const emailWorker = require('./emailWorker');
-
+const emailWorker = require("./emailWorker");
+const pushWorker = require("./pushWorker");
 const gracefulShutdown = async () => {
-  console.log('SIGTERM received. Closing workers...');
-  
-  await emailWorker.close();
+  console.log("SIGTERM received. Closing workers...");
 
-  
-  console.log('Workers closed. Exiting.');
+  await emailWorker.close();
+  await pushWorker.close();
+
+  console.log("Workers closed. Exiting.");
   process.exit(0);
 };
 
-process.on('SIGTERM', gracefulShutdown);
+process.on("SIGTERM", gracefulShutdown);
