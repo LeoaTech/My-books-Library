029b6d42ebca21a00997abc513e3eb3fa4b2719a
razaakomal <komal.raza@leoatech.com>
2025-12-14 20:52:29 +0500

    updated the rediss url

--- backend/config/redisConfig.js
+++ backend/config/redisConfig.js
@@ -1,6 +1,6 @@
 const IORedis = require("ioredis");
 require("dotenv").config();
-const connection = new IORedis(process.env.UPSTASH_REDIS_URL || "redis://127.0.0.1:6379", {
+const connection = new IORedis(process.env.RENDER_REDIS_URL || "redis://127.0.0.1:6379", {
   maxRetriesPerRequest: null,
 });
 
