4cd0250e45d8cce45c9a4562a8622b89d4afd531
razaakomal <komal.raza@leoatech.com>
2026-01-07 08:56:11 +0500

    LIB-457-[backend]-modify wishlist api to validate if book id exists for a specific library

--- backend/routes/ProductWishlist/ProductWishlistRoute.js
+++ backend/routes/ProductWishlist/ProductWishlistRoute.js
@@ -1,20 +1,36 @@
 const express = require("express");
 const { checkAuth } = require("../../middleware/authMiddleware");
 const db = require("../../config/dbConfig");
-
+const { pool } = require("../../config/dbConfig");
 const router = express.Router();
 
 router.use(checkAuth);
 
 router.post("/api/wishlist", async (req, res) => {
   const { itemId } = req.body;
-  const { entityId, userId } = req.user;
+  const entityId = req.user?.entityId || req?.user?.entity_id;
+  const userId = req.user?.userId || req?.user?.user_id;
+  const client = await pool.connect();
 
   try {
     if (!itemId || isNaN(itemId)) {
       return res.status(400).json({ error: "Invalid Book ID" });
     }
 
+    const branchId = await getMainBranchId(client, entityId);
+    if (!branchId) {
+      throw new Error("Main Branch ID not found.");
+    }
+
+    const getBookItem = await client.query(
+      `SELECT id,title from books WHERE id = $1 and branch_id =$2`,
+      [itemId, branchId]
+    );
+
+    const bookId = getBookItem.rows[0]?.id;
+    if (!bookId) {
+      return res.status(400).json({ error: "No Book ID Exists" });
+    }
     const query = `
       INSERT INTO wishlist (entity_id, user_id, book_id)
       VALUES ($1, $2, $3, $4)
@@ -23,11 +39,11 @@ router.post("/api/wishlist", async (req, res) => {
 
     const values = [entityId, userId, parseInt(itemId)];
 
-    const result = await db.query(query, values);
+    const result = await client.query(query, values);
 
     res.status(200).json({ message: "Added to wishlist" });
   } catch (error) {
-    res.status(500).json({ error: "Failed" });
+    res.status(500).json({ error, message: "Failed to Add item to wishlist" });
   }
 });
 
