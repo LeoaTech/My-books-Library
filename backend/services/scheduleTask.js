e19b43499dd39907b5535a3e7be28a39d1ece7aa
razaakomal <komal.raza@leoatech.com>
2026-01-12 08:20:02 +0500

    LIB-463-[backend]-call upcomingDueDate function in cronJob to send reminders for upcoming due to users

--- backend/services/scheduleTask.js
+++ backend/services/scheduleTask.js
@@ -1,16 +1,14 @@
 const corn = require("node-cron");
 const { checkOverdueStatusAndFine, checkUpcomingDueDates } = require("./automateProcess.js");
-
-
 // Run at midnight
 corn.schedule("0 0 * * *", async () => {
-    console.log('Running daily check for overdue books and add fine for late returns...');
+    console.log('Running daily check for upcoming due date and overdue books to add fine for late returns...');
     try {
         await checkOverdueStatusAndFine();
         await checkUpcomingDueDates();
-        console.log('Overdue process completed successfully.');
+        console.log('Booking due date and overdue process completed successfully.');
     } catch (error) {
-        console.error('Error during overdue process:', error);
+        console.error('Error during booking process:', error);
     }
 }, {
     timezone: "Asia/Karachi", 
