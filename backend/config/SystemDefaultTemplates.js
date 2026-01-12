a2dddb92a0327bb1d139ef2916658005fb5ce43a
razaakomal <komal.raza@leoatech.com>
2026-01-12 08:08:16 +0500

    LIB-464-[backend]-added  booking_due_reminder email template

--- backend/config/SystemDefaultTemplates.js
+++ backend/config/SystemDefaultTemplates.js
@@ -135,7 +135,7 @@ const SYSTEM_DEFAULTS = {
   "subscription-updated-email": {
     subject: "Subscription Updated : {{subscription_id}}",
     body: "Hi {{customer_name}}, \n\n Thanks for Subscribing again. Your Subscription for ID {{subscription_id}} updated successfully. Enjoy Your Reading!",
-    variables: ["customer_name", "booking_title", "booking_id", "address"],
+    variables: ["customer_name",  "plan_name", "subscription_id"],
     channel: "email",
   },
   "subscription-updated-push": {
@@ -144,6 +144,13 @@ const SYSTEM_DEFAULTS = {
     variables: ["customer_name", "plan_name", "subscription_id"],
     channel: "push",
   },
+  "booking-due-reminder-email": {
+    subject: "Reminder: Return Due Soon for {{book_title}}",
+    body: "Hi {{customer_name}},\n\nThis is a friendly reminder that your booking for Book \n\n {{book_title}} is due for return on {{due_date}}.\n\nPlease return it on time to avoid any late fees.\n\nHappy Reading!\n{{library_name}}",
+    variables: ["customer_name", "book_title", "due_date", "library_name"],
+    channel: "email",
+  },
+  
 };
 
 module.exports = SYSTEM_DEFAULTS;
