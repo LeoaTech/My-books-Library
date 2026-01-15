17b0361e80bd7eb2f4fa6d6e9c938825e62e7e89
razaakomal <komal.raza@leoatech.com>
2026-01-15 10:01:02 +0500

    LIB-421-[backend] fix stripe connection status api return response behavios

--- backend/routes/PG_Onboarding/StripeStatus.js
+++ backend/routes/PG_Onboarding/StripeStatus.js
@@ -14,14 +14,15 @@ router.get("/", async (req, res) => {
 
     const entity = await getEntityInfo(db, entityId);
 
-    if (!entity) res.status(404).json({ error: "Library ID not found" });
+    if (!entity) {
+      return res.status(404).json({ error: "Library ID not found" });
+    }
 
     let status = "disconnected";
     let connected = false;
     const paymentMethods = entity?.user_details?.stripe;
 
     if (paymentMethods?.stripe_account_id) {
-
       const account = await stripe.accounts.retrieve(
         paymentMethods.stripe_account_id
       );
@@ -33,14 +34,16 @@ router.get("/", async (req, res) => {
         false;
     }
 
-    res.json({
+    return res.status(200).json({
       connected,
       account_id: paymentMethods?.stripe_account_id,
       status,
     });
   } catch (error) {
     console.error("Status check error:", error);
-    res.status(500).json({ error: "Failed to fetch Library stripe status info" });
+    res
+      .status(500)
+      .json({ error: "Failed to fetch Library stripe status info" });
   }
 });
 
