88fad77e791b90a21369f4620cd26320edf397b8
razaakomal <komal.raza@leoatech.com>
2026-02-05 13:04:59 +0500

    LIB-485-[frontend]-fixed bookingByLocation chart height

--- frontend/src/components/_admin/Dashboard/LocationDonutChart.jsx
+++ frontend/src/components/_admin/Dashboard/LocationDonutChart.jsx
@@ -17,7 +17,7 @@ const LocationDonutChart = ({ data }) => {
 
     return (
         <TableCard title="Bookings per Location">
-            <div className="h-[200px] w-full mt-4">
+            <div className="h-[300px] w-full mt-4">
                 <ResponsiveContainer width="100%" height="100%">
                     <PieChart>
                         <Pie
