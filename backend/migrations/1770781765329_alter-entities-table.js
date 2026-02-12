f12eb72915e2b55f1e42cab2e6fb7d32a45ce3df
razaakomal <komal.raza@leoatech.com>
2026-02-12 08:26:45 +0500

    LIB-492-[database]- add library logo column in entities table

---
+++ backend/migrations/1770781765329_alter-entities-table.js
@@ -0,0 +1,30 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+const up = (pgm) => {
+     pgm.addColumn("entities", {
+    library_logo: {
+      type: "jsonb",
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+const down = (pgm) => {
+    pgm.dropColumn("entities", "library_logo")
+};
+
+
+
+module.exports= {up,down,shorthands}
