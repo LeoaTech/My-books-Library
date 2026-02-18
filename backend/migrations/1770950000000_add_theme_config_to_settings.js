b15730f93bbd6727861506003add54a72e00dc5d
razaakomal <komal.raza@leoatech.com>
2026-02-18 10:27:36 +0500

    LIB-496-[database]-feat: add theme_config field in Settings table

---
+++ backend/migrations/1770950000000_add_theme_config_to_settings.js
@@ -0,0 +1,24 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.addColumns('settings', {
+    theme_config: { type: 'jsonb', default: null },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropColumns('settings', ['theme_config']);
+};
