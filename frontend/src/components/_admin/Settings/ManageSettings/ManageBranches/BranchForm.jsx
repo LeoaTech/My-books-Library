87a80054a756c050c136b020bdf9125dbfcca926
razaakomal <komal.raza@leoatech.com>
2026-02-24 09:09:41 +0500

    LIB-293-[frontend]-refactor BranchManager component for improved styling and accessibility

--- frontend/src/components/_admin/Settings/ManageSettings/ManageBranches/BranchForm.jsx
+++ frontend/src/components/_admin/Settings/ManageSettings/ManageBranches/BranchForm.jsx
@@ -1,5 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
+import { RxCross1 } from 'react-icons/rx';
 import { toast } from 'react-toastify';
 import { z } from 'zod';
 
@@ -71,83 +72,100 @@ const BranchForm = ({
 
     };
     return (
-        <div className=" md:mx-20">
-            {/* Form Heading */}
-            <div className="rounded-sm p-3 mb-8 bg-slate-100 border-b border-[#E2E8F0] py-4 px-6.5 dark:border-[#2E3A47]  dark:bg-[#2c3745]">
-                <h3 className="font-bold text-[#313D4A] dark:text-white ">
-                    {initialData ? 'Edit Branch' : 'Create Branch'}
-                </h3>
+        <>
+            <div className="flex justify-end items-end ">
+                <RxCross1
+                    style={{
+                        height: 18,
+                        width: 18,
+                        cursor: "pointer",
+                        color: "var(--color-text)",
+                        strokeWidth: 2,
+                    }}
+                    onClick={onCancel}
+                />
             </div>
-
-            {/* Form Fields */}
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="mb-1">
-                    <label htmlFor="address" className="mb-2.5 block text-[#0284c7] dark:text-white">
-                        Branch Name</label>
-                    <input
-                        id="name"
-                        {...register('name', { required: true })}
-                        className="w-full rounded-sm border-[1.5px] dark:text-white border-[#E2E8F0] bg-transparent py-3 px-5 font-medium outline-none transition focus:border-[#3C50E0] active:border-[#3C50E0] disabled:cursor-default disabled:bg-[#F5F7FD] dark:border-[#3d4d60] dark:bg-[#1d2a39] dark:focus:border-[#3C50E0]"
-                    />
-                    {errors.name && <p className="text-red-500 text-xs mt-1">{errors.name.message}</p>}
-
+            <div className=" md:mx-20">
+                {/* Form Heading */}
+                <div className="rounded-sm p-3 mb-8 border-b border-border py-4 px-6.5 ">
+                    <h3 className="font-bold text-text">
+                        {initialData ? 'Edit Branch' : 'Create Branch'}
+                    </h3>
                 </div>
 
-                <div className="mb-1" >
-                    <label htmlFor="address" className="mb-2.5 block text-[#0284c7] dark:text-white">
-                        Address
-                    </label>
-                    <input
+                {/* Form Fields */}
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="mb-1">
+                        <label htmlFor="address" className="mb-2.5 block text-text">
+                            Branch Name</label>
+                        <input
+                            id="name"
+                            {...register('name', { required: true })}
+                            className="w-full rounded-sm border-[1.5px] border-border bg-background py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-[#F5F7FD] "
+                        />
+                        {errors.name && <p className="text-red-500 text-xs mt-1">{errors.name.message}</p>}
 
-                        id="address" {...register('address')}
-                        className="w-full rounded-sm border-[1.5px] dark:text-white border-[#E2E8F0] bg-transparent py-3 px-5 font-medium outline-none transition focus:border-[#3C50E0] active:border-[#3C50E0] disabled:cursor-default disabled:bg-[#F5F7FD] dark:border-[#3d4d60] dark:bg-[#1d2a39] dark:focus:border-[#3C50E0]"
+                    </div>
 
-                    />
-                    {errors.address && <p className="text-red-500 text-xs mt-1">{errors.address.message}</p>}
-                </div>
+                    <div className="mb-1" >
+                        <label htmlFor="address" className="mb-2.5 block text-text">
+                            Address
+                        </label>
+                        <input
 
-                <div className="mb-1" >
-                    <label htmlFor="city" className="mb-2.5 block text-[#0284c7] dark:text-white">
-                        City
-                    </label>
-                    <input
+                            id="address" {...register('address')}
+                            className="w-full rounded-sm border-[1.5px] border-border bg-background py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-[#F5F7FD] "
 
-                        id="city" {...register('city')}
-                        className="w-full rounded-sm border-[1.5px] dark:text-white border-[#E2E8F0] bg-transparent py-3 px-5 font-medium outline-none transition focus:border-[#3C50E0] active:border-[#3C50E0] disabled:cursor-default disabled:bg-[#F5F7FD] dark:border-[#3d4d60] dark:bg-[#1d2a39] dark:focus:border-[#3C50E0]"
+                        />
+                        {errors.address && <p className="text-red-500 text-xs mt-1">{errors.address.message}</p>}
+                    </div>
 
-                    />
-                    {errors.city && <p className="text-red-500 text-xs mt-1">{errors.city.message}</p>}
-                </div>
+                    <div className="mb-1" >
+                        <label htmlFor="city" className="mb-2.5 block text-text">
+                            City
+                        </label>
+                        <input
 
-                <div className="mb-1">
-                    <label htmlFor="phone" className="mb-2.5 block text-[#0284c7] dark:text-white">
-                        Phone
-                    </label>
-                    <input id="phone" {...register('phone')}
-                        className="w-full mb-4 rounded-sm border-[1.5px] dark:text-white border-[#E2E8F0] bg-transparent py-3 px-5 font-medium outline-none transition focus:border-[#3C50E0] active:border-[#3C50E0] disabled:cursor-default disabled:bg-[#F5F7FD] dark:border-[#3d4d60] dark:bg-[#1d2a39] dark:focus:border-[#3C50E0]" />
-                    {errors.phone && <p className="text-red-500 text-xs mt-1">{errors.phone.message}</p>}
-                </div>
+                            id="city" {...register('city')}
+                            className="w-full rounded-sm border-[1.5px] border-border bg-background py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-[#F5F7FD] "
 
-                <div>
-                    <label htmlFor="country" className="mb-2.5 block text-[#0284c7] dark:text-white">
-                        Country
-                    </label>
-                    <input id="country" {...register('country')}
-                        className="w-full rounded-sm border-[1.5px] dark:text-white border-[#E2E8F0] bg-transparent py-3 px-5 font-medium outline-none transition focus:border-[#3C50E0] active:border-[#3C50E0] disabled:cursor-default disabled:bg-[#F5F7FD] dark:border-[#3d4d60] dark:bg-[#1d2a39] dark:focus:border-[#3C50E0]"
+                        />
+                        {errors.city && <p className="text-red-500 text-xs mt-1">{errors.city.message}</p>}
+                    </div>
 
-                    />
-                    {errors.country && <p className="text-red-500 text-xs mt-1">{errors.country.message}</p>}
-                </div>
-                <div className="mt-4 px-4 py-3 sm:flex sm:flex-row-reverse gap-3 sm:px-6">
-                    <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
-                        {isSubmitting ? "Saving" : "Save"}
-                    </button>
-                    <button type="button" onClick={onCancel} className="bg-gray-500 text-white py-2 px-4 rounded hover:bg-gray-600">
-                        Cancel
-                    </button>
-                </div>
-            </form>
-        </div>
+                    <div className="mb-1">
+                        <label htmlFor="phone" className="mb-2.5 block text-text">
+                            Phone
+                        </label>
+                        <input id="phone" {...register('phone')}
+                            className="w-full mb-4 rounded-sm border-[1.5px] border-border bg-background py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-[#F5F7FD] " />
+                        {errors.phone && <p className="text-red-500 text-xs mt-1">{errors.phone.message}</p>}
+                    </div>
+
+                    <div>
+                        <label htmlFor="country" className="mb-2.5 block text-text">
+                            Country
+                        </label>
+                        <input id="country" {...register('country')}
+                            className="w-full rounded-sm border-[1.5px] border-border bg-background py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-[#F5F7FD] "
+
+                        />
+                        {errors.country && <p className="text-red-500 text-xs mt-1">{errors.country.message}</p>}
+                    </div>
+                    <div className="mt-4 px-4 py-3 sm:flex sm:flex-row-reverse gap-3 sm:px-6">
+                        <button type="submit" className={`flex items-center border-2 border-border gap-2 bg-background text-text px-4 py-2 rounded-md 
+                hover:bg-secondary focus:outline-none focus:ring-2 focus:ring-primary 
+                transition-colors duration-200 ${isSubmitting ? 'opacity-50 cursor-not-allowed' : ''
+                            }`}
+                        >
+                            {isSubmitting ? "Saving" : "Save"}
+                        </button>
+                        <button type="button" onClick={onCancel} className="bg-white text-text py-2 px-4 rounded hover:bg-background">
+                            Cancel
+                        </button>
+                    </div>
+                </form>
+            </div></>
     );
 }
 
--- frontend/src/components/_admin/Settings/ManageSettings/ManageBranches/BranchManager.jsx
+++ frontend/src/components/_admin/Settings/ManageSettings/ManageBranches/BranchManager.jsx
@@ -3,6 +3,7 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import BranchTable from './BranchTable';
 import BranchForm from './BranchForm';
 import { useBranchActions, useFetchBranches } from '../../../../../hooks/books/useFetchBranches';
+import { RxCross1 } from 'react-icons/rx';
 
 
 function BranchManager() {
@@ -83,8 +84,7 @@ function BranchManager() {
             <div className="mb-4 flex justify-end items-center">
                 <button
                     onClick={handleCreate}
-                    className=" bg-[#758aae] text-white active:bg-[#80CAEE] 
-            font-medium rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 px-2 py-2 md:px-3 "
+                    className="rounded-md border-border gap-2 bg-background text-text px-4 py-2 text-sm font-semibold shadow-sm disabled:opacity-50  hover:bg-secondary focus:outline-none focus:ring-2 focus:ring-primary"
                     type="button"
                 >
                     Create New Branch
@@ -105,8 +105,8 @@ function BranchManager() {
             )}
 
             {showForm && (
-                <div className="fixed inset-0 lg:left-[18rem] bg-black bg-opacity-50 flex items-center justify-center">
-                    <div className="bg-neutral-50 dark:border-[#2E3A47] dark:bg-[#24303F] p-6 rounded shadow-lg">
+                <div className="fixed inset-0 overflow-y-auto h-full w-full flex items-center justify-center bg-[#64748B] bg-opacity-75 transition-opacity z-50">
+                    <div className="bg-surface p-6 shadow-lg  rounded-md w-full mx-auto my-auto max-w-2xl">
 
                         <BranchForm
                             initialData={editData}
@@ -119,9 +119,20 @@ function BranchManager() {
 
             {/* Delete Branch */}
             {deleteModal && (
-                <div className="fixed inset-0 lg:left-[18rem] bg-black bg-opacity-50 flex items-center justify-center">
-                    <div className="bg-neutral-50 dark:border-[#2E3A47] dark:bg-[#24303F] p-6 rounded shadow-lg">
-
+                <div className="fixed inset-0 overflow-y-auto h-full w-full flex items-center justify-center bg-[#64748B] bg-opacity-75 transition-opacity z-50">
+                    <div className="bg-surface p-6 shadow-lg  rounded-md w-full mx-auto my-auto max-w-2xl">
+                        <div className="flex justify-end items-end ">
+                            <RxCross1
+                                style={{
+                                    height: 18,
+                                    width: 23,
+                                    cursor: "pointer",
+                                    color: "var(--color-text)",
+                                    strokeWidth: 2,
+                                }}
+                                onClick={() => setDeleteModal(false)}
+                            />
+                        </div>
 
 
                         <div className=" md:mx-20">
@@ -154,31 +165,42 @@ function BranchManager() {
                 </div>
             )}
             {viewData && (
-                <div className="fixed inset-0 lg:left-[18rem] bg-black bg-opacity-50 flex items-center justify-center">
-                    <div className="bg-neutral-50 dark:border-[#2E3A47] dark:bg-[#24303F] p-6 rounded shadow-lg">
-
+                <div className="fixed inset-0 overflow-y-auto h-full w-full flex items-center justify-center bg-[#64748B] bg-opacity-75 transition-opacity z-50">
+                    <div className="bg-surface p-6 shadow-lg  rounded-md w-full mx-auto my-auto max-w-2xl">
+                        <div className="flex justify-end items-end ">
+                            <RxCross1
+                                style={{
+                                    height: 18,
+                                    width: 23,
+                                    cursor: "pointer",
+                                    color: "var(--color-text)",
+                                    strokeWidth: 2,
+                                }}
+                                onClick={() => setViewData(false)}
+                            />
+                        </div>
                         <div className=" md:mx-20">
                             {/* Form Heading */}
-                            <div className="rounded-sm p-3 mb-8 bg-slate-100 border-b border-[#E2E8F0] py-4 px-6.5 dark:border-[#2E3A47]  dark:bg-[#2c3745]">
-                                <h3 className="font-bold text-[#313D4A] dark:text-white ">
+                            <div className="rounded-sm p-3 mb-8 bg-surface border-b border-border py-4 px-6.5 ">
+                                <h3 className="font-bold text-text ">
                                     View Branch
                                 </h3>
                             </div>
-                            <div className='p-2 border bg-slate-100 rounded-md shadow-sm'>
+                            <div className='p-2 border bg-background rounded-md shadow-sm'>
                                 <p
                                     className='py-4 px-2'
-                                ><strong className='text-gray-700 dark:text-neutral-400 px-2'>Name:</strong> {viewData.name}</p>
-                                <p className='py-4 px-2'><strong className='text-gray-700 dark:text-neutral-400 px-2'>Address:</strong> {viewData.address}</p>
-                                <p className='py-4 px-2'><strong className='text-gray-700 dark:text-neutral-400 px-2'>City:</strong> {viewData.city}</p>
-                                <p className='py-4 px-2'><strong className='text-gray-700 dark:text-neutral-400 px-2'>Country:</strong> {viewData.country}</p>
-                                <p className='py-4 px-2'><strong className='text-gray-700 dark:text-neutral-400 px-2'>Phone:</strong> {viewData.phone}</p>
+                                ><strong className='text-text px-2'>Name:</strong> {viewData.name}</p>
+                                <p className='py-4 px-2'><strong className='text-text px-2'>Address:</strong> {viewData.address}</p>
+                                <p className='py-4 px-2'><strong className='text-text px-2'>City:</strong> {viewData.city}</p>
+                                <p className='py-4 px-2'><strong className='text-text px-2'>Country:</strong> {viewData.country}</p>
+                                <p className='py-4 px-2'><strong className='text-text px-2'>Phone:</strong> {viewData.phone}</p>
 
 
                             </div>
                             <div className='flex justify-between gap-4'>
                                 <button
                                     onClick={() => setViewData(null)}
-                                    className="mt-4 bg-gray-500 text-white py-2 px-4 rounded hover:bg-gray-600"
+                                    className="mt-4 bg-white text-text py-2 px-4 rounded hover:bg-background"
                                 >
                                     Close
                                 </button>
