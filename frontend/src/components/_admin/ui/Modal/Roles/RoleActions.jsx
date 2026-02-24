d6cacab37cfdf06d83f66125f7ff25410a120f8c
razaakomal <komal.raza@leoatech.com>
2026-02-24 09:04:16 +0500

    LIB-267-[frontend]-refactor RoleActions component for improved styling and accessibility

--- frontend/src/components/_admin/ui/Modal/Roles/RoleActions.jsx
+++ frontend/src/components/_admin/ui/Modal/Roles/RoleActions.jsx
@@ -97,8 +97,8 @@ const RoleModal = ({ entityId, isEdit, values, close }) => {
 
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-[#64748B]/75 dark:bg-slate-300/65 lg:left-[18rem]">
-      <div className="relative w-[90%] max-w-md bg-neutral-50 dark:border-[#2E3A47] dark:bg-[#24303F] p-10 rounded-md shadow-lg">
+    <div className="fixed inset-0 overflow-y-auto h-full w-full flex items-center justify-center bg-[#64748B] bg-opacity-75 transition-opacity z-50">
+      <div className="relative p-5 rounded-md mx-auto my-auto w-full max-w-lg bg-surface">
         {/* Modal Close Button */}
         <div className="absolute top-4 right-4">
           <RxCross1
@@ -106,21 +106,25 @@ const RoleModal = ({ entityId, isEdit, values, close }) => {
               height: 18,
               width: 23,
               cursor: "pointer",
-              color: "#FFF",
+              color: "var(--color-text)",
               strokeWidth: 2,
             }}
             onClick={close}
           />
         </div>
 
-        <div className="flex flex-col justify-between items-center gap-5">
-          <h1 className="text-[17px] font-bold ">
+        <div className="flex justify-start items-start border-b pb-3 mb-4 ">
+          <h1 className="text-lg font-bold ">
             {isEdit ? "Update Role" : "Delete Role"}
           </h1>
+        </div>
+        <div className="flex flex-col justify-between items-center gap-5">
+
+
           {isEdit ? (
             <form onSubmit={handleSubmit(onSubmit)}>
               <div>
-                <label className="mb-2.5 block text-blue-500 dark:text-white">
+                <label className="mb-2.5 block text-text">
                   Role ID
                 </label>
                 <input
@@ -129,7 +133,7 @@ const RoleModal = ({ entityId, isEdit, values, close }) => {
                   placeholder="Role ID"
                   {...register("role_id")}
                   readOnly={true}
-                  className="w-full border-green-300 rounded-sm border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                  className="w-full border-border rounded-sm border-[1.5px] border-stroke bg-background py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter "
                 />
                 {errors?.role_id?.message && (
                   <p className="format-message error">
@@ -138,7 +142,7 @@ const RoleModal = ({ entityId, isEdit, values, close }) => {
                 )}
               </div>
               <div>
-                <label className=" mt-3 mb-2.5 block text-blue-500 dark:text-white">
+                <label className=" mt-3 mb-2.5 block text-text">
                   Name <span className="text-red-500">*</span>
                 </label>
                 <input
@@ -146,26 +150,25 @@ const RoleModal = ({ entityId, isEdit, values, close }) => {
                   type="text"
                   placeholder="Role Name"
                   {...register("name", { required: true })}
-                  className="w-full border-green-300 rounded-sm border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                  className="w-full border-border bg-background rounded-sm border-[1.5px] border-stroke  py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter "
                 />
                 {errors?.name?.message && (
                   <p className="format-message error">{errors.name.message}</p>
                 )}
               </div>
-              <div className="flex justify-evenly items-center p-5  ">
-                {" "}
-                <button
-                  className="inline-flex w-full justify-center rounded-md bg-[#FFBA00] hover:bg-orange-400 px-3 py-2 text-sm font-semibold text-white shadow-sm  sm:ml-3 sm:w-auto"
-                  disabled={!isDirty || !isValid || isSubmitting}
-                >Update </button>
-
+              <div className="mt-10 flex flex-col-reverse md:flex-row gap-4 md:gap-2 p-5  ">
                 <button
                   onClick={close}
-                  className="mt-3 inline-flex w-full justify-center rounded-md bg-white hover:bg-neutral-100 px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300  sm:mt-0 sm:w-auto"
+                className="rounded-md bg-white px-6 py-2 text-md font-medium text-gray-900 shadow-md ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
 
                 >  Close</button>
-
-              </div>{" "}
+                <button
+                  className={`flex justify-center items-center border-2 border-border gap-2 bg-background text-text px-3 py-2 rounded-md 
+                hover:bg-secondary focus:outline-none focus:ring-2 focus:ring-primary 
+                transition-colors duration-200 ${isSubmitting ? 'opacity-50 cursor-not-allowed' : ''
+                    }`} disabled={!isDirty || !isValid || isSubmitting}
+                >Update </button>
+              </div>
               {errors && (
                 <span className="text-meta-1 text-sm">
                   {errors?.root?.message}
@@ -175,23 +178,24 @@ const RoleModal = ({ entityId, isEdit, values, close }) => {
           ) : (
             <>
 
-              <p className="text-[18px] text-gray-700 dark:text-gray-300">Are u sure you want to delete the <span className="text-[18px] text-red-400 underline">{`${(values?.name).toUpperCase()} `}</span> Role ?</p>
+              <p className="text-md mt-2 font-bold text-text">Do you want to delete the <span className="text-sm text-primary underline">{`(${(values?.name).toUpperCase()})`}</span>  role permanently?</p>
 
               {/* Action Buttons */}
 
               <div className="mt-10 flex justify-evenly gap-6">
-                <button
-                  onClick={handleDelete}
-                  className="border-2 p-2 px-5 bg-red-400 text-white rounded-md text-[17px] hover:bg-red-600"
-                >
-                  Yes
-                </button>
                 <button
                   onClick={close}
                   className="border p-2 px-5 bg-green-400 text-white rounded-md text-[17px] hover:bg-green-600"
                 >
                   No
                 </button>
+                <button
+                  onClick={handleDelete}
+                  className="border-2 p-2 px-5 border-border bg-background text-text rounded-md text-md hover:bg-secondary focus:outline-none focus:ring-2 focus:ring-primary"
+                >
+                  Yes, Delete Role
+                </button>
+
               </div>
 
 
