2897bc7a62bcd54e159d9cb554106b0f90b9b77a
razaakomal <komal.raza@leoatech.com>
2026-02-24 09:08:48 +0500

    LIB-296-[frontend]-refactor CoverManager component for improved styling and accessibility

--- frontend/src/components/_admin/Settings/ManageSettings/ManageCovers/CoverManager.jsx
+++ frontend/src/components/_admin/Settings/ManageSettings/ManageCovers/CoverManager.jsx
@@ -3,6 +3,7 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import CoverTable from './CoverTable';
 import CoverForm from './CoverForm';
 import { useFetchCovers, useCoverActions } from '../../../../../hooks/books/useFetchCovers';
+import { RxCross1 } from 'react-icons/rx';
 
 function CoverManager() {
     const [showForm, setShowForm] = useState(false);
@@ -86,8 +87,7 @@ function CoverManager() {
             <div className="mb-4 flex justify-end items-center">
                 <button
                     onClick={handleCreate}
-                    className=" bg-[#758aae] text-white active:bg-[#80CAEE] 
-            font-medium rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 px-2 py-2 md:px-3 "
+                    className="rounded-md border-border gap-2 bg-background text-text px-4 py-2 text-sm font-semibold shadow-sm disabled:opacity-50  hover:bg-secondary focus:outline-none focus:ring-2 focus:ring-primary"
                     type="button"
                 >
                     Add New Cover
@@ -108,9 +108,8 @@ function CoverManager() {
             )}
 
             {showForm && (
-                <div className="fixed inset-0 lg:left-[18rem] bg-black bg-opacity-50 flex items-center justify-center">
-                    <div className="bg-neutral-50 dark:border-[#2E3A47] dark:bg-[#24303F] p-6 rounded shadow-lg">
-
+                <div className="fixed inset-0 overflow-y-auto h-full w-full flex items-center justify-center bg-[#64748B] bg-opacity-75 transition-opacity z-50">
+                    <div className="bg-surface p-6 shadow-lg  rounded-md w-full mx-auto my-auto max-w-2xl">
                         <CoverForm
                             initialData={editData}
                             saveCover={handleSubmit}
@@ -122,10 +121,20 @@ function CoverManager() {
 
             {/* Delete Cover */}
             {deleteModal && (
-                <div className="fixed inset-0 lg:left-[18rem] bg-black bg-opacity-50 flex items-center justify-center">
-                    <div className="bg-neutral-50 dark:border-[#2E3A47] dark:bg-[#24303F] p-6 rounded shadow-lg">
-
-
+                <div className="fixed inset-0 overflow-y-auto h-full w-full flex items-center justify-center bg-[#64748B] bg-opacity-75 transition-opacity z-50">
+                    <div className="bg-surface p-6 shadow-lg  rounded-md w-full mx-auto my-auto max-w-2xl">
+                        <div className="flex justify-end items-end ">
+                            <RxCross1
+                                style={{
+                                    height: 18,
+                                    width: 23,
+                                    cursor: "pointer",
+                                    color: "var(--color-text)",
+                                    strokeWidth: 2,
+                                }}
+                                onClick={() => setDeleteModal(false)}
+                            />
+                        </div>
 
                         <div className=" md:mx-20">
                             <div className="rounded-sm p-3 mb-8 bg-slate-100 border-b border-[#E2E8F0] py-4 px-6.5 dark:border-[#2E3A47]  dark:bg-[#2c3745]">
@@ -157,25 +166,36 @@ function CoverManager() {
                 </div>
             )}
             {viewData && (
-                <div className="fixed inset-0 lg:left-[18rem] bg-black bg-opacity-50 flex items-center justify-center">
-                    <div className="bg-neutral-50 dark:border-[#2E3A47] dark:bg-[#24303F] p-6 rounded shadow-lg">
-
+                <div className="fixed inset-0 overflow-y-auto h-full w-full flex items-center justify-center bg-[#64748B] bg-opacity-75 transition-opacity z-50">
+                    <div className="bg-surface p-6 shadow-lg  rounded-md w-full mx-auto my-auto max-w-2xl">
+                        <div className="flex justify-end items-end ">
+                            <RxCross1
+                                style={{
+                                    height: 18,
+                                    width: 23,
+                                    cursor: "pointer",
+                                    color: "var(--color-text)",
+                                    strokeWidth: 2,
+                                }}
+                                onClick={() => setViewData(false)}
+                            />
+                        </div>
                         <div className=" md:mx-20">
-                            <div className="rounded-sm p-3 mb-8 bg-slate-100 border-b border-[#E2E8F0] py-4 px-6.5 dark:border-[#2E3A47]  dark:bg-[#2c3745]">
-                                <h3 className="font-bold text-[#313D4A] dark:text-white ">
+                            <div className="rounded-sm p-3 mb-8 bg-surface border-b border-border py-4 px-6.5 ">
+                                <h3 className="font-bold text-text ">
                                     View Cover
                                 </h3>
                             </div>
-                            <div className='p-2 border bg-slate-100 rounded-md shadow-sm'>
+                            <div className='p-2 border bg-background rounded-md shadow-sm'>
                                 <p
                                     className='py-4 px-2'
-                                ><strong className='text-gray-700 dark:text-neutral-400 px-2'>Name:</strong> {viewData.name}</p>
+                                ><strong className='text-text px-2'>Name:</strong> {viewData.name}</p>
 
                             </div>
                             <div className='flex justify-between gap-4'>
                                 <button
                                     onClick={() => setViewData(null)}
-                                    className="mt-4 bg-gray-500 text-white py-2 px-4 rounded hover:bg-gray-600"
+                                    className="mt-4 bg-white text-text py-2 px-4 rounded hover:bg-background"
                                 >
                                     Close
                                 </button>
