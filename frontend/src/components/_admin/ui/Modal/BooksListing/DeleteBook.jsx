16d90de41ce378e6d9c4422b1ff081d7ddeafea4
razaakomal <komal.raza@leoatech.com>
2026-02-26 03:55:06 +0500

    LIB-141-[frontend]-refactor DeleteBookModal  for improved styling and accessibility

--- frontend/src/components/_admin/ui/Modal/BooksListing/DeleteBook.jsx
+++ frontend/src/components/_admin/ui/Modal/BooksListing/DeleteBook.jsx
@@ -23,45 +23,50 @@ const DeleteBook = ({ book, close }) => {
   });
 
   return (
-    <div className="fixed left-0 top-0  inset-0 bg-[#64748B] bg-opacity-75 transition-opacity dark:bg-slate-300 dark:bg-opacity-75 lg:left-[18rem]">
-      <div className="relative p-5 rounded-md">
+    <div className="fixed inset-0 overflow-y-auto h-full w-full  bg-[#64748B] bg-opacity-75 transition-opacity z-50">
+      <div className="relative p-5 rounded-md w-full mx-auto my-auto max-w-5xl ">
         {/* Modal Close Button */}
-        <div className="flex justify-end p-5 md:p-10 ">
+        <div className="flex justify-center items-center p-5 md:p-10 ">
           <RxCross1
             style={{
               height: 18,
               width: 23,
               cursor: "pointer",
-              color: "#FFF",
+              color: "var(--color-text)",
               strokeWidth: 2,
             }}
             onClick={close}
           />
         </div>
         <div className="my-5 rounded-md flex justify-center items-center z-70  overflow-hidden xs:h-[400px] overflow-y-auto ">
-          <div className="mt-10 p-10 relative rounded-md border border-[#E2E8F0] bg-white shadow-lg dark:border-[#2E3A47] dark:bg-[#24303F] md:px-8 md:py-8 ">
-            <div className="flex justify-between items-center rounded-sm p-3 bg-slate-100 border-b border-[#E2E8F0] py-4 px-6.5 dark:bg-[#2E3A47] dark:border-[#2E3A47]">
-              <h3 className="font-bold text-[#313D4A] dark:text-white">
+          <div className="mt-10 p-10 relative rounded-md border border-border bg-surface shadow-lg md:px-8 md:py-8 ">
+            <div className="flex justify-between items-center rounded-sm p-3  border-b border-primary py-4 px-6.5 ">
+              <h3 className="font-bold text-text">
                 Delete Book{" "}
               </h3>
             </div>
 
-            <p className="mt-10 text-xl sm:w-[500px]  text-blue-500 md:break-all">
+            <p className="mt-10 text-xl sm:w-[500px] text-text md:break-all">
               {`Are u sure you want to delete the Book `}<span className="font-bold text-[17px] text-orange-400">{`${book?.title}`}</span> {` from the library?`}
             </p>
-            <div className="mt-10 flex justify-evenly gap-2">
-              <button
-                onClick={() => deleteBookMutation(book?.id)}
-                className="border p-2 px-10 text-medium text-md bg-slate-600 text-white rounded-md text-[17px] hover:bg-slate-400"
-              >
-                {isLoading ? "Deleting..."
-                  : "Deleting"}
-              </button>
+            <div className="mt-10 flex flex-col md:flex-row justify-end items-end gap-2">
+
               <button
                 onClick={close}
-                className="border p-2 px-10 text-medium text-xl bg-slate-600 text-white rounded-md text-[17px] hover:bg-slate-400"
+                className="`flex items-center gap-2 border-2 border-border py-2 px-4 font-medium text-md bg-white text-black rounded-md  hover:bg-secondary"
               >
                 No
+              </button>
+              <button
+                onClick={() => deleteBookMutation(book?.id)}
+                className={`flex items-center border-2 border-border gap-2 bg-background text-text px-4 py-2 rounded-md 
+                hover:bg-secondary focus:outline-none focus:ring-2 focus:ring-primary 
+                transition-colors duration-200 
+                  }`}
+              // className="border p-2 px-10 text-medium text-md bg-slate-600 text-white rounded-md text-[17px] hover:bg-slate-400"
+              >
+                {isLoading ? "Deleting..."
+                  : "Yes, Delete"}
               </button>
             </div>
           </div>
