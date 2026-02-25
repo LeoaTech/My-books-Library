7902f63139661229379966cc023899ccc5bbc1aa
razaakomal <komal.raza@leoatech.com>
2026-02-26 03:45:31 +0500

    LIB-105-[frontend]-refactor roles table for improved styling and accessibility

--- frontend/src/components/_admin/ui/Tables/RolesTable.jsx
+++ frontend/src/components/_admin/ui/Tables/RolesTable.jsx
@@ -62,48 +62,48 @@ const RolesTable = ({ open, setOpenRoleModal, searchTerm }) => {
     return <h1>Loading...</h1>;
   }
   return (
-    <div className="rounded-sm border border-[#E2E8F0] bg-white shadow-default p-8 border-b  dark:border-[#2E3A47] dark:bg-[#24303F]">
-      <div className="max-w-full overflow-x-auto">
-        <table className="w-full table-auto">
+    <div className="rounded-sm border border-border bg-background shadow-default p-8 border-b]">
+      <div className="max-w-full overflow-x-auto ">
+        <table className="w-full table-auto divide-y divide-primary">
           <thead>
-            <tr className="bg-[#F7F9FC] text-left dark:bg-[#313D4A]">
-              <th className="min-w-[200px] py-4 px-4 font-medium text-gray-600 dark:text-white xl:pl-11">
+            <tr className="bg-secondary text-left ">
+              <th className="min-w-[200px] py-4 px-4 font-medium text-text xl:pl-11">
                 Role ID
               </th>
-              <th className="min-w-[220px] py-4 px-4 font-medium text-gray-600 dark:text-white xl:pl-11">
+              <th className="min-w-[220px] py-4 px-4 font-medium text-text xl:pl-11">
                 Role Name
               </th>
-              <th className="min-w-[80px] py-4 px-4 font-medium text-gray-600 dark:text-white xl:pl-11">
+              <th className="min-w-[80px] py-4 px-4 font-medium text-text xl:pl-11">
                 Default
               </th>
-              <th className="min-w-[220px] py-4 px-4 font-medium text-gray-600 dark:text-white xl:pl-11">
+              <th className="min-w-[220px] py-4 px-4 font-medium text-text xl:pl-11">
                 Created At
               </th>
-              <th className="min-w-[170px] py-4 px-4 font-medium text-gray-600 dark:text-white xl:pl-11">
+              <th className="min-w-[170px] py-4 px-4 font-medium text-text xl:pl-11">
                 Actions
               </th>
             </tr>
           </thead>
-          <tbody>
+          <tbody className="divide-y divide-primary">
             {currentRows?.map((role) => (
               <tr key={role?.role_id}>
-                <td className="border-b border-[#eee] py-5 px-4 pl-5 dark:border-[#2E3A47] xl:pl-11">
+                <td className="border-b border-border py-5 px-4 pl-5  xl:pl-11">
                   {role?.role_id}
                 </td>
-                <td className="border-b border-[#eee] py-5 px-4 pl-5 dark:border-[#2E3A47] xl:pl-11">
-                  <p className="inline-flex rounded-full bg-[#FF6766] bg-opacity-10 py-1 px-3 text-sm font-medium text-[#F0950C]">
+                <td className="border-b border-border py-5 px-4 pl-5  xl:pl-11">
+                  <p className="inline-flex rounded-full  bg-opacity-10 py-1 px-3 text-sm font-medium text-primary">
                     {role?.name}
                   </p>
                 </td>
-                <td className="border-b border-[#eee] py-5 px-4 pl-5 dark:border-[#2E3A47] xl:pl-11">
-                  <p className={`inline-flex rounded-full bg-[#c5ee86] bg-opacity-10 py-1 px-3 text-sm font-medium ${role?.is_default ? "text-[#15d257]" : "text-[#de245f]"} `}>
+                <td className="border-b border-border py-5 px-4 pl-5  xl:pl-11">
+                  <p className={`inline-flex rounded-full bg-opacity-10 py-1 px-3 text-sm font-medium ${role?.is_default ? "text-text" : "text-primary"} `}>
                     {role?.is_default == true ? "true" : "false"}
                   </p>
                 </td>
-                <td className="border-b border-[#eee] py-5 px-4 pl-5 dark:border-[#2E3A47] xl:pl-11">
+                <td className="border-b border-border py-5 px-4 pl-5  xl:pl-11">
                   {new Date(role?.created_at).toDateString()} {" "} - {" "}
                   {new Date(role?.created_at).toLocaleTimeString()}                 </td>
-                <td className="border-b border-[#eee] py-5 px-8 dark:border-[#2E3A47]">
+                <td className="border-b border-border py-5 px-8 ">
                   <div className="flex items-center space-x-3.5">
                     <button
                       disabled={role?.is_default}
