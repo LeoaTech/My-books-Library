70c7d9ba599fc28997440188f0e6f353782277f6
razaakomal <komal.raza@leoatech.com>
2026-02-26 04:33:16 +0500

    LIB-254-[frontend]: refactor ManageData page styles for improved styling and accessibility

--- frontend/src/components/_admin/Settings/ManageSettings/index.jsx
+++ frontend/src/components/_admin/Settings/ManageSettings/index.jsx
@@ -23,15 +23,15 @@ const ManageSettings = () => {
     // Get the selected tab component
     const SelectedComponent = tabs.find(tab => tab.id === selectedTab)?.component;
     return (
-        <div className="flex bg-gray-100 dark:border-[#2E3A47] dark:bg-[#24303F]">
-            <div className="w-1/5 bg-neutral-100 border-r border-gray-200 dark:border-[#2E3A47] dark:bg-[#24303F]">
+        <div className="flex bg-background border-border">
+            <div className="w-1/5 bg-surface border-r border-border">
                 <ul className="flex flex-col justify-center items-center p-4 gap-4">
                     {tabs.map(tab => (
                         <li key={tab.id}>
                             <button
                                 onClick={() => setSelectedTab(tab.id)}
-                                className={`w-full sm:w-[100px] bg-[#758aae] text-white active:bg-[#80CAEE] 
-            font-medium rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 px-2 py-2 md:px-3  ${selectedTab === tab.id ? 'bg-gray-700 text-white' : 'hover:bg-gray-400'
+                                className={`w-full sm:w-[100px] bg-background text-primary active:bg-primary 
+            font-medium rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 px-2 py-2 md:px-3  ${selectedTab === tab.id ? 'bg-primary text-surface' : 'hover:bg-secondary'
                                     }`}
                             >
                                 {tab.id}
